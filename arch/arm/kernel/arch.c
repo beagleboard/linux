0b1954ee5cf05b61db540267efcbe34d38a8116b
Juha Yrjola <juha.yrjola@nokia.com>
2005-05-09 13:09:27 -0700

    ARM: Add support for ATAG_BOARD
    Adds support for board specific tag ATAG_BOARD.
    
    Signed-off-by: Juha Yrjola <juha.yrjola@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/kernel/arch.c
+++ arch/arm/kernel/arch.c
@@ -6,6 +6,7 @@
 #include <linux/config.h>
 #include <linux/init.h>
 #include <linux/types.h>
+#include <linux/string.h>
 
 #include <asm/elf.h>
 #include <asm/page.h>
@@ -42,5 +43,28 @@ static int __init parse_tag_acorn(const struct tag *tag)
 }
 
 __tagtable(ATAG_ACORN, parse_tag_acorn);
+
+#endif
+
+#ifdef CONFIG_OMAP_BOOT_TAG
+
+unsigned char omap_bootloader_tag[512];
+int omap_bootloader_tag_len = 0;
+
+static int __init parse_tag_omap(const struct tag *tag)
+{
+	u32 size = tag->hdr.size - (sizeof(tag->hdr) >> 2);
+
+        size <<= 2;
+	if (size > sizeof(omap_bootloader_tag))
+		return -1;
+
+	memcpy(omap_bootloader_tag, tag->u.omap.data, size);
+	omap_bootloader_tag_len = size;
+
+        return 0;
+}
+
+__tagtable(ATAG_BOARD, parse_tag_omap);
 
 #endif
