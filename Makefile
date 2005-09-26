9463728f1b4a297ded6f35cc1cbf4e7d59382019
Tony Lindgren <tony@atomide.com>
2005-09-26 18:03:41 +0300

    ARM: OMAP: Add clock framework for omap2
    Add clock framework for omap2.
    
    Based on omap2 clock framework code by Richard Woodruff.

--- Makefile
+++ Makefile
@@ -194,7 +194,7 @@ SUBARCH := arm
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 
 ARCH		?= $(SUBARCH)
-CROSS_COMPILE	?= arm-linux-
+CROSS_COMPILE	?= arm-xscale-linux-gnu-
 
 # Architecture as present in compile.h
 UTS_MACHINE := $(ARCH)
--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -21,14 +21,15 @@
 #include <asm/arch/mux.h>
 
 extern void omap_sram_init(void);
+extern int omap2_clk_init(void);
 
 /*
  * The machine specific code may provide the extra mapping besides the
  * default mapping provided here.
  */
 static struct map_desc omap2_io_desc[] __initdata = {
-	{ L3_VIRT,	L3_PHYS,	L3_SIZE,	MT_DEVICE },
-	{ L4_VIRT,	L4_PHYS,	L4_SIZE,	MT_DEVICE },
+	{ L3_24XX_VIRT,	L3_24XX_PHYS,	L3_24XX_SIZE,	MT_DEVICE },
+	{ L4_24XX_VIRT,	L4_24XX_PHYS,	L4_24XX_SIZE,	MT_DEVICE },
 };
 
 void __init omap_map_common_io(void)
@@ -36,4 +37,5 @@ void __init omap_map_common_io(void)
 	iotable_init(omap2_io_desc, ARRAY_SIZE(omap2_io_desc));
 	omap_sram_init();
 	omap2_mux_init();
+	omap2_clk_init();
 }
