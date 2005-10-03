39352eab91a11c2ae2fe82c6d266625a6188f29e
Tony Lindgren <tony@atomide.com>
2005-10-03 11:46:42 +0300

    ARM: OMAP: Beautify include order in system.h
    Beautify include order in system.h

--- include/asm-arm/arch-omap/system.h
+++ include/asm-arm/arch-omap/system.h
@@ -6,8 +6,8 @@
 #define __ASM_ARCH_SYSTEM_H
 #include <linux/config.h>
 #include <asm/mach-types.h>
-#include <asm/arch/hardware.h>
 #include <asm/hardware/clock.h>
+#include <asm/arch/hardware.h>
 #include <asm/arch/prcm.h>
 
 #ifndef CONFIG_MACH_VOICEBLUE
