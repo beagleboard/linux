9781f2202cc01130421b5db3c417fe5bee4774f2
Komal Shah <komal_shah802003@yahoo.com>
2006-03-28 06:12:35 -0800

    Move i2c_driver out of ov9640_sensor structure and add H4 camera sensor powerup/down functions.
    Signed-off-by: Komal Shah <komal_shah802003@yahoo.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

---
+++ drivers/media/video/omap/h4sensorpower.h
@@ -0,0 +1,17 @@
+/*
+ * drivers/media/video/omap/h4sensorpower.h
+ *
+ * Copyright (C) 2005 Texas Instruments.
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2. This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
+
+#ifndef H4SENSORPOWER_H
+#define H4SENSORPOWER_H
+
+int h4_sensor_powerup(void);
+int h4_sensor_powerdown(void);
+
+#endif /*H4SENSORPOWER_H*/
