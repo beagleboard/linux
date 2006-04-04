46fc3296509ad3496ae028bc6431b002be1efb49
Jonathan McDowell <noodles@earth.li>
2006-04-04 07:40:09 -0700

    [PATCH] ARM: OMAP: Fix USB host on 1510/5910
    The OMAP5910 datasheet says that the lb clock needs enabled for USB
    to work, so it seems reasonable that this is required.

--- drivers/usb/host/ohci-omap.c
+++ drivers/usb/host/ohci-omap.c
@@ -353,7 +353,11 @@ int usb_hcd_omap_probe (const struct hc_driver *driver,
 	if (IS_ERR(usb_host_ck))
 		return PTR_ERR(usb_host_ck);
 
-	usb_dc_ck = clk_get(0, "usb_dc_ck");
+	if (!cpu_is_omap1510())
+		usb_dc_ck = clk_get(0, "usb_dc_ck");
+	else
+		usb_dc_ck = clk_get(0, "lb_ck");
+
 	if (IS_ERR(usb_dc_ck)) {
 		clk_put(usb_host_ck);
 		return PTR_ERR(usb_dc_ck);
