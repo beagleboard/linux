a75c9e6cdb68dc26e40f5ea1a887ccb8c54e7835
Imre Deak <imre.deak@solidboot.com>
2006-07-24 16:02:19 +0300

    lcd_mipid: keep a delay of two frames when disabling the LCD
    ls041y3 needs the PCLK,HS,VS signals for a while after sending the sleep
    in command, so sleep 50 msec.
    
    Signed-off-by: Imre Deak <imre.deak@solidboot.com>
    Signed-off-by: Juha Yrjola <juha.yrjola@solidboot.com>

--- drivers/video/omap/lcd_mipid.c
+++ drivers/video/omap/lcd_mipid.c
@@ -187,7 +187,7 @@ static void hw_guard_wait(struct mipid_device *md)
 
 static void set_sleep_mode(struct mipid_device *md, int on)
 {
-	int cmd, sleep_time = 5;
+	int cmd, sleep_time = 50;
 
 	if (on)
 		cmd = MIPID_CMD_SLEEP_IN;
@@ -197,7 +197,9 @@ static void set_sleep_mode(struct mipid_device *md, int on)
 	mipid_cmd(md, cmd);
 	hw_guard_start(md, 120);
 	/* When we enable the panel, it seems we _have_ to sleep
-	 * 120 ms before sending the init string */
+	 * 120 ms before sending the init string. When disabling the
+	 * panel we'll sleep for the duration of 2 frames, so that the
+	 * controller can still provide the PCLK,HS,VS signals. */
 	if (!on)
 		sleep_time = 120;
 	msleep(sleep_time);
