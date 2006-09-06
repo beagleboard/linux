231954e596236437bb69e22755e5788ddf76a268
Juha Yrjola <juha.yrjola@solidboot.com>
2006-09-06 17:01:15 +0300

    IDLETIMER: Update license text
    Signed-off-by: Juha Yrjola <juha.yrjola@solidboot.com>

--- net/ipv4/netfilter/ipt_IDLETIMER.c
+++ net/ipv4/netfilter/ipt_IDLETIMER.c
@@ -4,13 +4,22 @@
  * Netfilter module to trigger a timer when packet matches.
  * After timer expires a kevent will be sent.
  *
- * Copyright (C) 2004 Nokia Corporation
- * Written by Timo Teräs <ext-timo.teras@nokia.com>
+ * Copyright (C) 2004 Nokia Corporation. All rights reserved.
+ * Written by Timo Teras <ext-timo.teras@nokia.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 
+ * version 2 as published by the Free Software Foundation. 
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 #include <linux/module.h>
@@ -294,6 +303,6 @@ static void __exit fini(void)
 module_init(init);
 module_exit(fini);
 
-MODULE_AUTHOR("Timo Teräs <ext-timo.teras@nokia.com>");
+MODULE_AUTHOR("Timo Teras <ext-timo.teras@nokia.com>");
 MODULE_DESCRIPTION("iptables idletimer target module");
 MODULE_LICENSE("GPL");
