70f2e9a31846277fff6e9c789e92d4ae320d5806
Kyungmin Park <kyungmin.park@samsung.com>
2006-11-01 22:50:12 +0200

    ARM: OMAP: fix apollon USB device support
    -  Add apollon use the non-double-buffering in PIO mode
    -  Fix compiler warning
    
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/usb/gadget/omap_udc.c
+++ drivers/usb/gadget/omap_udc.c
@@ -2579,6 +2579,7 @@ omap_ep_setup(char *name, u8 addr, u8 type,
 		 * (for more reliable behavior)
 		 */
 		if ((!use_dma && (addr & USB_DIR_IN))
+				|| machine_is_omap_apollon()
 				|| cpu_is_omap15xx())
 			dbuf = 0;
 
@@ -2963,9 +2964,10 @@ known:
 	}
 
 	create_proc_file();
-	device_add(&udc->gadget.dev);
-	return 0;
-
+	status = device_add(&udc->gadget.dev);
+	if (!status)
+		return status;
+	/* If fail, fall through */
 #ifdef	USE_ISO
 cleanup3:
 	free_irq(pdev->resource[2].start, udc);
