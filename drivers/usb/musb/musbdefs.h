675dab8cbe4cb8bc89f15f7b16e151ef20d9ae58
Tony Lindgren <tony@atomide.com>
2007-08-13 04:13:20 -0700

    musb_hdrc: Get rid of old unused MUSB_EpFifoDescriptor
    Get rid of old unused MUSB_EpFifoDescriptor
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/usb/musb/musbdefs.h
+++ drivers/usb/musb/musbdefs.h
@@ -246,26 +246,6 @@ enum musb_g_ep0_state {
 
 #define MUSB_MODE(musb) ((musb)->is_host ? "Host" : "Peripheral")
 
-/************************** Ep Configuration ********************************/
-
-/** The End point descriptor */
-struct MUSB_EpFifoDescriptor {
-	u8 bType;		/* 0 for autoconfig, CNTR, ISOC, BULK, INTR */
-	u8 bDir;		/* 0 for autoconfig, INOUT, IN, OUT */
-	int wSize;		/* 0 for autoconfig, or the size */
-};
-
-#define MUSB_EPD_AUTOCONFIG	0
-
-#define MUSB_EPD_T_CNTRL	1
-#define MUSB_EPD_T_ISOC		2
-#define MUSB_EPD_T_BULK		3
-#define MUSB_EPD_T_INTR		4
-
-#define MUSB_EPD_D_INOUT	0
-#define MUSB_EPD_D_TX		1
-#define MUSB_EPD_D_RX		2
-
 /******************************** TYPES *************************************/
 
 /*
