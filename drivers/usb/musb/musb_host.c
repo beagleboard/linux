318068ca7104295d6177637521c5ec6cf8cb88b1
Tony Lindgren <tony@atomide.com>
2007-10-09 16:26:59 -0700

    musb_hdrc: Allow suspend if host is already suspended
    Allow suspend if host is already suspended
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -2128,6 +2128,9 @@ static int musb_bus_suspend(struct usb_hcd *hcd)
 {
 	struct musb	*musb = hcd_to_musb(hcd);
 
+	if (musb->xceiv.state == OTG_STATE_A_SUSPEND)
+		return 0;
+
 	if (is_host_active(musb) && musb->is_active) {
 		WARN("trying to suspend as %s is_active=%i\n",
 			otg_state_string(musb), musb->is_active);
