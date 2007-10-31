c4aeae3e38d6d1f46aff2e753d4ba6fc308e1b53
Tony Lindgren <tony@atomide.com>
2007-10-31 02:05:21 -0700

    musb_hdrc: Trivial compile fixes for gadget after updating kernel
    Host fixes to follow.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/usb/musb/musb_core.h
+++ drivers/usb/musb/musb_core.h
@@ -43,7 +43,7 @@
 #include <linux/clk.h>
 #include <linux/device.h>
 #include <linux/usb/ch9.h>
-#include <linux/usb_gadget.h>
+#include <linux/usb/gadget.h>
 #include <linux/usb.h>
 #include <linux/usb/otg.h>
 #include <linux/usb/musb.h>
