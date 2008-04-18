5cbb5ae3d45f2d27c0747f359eadcaee8570cc3d
Eduardo Valentin <eduardo.valentin@indt.org.br>
2008-04-18 04:00:54 -0400

    Code clean-up for sound/arm/omap/omap-alsa-dma.h
    Removed lots of whitespaces and a few errors and
    warnings reported by checkpatch.pl.
    
    Signed-off-by: Eduardo Valentin <eduardo.valentin@indt.org.br>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- sound/arm/omap/omap-alsa-dma.h
+++ sound/arm/omap/omap-alsa-dma.h
@@ -1,4 +1,4 @@
-/*  
+/*
  * linux/sound/arm/omap/omap-alsa-dma.h
  *
  * Common audio DMA handling for the OMAP processors
@@ -6,7 +6,7 @@
  * Copyright (C) 2006 Mika Laitio <lamikr@cc.jyu.fi>
  *
  * Copyright (C) 2005 Instituto Nokia de Tecnologia - INdT - Manaus Brazil
- * 
+ *
  * Copyright (C) 2004 Texas Instruments, Inc.
  *
  * Copyright (C) 2000, 2001 Nicolas Pitre <nico@cam.org>
@@ -21,8 +21,9 @@
  *
  * History:
  *
- * 
- * 2004/08/12  Nishanth Menon - Modified to integrate Audio requirements on 1610,1710 platforms
+ *
+ * 2004/08/12  Nishanth Menon - Modified to integrate Audio requirements on
+ * 				1610, 1710 platforms
  *
  * 2005/07/25  INdT Kernel Team - Renamed to omap-alsa-dma.h. Ported to Alsa.
  */
@@ -30,23 +31,22 @@
 #ifndef __OMAP_AUDIO_ALSA_DMA_H
 #define __OMAP_AUDIO_ALSA_DMA_H
 
-/************************** INCLUDES *************************************/
-
 #include <asm/arch/omap-alsa.h>
 
-/************************** GLOBAL DATA STRUCTURES *********************************/
+/* Global data structures */
 
 typedef void (*dma_callback_t) (int lch, u16 ch_status, void *data);
 
-/**************** ARCH SPECIFIC FUNCIONS *******************************************/
+/* arch specific functions */
 
-void omap_clear_alsa_sound_dma(struct audio_stream * s);
+void omap_clear_alsa_sound_dma(struct audio_stream *s);
 
 int omap_request_alsa_sound_dma(int device_id, const char *device_name,
-			   void *data, int **channels);
+				void *data, int **channels);
 int omap_free_alsa_sound_dma(void *data, int **channels);
 
-int omap_start_alsa_sound_dma(struct audio_stream *s, dma_addr_t dma_ptr,  u_int dma_size);
+int omap_start_alsa_sound_dma(struct audio_stream *s, dma_addr_t dma_ptr,
+				u_int dma_size);
 
 void omap_stop_alsa_sound_dma(struct audio_stream *s);
 
