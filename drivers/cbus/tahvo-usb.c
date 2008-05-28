d305c615bb7f85d49788a4ed28cb6aace2317ab7
Tony Lindgren <tony@atomide.com>
2008-05-27 18:25:38 -0700

    ARM: OMAP: Misc compile fixes
    Misc compile fixes
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/cbus/tahvo-usb.c
+++ drivers/cbus/tahvo-usb.c
@@ -31,6 +31,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
+#include <linux/io.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
 #include <linux/usb/ch9.h>
--- drivers/i2c/chips/isp1301_omap.c
+++ drivers/i2c/chips/isp1301_omap.c
@@ -25,6 +25,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
+#include <linux/io.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
 #include <linux/usb/ch9.h>
