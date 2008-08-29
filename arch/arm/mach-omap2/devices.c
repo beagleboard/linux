39a389e5729342bd7d0551e55f326d4497418952
Sergio Aguirre <saaguirre@ti.com>
2008-08-29 18:34:17 -0500

    OMAP3 camera driver: OMAP34XXCAM: Camera Base Address.
    ARM: OMAP: OMAP34XXCAM: Camera Base Address.
    
    Adding OMAP 3 Camera registers base address, and Platform Device.
    
    Signed-off-by: Sergio Aguirre <saaguirre@ti.com>
    Signed-off-by: Sameer Venkatraman <sameerv@ti.com>
    Signed-off-by: Mohit Jalori <mjalori@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -50,6 +50,32 @@ static inline void omap_init_camera(void)
 {
 	platform_device_register(&omap_cam_device);
 }
+
+#elif defined(CONFIG_VIDEO_OMAP3) || defined(CONFIG_VIDEO_OMAP3_MODULE)
+
+static struct resource cam_resources[] = {
+	{
+		.start		= OMAP34XX_CAMERA_BASE,
+		.end		= OMAP34XX_CAMERA_BASE + 0x1B70,
+		.flags		= IORESOURCE_MEM,
+	},
+	{
+		.start		= INT_34XX_CAM_IRQ,
+		.flags		= IORESOURCE_IRQ,
+	}
+};
+
+static struct platform_device omap_cam_device = {
+	.name		= "omap34xxcam",
+	.id		= -1,
+	.num_resources	= ARRAY_SIZE(cam_resources),
+	.resource	= cam_resources,
+};
+
+static inline void omap_init_camera(void)
+{
+	platform_device_register(&omap_cam_device);
+}
 #else
 static inline void omap_init_camera(void)
 {
