4994d1ec9f118c12298f504fe907135862628bf3
Aaro Koskinen <Aaro.Koskinen@nokia.com>
2009-02-18 09:22:18 +0000

    ARM: OMAP: Allow I2C bus driver to be compiled as a module
    Fixes a linker error when OMAP I2C bus driver is compiled as a module:
    
       ERROR: "i2c_register_board_info" [arch/arm/plat-omap/i2c.ko] undefined!
    
    The I2C utility functions used for board initialization should be always
    built-in.
    
    Signed-off-by: Aaro Koskinen <Aaro.Koskinen@nokia.com>
    Acked-by: Jarkko Nikula  <jarkko.nikula@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/common.h
+++ arch/arm/plat-omap/include/mach/common.h
@@ -33,7 +33,7 @@ struct sys_timer;
 
 extern void omap_map_common_io(void);
 extern struct sys_timer omap_timer;
-#ifdef CONFIG_I2C_OMAP
+#if defined(CONFIG_I2C_OMAP) || defined(CONFIG_I2C_OMAP_MODULE)
 extern int omap_register_i2c_bus(int bus_id, u32 clkrate,
 				 struct i2c_board_info const *info,
 				 unsigned len);
