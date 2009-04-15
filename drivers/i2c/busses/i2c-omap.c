1ed6c3b5ba3fdac07f0c0061774014f2d70cb7d2
Eero Nurkkala <ext-eero.nurkkala@nokia.com>
2009-04-15 10:45:01 -0700

    i2c-omap: Fix BUFSTAT_REG reading
    The number of bytes to be received is read from wrong
    place with all OMAPs with highspeed I2C support,
    which involves a FIFO and BUFSTAT_REG. It is the 6
    bits starting from the bit 8 in the BUFSTAT_REG
    that indicate this amount of bytes to be read.
    Moreover, only the 6 LSB:s are relevant for the
    TXSTAT field.
    
    Signed-off-by: Eero Nurkkala <ext-eero.nurkkala@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/i2c/busses/i2c-omap.c
+++ drivers/i2c/busses/i2c-omap.c
@@ -672,8 +672,9 @@ omap_i2c_isr(int this_irq, void *dev_id)
 				if (stat & OMAP_I2C_STAT_RRDY)
 					num_bytes = dev->fifo_size;
 				else
-					num_bytes = omap_i2c_read_reg(dev,
-							OMAP_I2C_BUFSTAT_REG);
+					num_bytes = (omap_i2c_read_reg(dev,
+							OMAP_I2C_BUFSTAT_REG)
+							>> 8) & 0x3F;
 			}
 			while (num_bytes) {
 				num_bytes--;
@@ -711,8 +712,9 @@ omap_i2c_isr(int this_irq, void *dev_id)
 				if (stat & OMAP_I2C_STAT_XRDY)
 					num_bytes = dev->fifo_size;
 				else
-					num_bytes = omap_i2c_read_reg(dev,
-							OMAP_I2C_BUFSTAT_REG);
+					num_bytes = (omap_i2c_read_reg(dev,
+							OMAP_I2C_BUFSTAT_REG))
+							& 0x3F;
 			}
 			while (num_bytes) {
 				num_bytes--;
