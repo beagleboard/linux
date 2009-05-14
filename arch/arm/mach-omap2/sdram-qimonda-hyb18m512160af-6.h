c82881eac1c6999abd768c548ad196c1973d3a55
Paul Walmsley <paul@pwsan.com>
2009-05-14 13:36:45 -0700

    [v2, 2/2] OMAP3 SDRC: Add rounded rates for devices using the Qimonda
    The 3430SDPs, many of which use Qimonda SDRAM, are finally using
    bootloaders that program rounded rates for DPLL3.  Since no SDRAM
    memory timings are defined for the rounded rates, the initial SDRC
    reprogram during init fails.  Add in the correct timings here.
    
    Problem reported by Kevin Hilman <khilman@deeprootsystems.com>.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Tested-by: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/sdram-qimonda-hyb18m512160af-6.h
+++ arch/arm/mach-omap2/sdram-qimonda-hyb18m512160af-6.h
@@ -19,20 +19,34 @@
 /* Qimonda HYB18M512160AF-6 */
 static struct omap_sdrc_params hyb18m512160af6_sdrc_params[] = {
 	[0] = {
-		.rate	     = 165941176,
+		.rate	     = 166000000,
 		.actim_ctrla = 0x629db4c6,
 		.actim_ctrlb = 0x00012214,
 		.rfr_ctrl    = 0x0004dc01,
 		.mr	     = 0x00000032,
 	},
 	[1] = {
+		.rate	     = 165941176,
+		.actim_ctrla = 0x629db4c6,
+		.actim_ctrlb = 0x00012214,
+		.rfr_ctrl    = 0x0004dc01,
+		.mr	     = 0x00000032,
+	},
+	[2] = {
+		.rate	     = 83000000,
+		.actim_ctrla = 0x31512283,
+		.actim_ctrlb = 0x0001220a,
+		.rfr_ctrl    = 0x00025501,
+		.mr	     = 0x00000022,
+	},
+	[3] = {
 		.rate	     = 82970588,
 		.actim_ctrla = 0x31512283,
 		.actim_ctrlb = 0x0001220a,
 		.rfr_ctrl    = 0x00025501,
 		.mr	     = 0x00000022,
 	},
-	[2] = {
+	[4] = {
 		.rate	     = 0
 	},
 };
