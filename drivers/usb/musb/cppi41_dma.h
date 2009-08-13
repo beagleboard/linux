6515a8746667b75528936a4cac1f0debfba7116a
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-08-13 11:11:35 +0530

    musb: cppi41: Remove platform definitions from cppi41_dma.h
    cppi41_dma.h should have common definitions as it would be used
    by multiple platforms. As different platform can have different
    register layouts so it's better to remove these definitions and
    let them come from platform file at plat/usb.h
    
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- drivers/usb/musb/cppi41_dma.h
+++ drivers/usb/musb/cppi41_dma.h
@@ -19,63 +19,7 @@
 
 #ifndef _CPPI41_DMA_H_
 #define _CPPI41_DMA_H_
-
-/* USB 2.0 OTG module registers */
-#define USB_REVISION_REG	0x00
-#define USB_CTRL_REG		0x04
-#define USB_STAT_REG		0x08
-#define USB_EMULATION_REG	0x08
-#define USB_MODE_REG		0x10	/* Transparent, CDC, [Generic] RNDIS */
-#define USB_AUTOREQ_REG		0x14
-#define USB_SRP_FIX_TIME_REG	0x18
-#define USB_TEARDOWN_REG	0x1c
-#define USB_INTR_SRC_REG	0x20
-#define USB_INTR_SRC_SET_REG	0x24
-#define USB_INTR_SRC_CLEAR_REG	0x28
-#define USB_INTR_MASK_REG	0x2c
-#define USB_INTR_MASK_SET_REG	0x30
-#define USB_INTR_MASK_CLEAR_REG 0x34
-#define USB_INTR_SRC_MASKED_REG	0x38
-#define USB_END_OF_INTR_REG	0x3c
-#define USB_GENERIC_RNDIS_EP_SIZE_REG(n) (0x50 + (((n) - 1) << 2))
-
-/* Control register bits */
-#define USB_SOFT_RESET_MASK	1
-
-/* Mode register bits */
-#define USB_RX_MODE_SHIFT(n)	(16 + (((n) - 1) << 2))
-#define USB_RX_MODE_MASK(n)	(3 << USB_RX_MODE_SHIFT(n))
-#define USB_TX_MODE_SHIFT(n)	((((n) - 1) << 2))
-#define USB_TX_MODE_MASK(n)	(3 << USB_TX_MODE_SHIFT(n))
-#define USB_TRANSPARENT_MODE	0
-#define USB_RNDIS_MODE		1
-#define USB_CDC_MODE		2
-#define USB_GENERIC_RNDIS_MODE	3
-
-/* AutoReq register bits */
-#define USB_RX_AUTOREQ_SHIFT(n) (((n) - 1) << 1)
-#define USB_RX_AUTOREQ_MASK(n)	(3 << USB_RX_AUTOREQ_SHIFT(n))
-#define USB_NO_AUTOREQ		0
-#define USB_AUTOREQ_ALL_BUT_EOP 1
-#define USB_AUTOREQ_ALWAYS	3
-
-/* Teardown register bits */
-#define USB_TX_TDOWN_SHIFT(n)	(16 + (n))
-#define USB_TX_TDOWN_MASK(n)	(1 << USB_TX_TDOWN_SHIFT(n))
-#define USB_RX_TDOWN_SHIFT(n)	(n)
-#define USB_RX_TDOWN_MASK(n)	(1 << USB_RX_TDOWN_SHIFT(n))
-
-/* USB interrupt register bits */
-#define USB_INTR_USB_SHIFT	16
-#define USB_INTR_USB_MASK	(0x1ff << USB_INTR_USB_SHIFT) /* 8 Mentor */
-					/* interrupts and DRVVBUS interrupt */
-#define USB_INTR_DRVVBUS	0x100
-#define USB_INTR_RX_SHIFT	8
-#define USB_INTR_TX_SHIFT	0
-
-#define USB_MENTOR_CORE_OFFSET	0x400
-
-#define USB_CPPI41_NUM_CH	4
+#include <plat/usb.h>
 
 /**
  * struct usb_cppi41_info - CPPI 4.1 USB implementation details
