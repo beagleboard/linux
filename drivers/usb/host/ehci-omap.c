28c57be745113b0828c35ffce25f9d11e39563e7
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-10-12 09:17:14 -0700

    ehci: update driver with generic change
    Update the OMAP EHCI driver in accordance with below patch
    introduced in generic EHCI driver.
    
    commit: b18ffd49e86102a9ed0a1cc83fdafe3891e844e5
    USB: EHCI: update toggle state for linked QHs
    
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

--- drivers/usb/host/ehci-omap.c
+++ drivers/usb/host/ehci-omap.c
@@ -738,6 +738,7 @@ static const struct hc_driver ehci_omap_hc_driver = {
 	.urb_enqueue		= ehci_urb_enqueue,
 	.urb_dequeue		= ehci_urb_dequeue,
 	.endpoint_disable	= ehci_endpoint_disable,
+	.endpoint_reset		= ehci_endpoint_reset,
 
 	/*
 	 * scheduling support
