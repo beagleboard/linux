f746c048052046009eafd6f71041204967e231a3
Felipe Balbi <me@felipebalbi.com>
2010-05-01 18:01:09 +0000

    cbus: fix compilation with current mainline
    commit 5a0e3ad6af8660be21ca98a971cd00f331318c05 broke
    compilation of the retu-pwrbutton driver when it dropped
    implicit inclusion of slab.h and gfp.h.
    
    Fix it by including slab.h on retu-pwrbutton.c, while at
    that, also include slab.h on retu-rtc.c to avoid later
    problems with implicit inclusion of that being dropped.
    
    Compile tested with n770_defconfig and n8x0_defconfig
    
    Signed-off-by: Felipe Balbi <me@felipebalbi.com>
    Acked-by: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/cbus/retu-rtc.c
+++ drivers/cbus/retu-rtc.c
@@ -36,6 +36,7 @@
 #include <linux/device.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/completion.h>
 #include <linux/platform_device.h>
