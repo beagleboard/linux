a248c13619634d8ca222afbf7a327c1ce9a292a6
Russell King <rmk+kernel@arm.linux.org.uk>
2010-07-02 13:34:38 +0300

    ARM: initial LMB trial
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/include/asm/lmb.h
@@ -0,0 +1,15 @@
+#ifndef _ASM_ARM_LMB_H
+#define _ASM_ARM_LMB_H
+
+#ifdef CONFIG_MMU
+extern phys_addr_t lowmem_end_addr;
+#define LMB_REAL_LIMIT	lowmem_end_addr
+#else
+#define LMB_REAL_LIMIT	0
+#endif
+
+struct meminfo;
+
+extern void arm_lmb_init(struct meminfo *);
+
+#endif
