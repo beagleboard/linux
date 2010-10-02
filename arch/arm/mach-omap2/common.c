e1db73912a2f1855926a81394bfc8b987617c11e
Tony Lindgren <tony@atomide.com>
2010-10-01 17:23:48 -0700

    omap: Fix build after merging in hwmod support for testing
    I've merged in the hwmod support from Kevin's tree for omap2+ machines,
    so you need to update your serial port names to use ttyO instead of ttyS:
    
    - u-boot bootargs for kernel cmdline needs to change with setenv
    - if you use hardcoded CONFIG_CMDLINE, change it in your .config
    - /etc/inittab in your omap root file system if using serial console
    
    This should not affect omap1 machines. Please test and post issues
    to the linux-omap mailing list!
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/common.c
+++ arch/arm/mach-omap2/common.c
@@ -36,7 +36,6 @@ static void __init __omap2_set_globals(struct omap_globals *omap2_globals)
 	omap2_set_globals_sdrc(omap2_globals);
 	omap2_set_globals_control(omap2_globals);
 	omap2_set_globals_prcm(omap2_globals);
-	omap2_set_globals_uart(omap2_globals);
 }
 
 #endif
@@ -131,7 +130,6 @@ void __init omap2_set_globals_443x(void)
 	omap2_set_globals_tap(&omap4_globals);
 	omap2_set_globals_control(&omap4_globals);
 	omap2_set_globals_prcm(&omap4_globals);
-	omap2_set_globals_uart(&omap4_globals);
 }
 #endif
 
