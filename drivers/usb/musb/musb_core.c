da1e63dae2b8874f4c7fd13bdac63062855d160a
Felipe Balbi <balbi@ti.com>
2010-12-02 13:17:53 +0200

    usb: musb: drop musb_platform_suspend/resume
    all glue layers are now fully moved to the
    new setup. We are now using dev_pm_ops to
    implement suspend/resume functionality and
    thus, musb_platform_suspend/resume has become
    deprecated and useless.
    
    This patch drops those function pointers and
    its uses.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2307,8 +2307,6 @@ static void musb_save_context(struct musb *musb)
 				musb_read_rxhubport(musb_base, i);
 		}
 	}
-
-	musb_platform_suspend(musb);
 }
 
 static void musb_restore_context(struct musb *musb)
@@ -2317,8 +2315,6 @@ static void musb_restore_context(struct musb *musb)
 	void __iomem *musb_base = musb->mregs;
 	void __iomem *ep_target_regs;
 	void __iomem *epio;
-
-	musb_platform_resume(musb);
 
 	if (is_host_enabled(musb)) {
 		musb_writew(musb_base, MUSB_FRAME, musb->context.frame);
