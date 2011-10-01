6432725f7376f326bd94ace383a7ddd6ddbb3956
Vaibhav Bedia <vaibhav.bedia@ti.com>
2011-10-01 16:59:00 +0530

    ASoC: Davinci: Correct the rotation for different data-types
    During audio Rx none of the data-types need rotation in the
    Receive Format Unit of the McASP
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

--- sound/soc/davinci/davinci-mcasp.c
+++ sound/soc/davinci/davinci-mcasp.c
@@ -599,7 +599,7 @@ static int davinci_config_channel_size(struct davinci_audio_dev *dev,
 					TXSSZ(fmt), TXSSZ(0x0F));
 	mcasp_mod_bits(dev->base + DAVINCI_MCASP_TXFMT_REG, TXROT(rotate),
 							TXROT(7));
-	mcasp_mod_bits(dev->base + DAVINCI_MCASP_RXFMT_REG, RXROT(rotate),
+	mcasp_mod_bits(dev->base + DAVINCI_MCASP_RXFMT_REG, RXROT(0),
 							RXROT(7));
 	mcasp_set_reg(dev->base + DAVINCI_MCASP_TXMASK_REG, mask);
 	mcasp_set_reg(dev->base + DAVINCI_MCASP_RXMASK_REG, mask);
