850877b3250c560d18e7bea4763f4739f800be31
Afzal Mohammed <afzal@ti.com>
2011-10-04 15:36:25 +0530

    arm:omap:am33xx: board_init pin mux initialization
    Configure I2C0 pin mux in board init, required for
    board detection (read eeprom). Also add function
    for dynamic pin mux.
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- arch/arm/mach-omap2/board-am335xevm.c
+++ arch/arm/mach-omap2/board-am335xevm.c
@@ -14,6 +14,7 @@
  */
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/i2c.h>
 
 #include <mach/hardware.h>
 #include <asm/mach-types.h>
@@ -26,11 +27,45 @@
 
 #include "common.h"
 
+#include "mux.h"
+
+#ifdef CONFIG_OMAP_MUX
+static struct omap_board_mux board_mux[] __initdata = {
+	AM33XX_MUX(I2C0_SDA, OMAP_MUX_MODE0 | AM33XX_SLEWCTRL_SLOW |
+			AM33XX_INPUT_EN | AM33XX_PIN_OUTPUT),
+	AM33XX_MUX(I2C0_SCL, OMAP_MUX_MODE0 | AM33XX_SLEWCTRL_SLOW |
+			AM33XX_INPUT_EN | AM33XX_PIN_OUTPUT),
+	{ .reg_offset = OMAP_MUX_TERMINATOR },
+};
+#else
+#define	board_mux	NULL
+#endif
+
+/* module pin mux structure */
+struct pinmux_config {
+	const char *string_name; /* signal name format */
+	int val; /* Options for the mux register value */
+};
+
+/*
+* @pin_mux - single module pin-mux structure which defines pin-mux
+*			details for all its pins.
+*/
+static void setup_pin_mux(struct pinmux_config *pin_mux)
+{
+	int i;
+
+	for (i = 0; pin_mux->string_name != NULL; pin_mux++)
+		omap_mux_init_signal(pin_mux->string_name, pin_mux->val);
+
+}
+
 static struct omap_board_config_kernel am335x_evm_config[] __initdata = {
 };
 
 static void __init am335x_evm_init(void)
 {
+	am33xx_mux_init(board_mux);
 	omap_serial_init();
 	omap_sdrc_init(NULL, NULL);
 	omap_board_config = am335x_evm_config;
