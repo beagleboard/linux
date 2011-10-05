6b26b2dd0cc386f69180c785949051b0199aa509
Thomas Gleixner <tglx@linutronix.de>
2011-10-05 11:59:38 -0700

    rcu: Merge RCU-bh into RCU-preempt
    The Linux kernel has long RCU-bh read-side critical sections that
    intolerably increase scheduling latency under mainline's RCU-bh rules,
    which include RCU-bh read-side critical sections being non-preemptible.
    This patch therefore arranges for RCU-bh to be implemented in terms of
    RCU-preempt for CONFIG_PREEMPT_RT_FULL=y.
    
    This has the downside of defeating the purpose of RCU-bh, namely,
    handling the case where the system is subjected to a network-based
    denial-of-service attack that keeps at least one CPU doing full-time
    softirq processing.  This issue will be fixed by a later commit.
    
    The current commit will need some work to make it appropriate for
    mainline use, for example, it needs to be extended to cover Tiny RCU.
    
    [ paulmck: Added a useful changelog ]
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20111005185938.GA20403@linux.vnet.ibm.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -167,6 +167,9 @@ void call_rcu(struct rcu_head *head,
 
 #endif /* #else #ifdef CONFIG_PREEMPT_RCU */
 
+#ifdef CONFIG_PREEMPT_RT_FULL
+#define call_rcu_bh	call_rcu
+#else
 /**
  * call_rcu_bh() - Queue an RCU for invocation after a quicker grace period.
  * @head: structure to be used for queueing the RCU updates.
@@ -190,6 +193,7 @@ void call_rcu(struct rcu_head *head,
  */
 void call_rcu_bh(struct rcu_head *head,
 		 void (*func)(struct rcu_head *head));
+#endif
 
 /**
  * call_rcu_sched() - Queue an RCU for invocation after sched grace period.
@@ -296,7 +300,13 @@ static inline int rcu_preempt_depth(void)
 void rcu_init(void);
 void rcu_end_inkernel_boot(void);
 void rcu_sched_qs(void);
+
+#ifdef CONFIG_PREEMPT_RT_FULL
+static inline void rcu_bh_qs(void) { }
+#else
 void rcu_bh_qs(void);
+#endif
+
 void rcu_check_callbacks(int user);
 struct notifier_block;
 void rcu_idle_enter(void);
@@ -470,7 +480,14 @@ extern struct lockdep_map rcu_callback_map;
 int debug_lockdep_rcu_enabled(void);
 
 int rcu_read_lock_held(void);
+#ifdef CONFIG_PREEMPT_RT_FULL
+static inline int rcu_read_lock_bh_held(void)
+{
+	return rcu_read_lock_held();
+}
+#else
 int rcu_read_lock_bh_held(void);
+#endif
 
 /**
  * rcu_read_lock_sched_held() - might we be in RCU-sched read-side critical section?
@@ -997,10 +1014,14 @@ static inline void rcu_read_unlock(void)
 static inline void rcu_read_lock_bh(void)
 {
 	local_bh_disable();
+#ifdef CONFIG_PREEMPT_RT_FULL
+	rcu_read_lock();
+#else
 	__acquire(RCU_BH);
 	rcu_lock_acquire(&rcu_bh_lock_map);
 	rcu_lockdep_assert(rcu_is_watching(),
 			   "rcu_read_lock_bh() used illegally while idle");
+#endif
 }
 
 /*
@@ -1010,10 +1031,14 @@ static inline void rcu_read_lock_bh(void)
  */
 static inline void rcu_read_unlock_bh(void)
 {
+#ifdef CONFIG_PREEMPT_RT_FULL
+	rcu_read_unlock();
+#else
 	rcu_lockdep_assert(rcu_is_watching(),
 			   "rcu_read_unlock_bh() used illegally while idle");
 	rcu_lock_release(&rcu_bh_lock_map);
 	__release(RCU_BH);
+#endif
 	local_bh_enable();
 }
 
