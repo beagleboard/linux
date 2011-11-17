45e620fff10480deea221dfe8bea3d19ceec6831
Philip, Avinash <avinashphilip@ti.com>
2011-11-17 20:13:56 +0530

    MTD: omap2: 14 byte ECC support for BCH8
    14 bytes of ECC is provided for every 512 byte even though 13 byte is the
    actual requirement. This is to synchronize the ECC layout with U-boot.
    Extra byte is cleared to 0.
    
    Signed-off-by: Philip, Avinash <avinashphilip@ti.com>
    Signed-off-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- drivers/mtd/nand/omap2.c
+++ drivers/mtd/nand/omap2.c
@@ -100,8 +100,8 @@
 #define JFFS2_CLEAN_MARKER_OFFSET  0x2
 
 #define BCH_ECC_POS			0x2
-#define BCH_JFFS2_CLEAN_MARKER_OFFSET	0x36
-#define OMAP_BCH8_ECC_SECT_BYTES	13
+#define BCH_JFFS2_CLEAN_MARKER_OFFSET	0x3a
+#define OMAP_BCH8_ECC_SECT_BYTES	14
 
 /* oob info generated runtime depending on ecc algorithm and layout selected */
 static struct nand_ecclayout omap_oobinfo;
@@ -815,6 +815,8 @@ static int omap_read_page_bch(struct mtd_info *mtd, struct nand_chip *chip,
 	uint32_t data_pos;
 	uint32_t oob_pos;
 
+	struct omap_nand_info *info = container_of(mtd, struct omap_nand_info,
+							mtd);
 	data_pos = 0;
 	/* oob area start */
 	oob_pos = (eccsize * eccsteps) + chip->ecc.layout->eccpos[0];
@@ -828,7 +830,11 @@ static int omap_read_page_bch(struct mtd_info *mtd, struct nand_chip *chip,
 
 		/* read respective ecc from oob area */
 		chip->cmdfunc(mtd, NAND_CMD_RNDOUT, oob_pos, page);
-		chip->read_buf(mtd, oob, eccbytes);
+
+		if (info->ecc_opt == OMAP_ECC_BCH8_CODE_HW)
+			chip->read_buf(mtd, oob, 13);
+		else
+			chip->read_buf(mtd, oob, eccbytes);
 		/* read syndrome */
 		chip->ecc.calculate(mtd, p, &ecc_calc[i]);
 
