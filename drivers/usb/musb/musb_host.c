a866e3278835e0390506033e626455eb1beb77ae
Ajay Kumar Gupta <ajay.gupta@ti.com>
2011-11-18 12:26:02 +0530

    usb: musb: cppi41: Workaround fix for Tx fifo flush during disconnect
    Unable to flush musb Tx endpoint fifo during disconnect while i/o
    in progress. The workaround is to set only fifoflush bit and clear
    other bits in tx-csr register.
    
    Signed-off-by: Ravi B <ravibabu@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -141,9 +141,11 @@ static void musb_h_tx_flush_fifo(struct musb_hw_ep *ep)
 		if (csr != lastcsr)
 			dev_dbg(musb->controller, "Host TX FIFONOTEMPTY csr: %02x\n", csr);
 		lastcsr = csr;
-		csr |= MUSB_TXCSR_FLUSHFIFO;
+		csr = MUSB_TXCSR_FLUSHFIFO;
 		musb_writew(epio, MUSB_TXCSR, csr);
 		csr = musb_readw(epio, MUSB_TXCSR);
+		if (!(csr & MUSB_TXCSR_FIFONOTEMPTY))
+			break;
 		if (retries-- < 1) {
 			dev_dbg(musb->controller, "Could not flush host TX%d fifo: csr: %04x\n",
 				ep->epnum, csr);
