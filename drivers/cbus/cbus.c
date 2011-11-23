eff809eb99f3ff17b5e0f320a2d8bda393f9017f
Felipe Balbi <balbi@ti.com>
2011-11-23 16:00:43 +0200

    cbus: fix compile breakage
    we need to include <linux/export.h> and <linux/module.h>
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/cbus/cbus.c
+++ drivers/cbus/cbus.c
@@ -26,6 +26,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/export.h>
+#include <linux/module.h>
 #include <linux/device.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
