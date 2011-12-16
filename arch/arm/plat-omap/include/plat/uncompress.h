2ccf6b638396348af2cc4aed0682cef2a50e64d4
Oleg Drokin <green@linuxhacker.ru>
2011-12-16 14:01:51 -0800

    Initial B&N Nook Color (encore) support
    Bare-bones board file, comes with serial console, gpio keys,
    MMC/SDCard and USB (peripheral) support.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    [tony@atomide.com: updated for irq and timer cleanup and to select the board by default]
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -157,6 +157,7 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 		DEBUG_LL_OMAP3(3, cm_t3730);
 		DEBUG_LL_OMAP3(3, craneboard);
 		DEBUG_LL_OMAP3(3, devkit8000);
+		DEBUG_LL_OMAP3(3, encore);
 		DEBUG_LL_OMAP3(3, igep0020);
 		DEBUG_LL_OMAP3(3, igep0030);
 		DEBUG_LL_OMAP3(3, nokia_rm680);
