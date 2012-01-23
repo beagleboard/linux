Sabayon/kernel

arm:omap:am33xx: Board support for AM335X IA EVM
UART3 is used as boot console for IA EVM.
To capture decompressor logs & early prints,
a different mach-id is required and so a new
board support. It has machine specific entry
points same as that of AM335XEVM.

Signed-off-by: Afzal Mohammed <afzal@ti.com>
Signed-off-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>

arch/arm/mach-omap2/board-am335xevm.c
@@ -1380,3 +1380,13 @@ MACHINE_START(AM335XEVM, "am335xevm")
 	.timer		= &omap3_am33xx_timer,
 	.init_machine	= am335x_evm_init,
 MACHINE_END
+
+MACHINE_START(AM335XIAEVM, "am335xiaevm")
+	/* Maintainer: Texas Instruments */
+	.atag_offset	= 0x100,
+	.map_io		= am335x_evm_map_io,
+	.init_irq	= ti816x_init_irq,
+	.init_early	= am335x_init_early,
+	.timer		= &omap3_am33xx_timer,
+	.init_machine	= am335x_evm_init,
+MACHINE_END

beagleboard/linux

Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

arch/arm/mach-omap2/board-am335xevm.c
@@ -1382,3 +1382,13 @@ MACHINE_START(AM335XEVM, "am335xevm")
 	.timer		= &omap3_am33xx_timer,
 	.init_machine	= am335x_evm_init,
 MACHINE_END
+
+MACHINE_START(AM335XIAEVM, "am335xiaevm")
+	/* Maintainer: Texas Instruments */
+	.atag_offset	= 0x100,
+	.map_io		= am335x_evm_map_io,
+	.init_irq	= ti81xx_init_irq,
+	.init_early	= am33xx_init_early,
+	.timer		= &omap3_am33xx_timer,
+	.init_machine	= am335x_evm_init,
+MACHINE_END