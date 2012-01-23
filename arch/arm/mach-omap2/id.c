Sabayon/kernel

arm:omap:am33xx: SoC type detection
Determine SoC type

Perhaps this can be merged with,144d6cf,
arm:omap:am33xx: Update common OMAP machine specific sources

Signed-off-by: Afzal Mohammed <afzal@ti.com>

arch/arm/mach-omap2/id.c
@@ -44,6 +44,8 @@ int omap_type(void)
 
 	if (cpu_is_omap24xx()) {
 		val = omap_ctrl_readl(OMAP24XX_CONTROL_STATUS);
+	} else if (cpu_is_am33xx()) {
+		val = omap_ctrl_readl(AM33XX_CONTROL_STATUS);
 	} else if (cpu_is_omap34xx()) {
 		val = omap_ctrl_readl(OMAP343X_CONTROL_STATUS);
 	} else if (cpu_is_omap44xx()) {

beagleboard/linux

Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

arch/arm/mach-omap2/id.c
@@ -44,6 +44,8 @@ int omap_type(void)
 
 	if (cpu_is_omap24xx()) {
 		val = omap_ctrl_readl(OMAP24XX_CONTROL_STATUS);
+	} else if (cpu_is_am33xx()) {
+		val = omap_ctrl_readl(AM33XX_CONTROL_STATUS_OFF);
 	} else if (cpu_is_omap34xx()) {
 		val = omap_ctrl_readl(OMAP343X_CONTROL_STATUS);
 	} else if (cpu_is_omap44xx()) {