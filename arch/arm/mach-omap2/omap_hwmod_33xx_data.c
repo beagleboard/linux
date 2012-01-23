0e82a6b311ae038dc03f5291c93b42747762dbd7
Patil, Rachna <rachna@ti.com>
2012-01-23 16:56:59 +0530

    arm: omap: am33xx: update TSC hwmod data
    Sysconfig register bits are updated for TSC
    hwmod. TSC supports idle mode at bit position 2&3,
    hence following omap4 IP's.
    
    Signed-off-by: Patil, Rachna <rachna@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_33xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_33xx_data.c
@@ -277,8 +277,18 @@ static struct omap_hwmod am33xx_l4wkup_hwmod = {
 };
 
 /* 'adc_tsc' class */
+static struct omap_hwmod_class_sysconfig am33xx_adc_tsc_sysc = {
+	.rev_offs	= 0x00,
+	.sysc_offs	= 0x10,
+	.sysc_flags	= SYSC_HAS_SIDLEMODE,
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO |
+				SIDLE_SMART | SIDLE_SMART_WKUP),
+	.sysc_fields	= &omap_hwmod_sysc_type2,
+};
+
 static struct omap_hwmod_class am33xx_adc_tsc_hwmod_class = {
 	.name		= "adc_tsc",
+	.sysc		= &am33xx_adc_tsc_sysc,
 };
 
 /* adc_tsc */
