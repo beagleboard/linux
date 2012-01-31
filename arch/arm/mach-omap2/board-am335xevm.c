23e8f8dbc5a878210b4f23633cc3f36ce6a89891
Koen Kooi <koen@dominion.thruhere.net>
2012-01-31 10:09:06 +0100

    beaglebone: disable tsadc
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- arch/arm/mach-omap2/board-am335xevm.c
+++ arch/arm/mach-omap2/board-am335xevm.c
@@ -242,11 +242,11 @@ struct da8xx_lcdc_platform_data dvi_pdata = {
 static struct tsc_data am335x_touchscreen_data  = {
 	.wires  = 4,
 	.x_plate_resistance = 200,
-	.mode = TI_TSCADC_TSCMODE,
+//	.mode = TI_TSCADC_TSCMODE,
 };
 
 static struct tsc_data bone_touchscreen_data  = {
-	.mode = TI_TSCADC_GENMODE,
+//	.mode = TI_TSCADC_GENMODE,
 };
 
 static u8 am335x_iis_serializer_direction1[] = {
