1513426f6f10d317f466e9347f1894df8925c1f4
Ohad Ben-Cohen <ohad@wizery.com>
2012-11-04 12:08:21 +0200

    rpmsg: add a description field
    Add a new description field to the rpmsg bus infrastructure
    that can be passed onto the rpmsg client drivers for additional
    information. The current rpmsg bus client drivers need to have
    a fixed id_table for proper matching, this new field can allow
    flexibility for the client drivers (eg: like creating unique
    cdevs).
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- include/linux/rpmsg.h
+++ include/linux/rpmsg.h
@@ -66,7 +66,8 @@ struct rpmsg_hdr {
 
 /**
  * struct rpmsg_ns_msg - dynamic name service announcement message
- * @name: name of remote service that is published
+ * @name: name of remote service that is being published
+ * @desc: description of remote service
  * @addr: address of remote service that is published
  * @flags: indicates whether service is created or destroyed
  *
@@ -78,6 +79,7 @@ struct rpmsg_hdr {
  */
 struct rpmsg_ns_msg {
 	char name[RPMSG_NAME_SIZE];
+	char desc[RPMSG_NAME_SIZE];
 	u32 addr;
 	u32 flags;
 } __packed;
@@ -102,6 +104,7 @@ struct virtproc_info;
  * @vrp: the remote processor this channel belongs to
  * @dev: the device struct
  * @id: device id (used to match between rpmsg drivers and devices)
+ * @desc: description of remote service
  * @src: local address
  * @dst: destination address
  * @ept: the rpmsg endpoint of this channel
@@ -111,6 +114,7 @@ struct rpmsg_channel {
 	struct virtproc_info *vrp;
 	struct device dev;
 	struct rpmsg_device_id id;
+	char desc[RPMSG_NAME_SIZE];
 	u32 src;
 	u32 dst;
 	struct rpmsg_endpoint *ept;
