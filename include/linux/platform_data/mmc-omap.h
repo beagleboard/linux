8dceedc2d7ad38bdb5f970df4df0609c325ac747
Pantelis Antoniou <panto@antoniou-consulting.com>
2012-11-30 12:18:16 +0200

    omap_hsmmc: Bug fixes, pinctl & gpio-reset
    Cleanup omapmmc & add gpio-reset.
    
    Signed-off-by: Pantelis Antoniou <panto@antoniou-consulting.com>
    
    Conflicts:
    	drivers/mmc/host/omap_hsmmc.c

--- include/linux/platform_data/mmc-omap.h
+++ include/linux/platform_data/mmc-omap.h
@@ -115,6 +115,9 @@ struct omap_mmc_platform_data {
 
 		int switch_pin;			/* gpio (card detect) */
 		int gpio_wp;			/* gpio (write protect) */
+		int gpio_reset;			/* gpio (reset) */
+		int gpio_reset_active_low;	/* 1 if reset is active low */
+		u32 gpio_reset_hold_us;		/* time to hold in us */
 
 		int (*set_bus_mode)(struct device *dev, int slot, int bus_mode);
 		int (*set_power)(struct device *dev, int slot,
