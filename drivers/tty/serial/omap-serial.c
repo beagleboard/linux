54770062efe65bb9be02b4c6e22bd08a7e8cc2de
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2012-12-06 09:45:04 +0100

    serial: omap: add the functionality of a 9-bit UART with userspaces CMSPAR
    Some systems require the additional communication functionality of a
    9-bit UART. For that we could use the "stick" (mark/space) parity
    bit supported on omap serial device. When is set, if PARODD is set the
    parity bit is always 1; if PARODD is not set, then the parity bit is
    always 0.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@iseebcn.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/omap-serial.c
+++ drivers/tty/serial/omap-serial.c
@@ -835,6 +835,8 @@ serial_omap_set_termios(struct uart_port *port, struct ktermios *termios,
 		cval |= UART_LCR_PARITY;
 	if (!(termios->c_cflag & PARODD))
 		cval |= UART_LCR_EPAR;
+	if (termios->c_cflag & CMSPAR)
+		cval |= UART_LCR_SPAR;
 
 	/*
 	 * Ask the core to calculate the divisor for us.
