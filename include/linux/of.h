d15beae78781f52659884e0a773a91318ce3341e
Pantelis Antoniou <panto@antoniou-consulting.com>
2013-01-03 11:46:39 +0200

    OF: export of_property_notify
    of_property_notify can be utilized by other users too, export it.
    
    Signed-off-by: Pantelis Antoniou <panto@antoniou-consulting.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -307,6 +307,17 @@ extern int of_parse_phandle_with_fixed_args(const struct device_node *np,
 extern int of_count_phandle_with_args(const struct device_node *np,
 	const char *list_name, const char *cells_name);
 
+#if defined(CONFIG_OF_DYNAMIC)
+extern int of_property_notify(int action, struct device_node *np,
+			      struct property *prop);
+#else
+static inline int of_property_notify(int action, struct device_node *np,
+			      struct property *prop)
+{
+	return 0;
+}
+#endif
+
 extern void of_alias_scan(void * (*dt_alloc)(u64 size, u64 align));
 extern int of_alias_get_id(struct device_node *np, const char *stem);
 
