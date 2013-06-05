a9c410fd4d91c2e9425ae62a94d1ec0904ad4b28
Pantelis Antoniou <panto@antoniou-consulting.com>
2013-06-05 19:52:25 +0300

    WIP of new tda998x patches.
    Video works, but no audio. RMK uses SPDIF so I2S is missing various bits.
    With the tilcdc driver the some non CEC modes are shifted too.
    
    Signed-off-by: Pantelis Antoniou <panto@antoniou-consulting.com>

--- include/drm/i2c/tda998x.h
+++ include/drm/i2c/tda998x.h
@@ -1,23 +1,26 @@
 #ifndef __TDA998X_H__
 #define __TDA998X_H__
 
+#include <linux/types.h>
+
 enum tda998x_audio_format {
 	AFMT_I2S,
 	AFMT_SPDIF,
 };
 
 struct tda998x_encoder_params {
-	int audio_cfg;
-	int audio_clk_cfg;
+	uint8_t audio_cfg;
+	uint8_t audio_clk_cfg;
 	enum tda998x_audio_format audio_format;
 	int audio_sample_rate;
-	char audio_frame[6];
-	int swap_a, mirr_a;
-	int swap_b, mirr_b;
-	int swap_c, mirr_c;
-	int swap_d, mirr_d;
-	int swap_e, mirr_e;
-	int swap_f, mirr_f;
+	uint8_t audio_frame[6];
+	uint8_t swap_a, mirr_a;
+	uint8_t swap_b, mirr_b;
+	uint8_t swap_c, mirr_c;
+	uint8_t swap_d, mirr_d;
+	uint8_t swap_e, mirr_e;
+	uint8_t swap_f, mirr_f;
+	uint8_t i2s_fmt;
 };
 
 #endif
