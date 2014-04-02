bb94b68eb8cf084aa5e0f310f1523eecb17ef70c
Daniel Mack <zonque@gmail.com>
2014-04-02 13:58:27 +0200

    usb: musb: add reset hook to platform ops
    Babble interrupts require us to reset the DSPS glue layer. In order to
    handle all other recovery tasks independently, add a new hook for
    platform-specific implementations of the actual reset.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_core.h
+++ drivers/usb/musb/musb_core.h
@@ -206,6 +206,7 @@ struct musb_platform_ops {
 
 	int	(*set_mode)(struct musb *musb, u8 mode);
 	void	(*try_idle)(struct musb *musb, unsigned long timeout);
+	void	(*reset)(struct musb *musb);
 
 	int	(*vbus_status)(struct musb *musb);
 	void	(*set_vbus)(struct musb *musb, int on);
@@ -563,6 +564,12 @@ static inline void musb_platform_try_idle(struct musb *musb,
 {
 	if (musb->ops->try_idle)
 		musb->ops->try_idle(musb, timeout);
+}
+
+static inline void musb_platform_reset(struct musb *musb)
+{
+	if (musb->ops->reset)
+		musb->ops->reset(musb);
 }
 
 static inline int musb_platform_get_vbus_status(struct musb *musb)
