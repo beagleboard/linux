31f90db7cc99aacdea28e3fd850a493ea6c5cf7e
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-28 23:13:03 +0200

    Instering Logibone driver into kernel

---
+++ drivers/misc/cape/beaglebone/logibone/generic.h
@@ -0,0 +1,11 @@
+#define DEVICE_NAME "logibone"
+#define DEVICE_NAME_MEM "logibone_mem"
+
+//I2C
+#define I2C_IO_EXP_ADDR	0x24
+
+//FPGA
+#define FPGA_BASE_ADDR	 0x01000000
+#define FPGA_MEM_SIZE	 131072
+
+#define MAX_DMA_TRANSFER_IN_BYTES   (32768)
