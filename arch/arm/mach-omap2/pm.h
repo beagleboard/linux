b45ad743f2377925b881f22b0defe5e3c7e4b5cd
Dave Gerlach <d-gerlach@ti.com>
2014-07-15 11:12:44 -0500

    ARM: OMAP2+: DRA7XX: Add opp7xx_data to enable higher OPPs
    Certain OPPs on DRA7XX are not available for all variants. The available
    OPPs can be determined by reading a die_id register. This patch
    reads the register and enables the appropriate OPPs.
    
    OPP Information from DRA7x Data Manual, SPRS857K, Revised July 2014.
    
    efuse decoded using VayuSpeedGrades.xlsx document, version 0.2, revised
    May 30, 2014.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

--- arch/arm/mach-omap2/pm.h
+++ arch/arm/mach-omap2/pm.h
@@ -40,6 +40,7 @@ extern int omap3_opp_init(void);
 extern int omap4_opp_init(void);
 extern int am33xx_opp_init(void);
 extern int am43xx_opp_init(void);
+extern int dra7xx_opp_init(void);
 #else
 static inline int omap3_opp_init(void)
 {
@@ -54,6 +55,10 @@ static inline int am33xx_opp_init(void)
 	return -EINVAL;
 }
 static inline int am43xx_opp_init(void)
+{
+	return -EINVAL;
+}
+static inline int dra7xx_opp_init(void)
 {
 	return -EINVAL;
 }
