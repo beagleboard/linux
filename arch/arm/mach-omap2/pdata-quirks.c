cf21ec76f6aeeb16524b8aeea41d9b1740c945c9
Suman Anna <s-anna@ti.com>
2014-07-17 18:09:16 -0500

    ARM: OMAP2+: Extend iommu pdata-quirks to DRA7 IPUs & DSP1
    The IOMMUs within the IPU and DSP processor subsystems in
    DRA7xx are very similar to those in OMAP4/OMAP5, so extend
    the OMAP4 iommu pdata quirks for these MMUs as well.
    
    The DSP processor subsystem in DRA7xx, though, has two MMUs,
    one for the processor port and another for an EDMA port. Both
    these MMUs share a common reset line, the MMU on the EDMA
    port will always be mirror-programmed alongside the primary
    MMU, so the pdata quirks for reset is only added for the MMU
    associated with the processor port.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -382,7 +382,8 @@ static void __init omap3_pandora_legacy_init(void)
 }
 #endif /* CONFIG_ARCH_OMAP3 */
 
-#if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5)
+#if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5) || \
+	defined(CONFIG_SOC_DRA7XX)
 static struct iommu_platform_data omap4_iommu_pdata = {
 	.reset_name = "mmu_cache",
 	.assert_reset = omap_device_assert_hardreset,
@@ -474,6 +475,12 @@ static struct of_dev_auxdata omap_auxdata_lookup[] __initdata = {
 #endif
 #ifdef CONFIG_SOC_DRA7XX
 	OF_DEV_AUXDATA("ti,dra7-padconf", 0x4a003400, "4a003400.pinmux", &pcs_pdata),
+	OF_DEV_AUXDATA("ti,dra7-dsp-iommu", 0x40d01000, "40d01000.mmu",
+		       &omap4_iommu_pdata),
+	OF_DEV_AUXDATA("ti,dra7-iommu", 0x55082000, "55082000.mmu",
+		       &omap4_iommu_pdata),
+	OF_DEV_AUXDATA("ti,dra7-iommu", 0x58882000, "58882000.mmu",
+		       &omap4_iommu_pdata),
 #endif
 #ifdef CONFIG_SOC_AM43XX
 	OF_DEV_AUXDATA("ti,am437-padconf", 0x44e10800, "44e10800.pinmux", &pcs_pdata),
