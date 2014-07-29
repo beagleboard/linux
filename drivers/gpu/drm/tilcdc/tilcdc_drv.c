b840cf551a4316334e67bae9e7ec89aadb4aed32
Dave Gerlach <d-gerlach@ti.com>
2014-07-29 06:27:58 +0000

    drm/tilcdc: adopt pinctrl support
    Update tilcdc driver to set the state of the pins to:
    - "default on resume
    - "sleep" on suspend
    
    By optionally putting the pins into sleep state in the suspend callback
    we can accomplish two things.
    - minimize current leakage from pins and thus save power,
    - prevent the IP from driving pins output in an uncontrolled manner,
    which may happen if the power domain drops the domain regulator.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Darren Etheridge <detheridge@ti.com>

--- drivers/gpu/drm/tilcdc/tilcdc_drv.c
+++ drivers/gpu/drm/tilcdc/tilcdc_drv.c
@@ -17,6 +17,7 @@
 
 /* LCDC DRM driver, based on da8xx-fb */
 
+#include <linux/pinctrl/consumer.h>
 #include <linux/suspend.h>
 #include "tilcdc_drv.h"
 #include "tilcdc_regs.h"
@@ -584,6 +585,9 @@ static int tilcdc_pm_suspend(struct device *dev)
 		if (registers[i].save && (priv->rev >= registers[i].rev))
 			priv->saved_register[n++] = tilcdc_read(ddev, registers[i].reg);
 
+	/* Select sleep pin state */
+	pinctrl_pm_select_sleep_state(dev);
+
 	return 0;
 }
 
@@ -592,6 +596,9 @@ static int tilcdc_pm_resume(struct device *dev)
 	struct drm_device *ddev = dev_get_drvdata(dev);
 	struct tilcdc_drm_private *priv = ddev->dev_private;
 	unsigned i, n = 0;
+
+	/* Select default pin state */
+	pinctrl_pm_select_default_state(dev);
 
 	/* Restore register state: */
 	for (i = 0; i < ARRAY_SIZE(registers); i++)
