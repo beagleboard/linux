6d0984ae8281c9f51c3416c46a8c04b5035366eb
clayshooter811@gmail.com <clayshooter811@gmail.com>
2014-08-20 17:12:09 -0500

    backport: am335x: ti,omap4-rng: from ti v3.12 bsp
    http://beagleboard.org/Community/Forums?place=msg%2Fbeagleboard%2FU3-4wh9yook%2Fgfc81YXrH6kJ
    
    Signed-off-by: James Johnson <clayshooter811@gmail.com>
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -471,8 +471,10 @@ static void omap_init_rng(void)
 	struct platform_device *pdev;
 
 	oh = omap_hwmod_lookup("rng");
-	if (!oh)
+	if (!oh) {
+		pr_err("Could not lookup rng hwmod\n");
 		return;
+	}
 
 	pdev = omap_device_build("omap_rng", -1, oh, NULL, 0, NULL, 0, 0);
 	WARN(IS_ERR(pdev), "Can't build omap_device for omap_rng\n");
@@ -631,9 +633,9 @@ static int __init omap2_init_devices(void)
 		omap_init_mcspi();
 		omap_init_sham();
 		omap_init_aes();
+		omap_init_rng();
 	}
 	omap_init_sti();
-	omap_init_rng();
 	omap_init_vout();
 	omap_init_ocp2scp();
 	if (soc_is_am33xx()) {
