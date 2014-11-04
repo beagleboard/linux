77aafe0a050e83ebd5f0e4525b7b5d85eec68b54
Suman Anna <s-anna@ti.com>
2014-11-04 14:49:06 -0600

    rpmsg: rpc: fix potential memory leak of unprocessed skbs
    A user thread sends a request for a remote function execution
    on the remote processor through a write() fop. All the responses
    from the remote service are queued using allocated skbs in the
    driver's rpmsg callback. The allocated skbs are processed and
    freed in a read() fop. An error recovery causes a blocked user
    thread to bail out immediately and any in-flight queued skbs
    are left unprocessed. These in-flight skbs are never freed and
    can result in a memory leak.
    
    Fix the memory leak by checking for the presence of any of these
    unprocessed skbs in the read queue, and freeing them during the
    file descriptor's release() function. This also ensures no memory
    is leaked for user applications with bugs and not using matching
    write() and read() fops.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/rpmsg/rpmsg_rpc.c
+++ drivers/rpmsg/rpmsg_rpc.c
@@ -600,6 +600,7 @@ static int rppc_release(struct inode *inode, struct file *filp)
 {
 	struct rppc_instance *rpc = filp->private_data;
 	struct rppc_device *rppcdev = rpc->rppcdev;
+	struct sk_buff *skb = NULL;
 
 	dev_dbg(rppcdev->dev, "releasing Instance %p, in state %d\n", rpc,
 		rpc->state);
@@ -613,6 +614,11 @@ static int rppc_release(struct inode *inode, struct file *filp)
 	}
 
 	rppc_delete_fxns(rpc);
+
+	while (!skb_queue_empty(&rpc->queue)) {
+		skb = skb_dequeue(&rpc->queue);
+		kfree_skb(skb);
+	}
 
 	mutex_lock(&rpc->lock);
 	idr_for_each(&rpc->dma_idr, rppc_free_dmabuf, rpc);
