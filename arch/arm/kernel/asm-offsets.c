0ba870daa9528af54d2484d68c904a24ad97aaf6
Robert Nelson <robertcnelson@gmail.com>
2014-11-11 18:19:30 -0600

    trusty: gcc-4.8 (4.8.2-19ubuntu1) has fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- arch/arm/kernel/asm-offsets.c
+++ arch/arm/kernel/asm-offsets.c
@@ -50,7 +50,7 @@
 #error Your compiler is too buggy; it is known to miscompile kernels.
 #error    Known good compilers: 3.3, 4.x
 #endif
-#if GCC_VERSION >= 40800 && GCC_VERSION < 40803
+#if GCC_VERSION >= 40800 && GCC_VERSION < 40802
 #error Your compiler is too buggy; it is known to miscompile kernels
 #error and result in filesystem corruption and oopses.
 #endif
