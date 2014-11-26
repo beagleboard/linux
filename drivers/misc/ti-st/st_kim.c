caa705b8349565b4a9ed46a555a7ca86bbabc2c3
Eyal Reizer <eyalr@ti.com>
2014-11-26 16:39:01 +0200

    drivers: misc: ti-st: fix debugfs creation error handling
    In case the debugfs creation fails the whole init process was failing.
    There is no need to do this as the shared transport can work without it.
    Fix it so it just reports the failure and continue.
    
    Signed-off-by: Eyal Reizer <eyalr@ti.com>

--- drivers/misc/ti-st/st_kim.c
+++ drivers/misc/ti-st/st_kim.c
@@ -835,8 +835,7 @@ static int kim_probe(struct platform_device *pdev)
 	kim_debugfs_dir = debugfs_create_dir("ti-st", NULL);
 	if (IS_ERR(kim_debugfs_dir)) {
 		pr_err(" debugfs entries creation failed ");
-		err = -EIO;
-		goto err_debugfs_dir;
+		return 0;
 	}
 
 	debugfs_create_file("version", S_IRUGO, kim_debugfs_dir,
@@ -845,9 +844,6 @@ static int kim_probe(struct platform_device *pdev)
 				kim_gdata, &list_debugfs_fops);
 	pr_info(" debugfs entries created ");
 	return 0;
-
-err_debugfs_dir:
-	sysfs_remove_group(&pdev->dev.kobj, &uim_attr_grp);
 
 err_sysfs_group:
 	st_core_exit(kim_gdata->core_data);
