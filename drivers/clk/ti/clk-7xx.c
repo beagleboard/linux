65e3f40c9b9719982c5d1e58ba9a086b6559c0df
Peter Ujfalusi <peter.ujfalusi@ti.com>
2015-02-26 11:55:53 +0200

    clk: ti: clk-7xx: Set working configuration for ATL
    The parent for ATL should be set to dpll_abe_m2_ck and ATL2 rate should be
    configured to 5644800 so it can be used for audio.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    
    Conflicts:
    	drivers/clk/ti/clk-7xx.c

--- drivers/clk/ti/clk-7xx.c
+++ drivers/clk/ti/clk-7xx.c
@@ -20,6 +20,7 @@
 #define DRA7_DPLL_GMAC_DEFFREQ				1000000000
 #define DRA7_DPLL_USB_DEFFREQ				960000000
 
+#define DRA7_ATL_DEFFREQ				5644800
 
 static struct ti_dt_clk dra7xx_clks[] = {
 	DT_CLK(NULL, "atl_clkin0_ck", "atl_clkin0_ck"),
@@ -313,6 +314,7 @@ int __init dra7xx_dt_clk_init(void)
 {
 	int rc;
 	struct clk *abe_dpll_mux, *sys_clkin2, *dpll_ck, *hdcp_ck;
+	struct clk *atl_fck, *atl_parent;
 
 	ti_dt_clocks_register(dra7xx_clks);
 
@@ -352,6 +354,17 @@ int __init dra7xx_dt_clk_init(void)
 	rc = clk_prepare_enable(hdcp_ck);
 	if (rc)
 		pr_err("%s: failed to set dss_deshdcp_clk\n", __func__);
+
+	atl_fck = clk_get_sys(NULL, "atl_gfclk_mux");
+	atl_parent = clk_get_sys(NULL, "dpll_abe_m2_ck");
+	rc = clk_set_parent(atl_fck, atl_parent);
+	if (rc)
+		pr_err("%s: failed to reparent atl_gfclk_mux\n", __func__);
+
+	atl_fck = clk_get_sys(NULL, "atl_clkin2_ck");
+	rc = clk_set_rate(atl_fck, DRA7_ATL_DEFFREQ);
+	if (rc)
+		pr_err("%s: failed to set atl_clkin2_ck\n", __func__);
 
 	return rc;
 }
