6c035d791f6c5344a651d6f00634d0deb4564763
Jyri Sarha <jsarha@ti.com>
2015-05-12 11:29:19 +0300

    OMAPDSS: sii9022-audio: Make reference clock inaccuracy message less alarming
    Make reference clock inaccuracy message less alarming and increase it
    from info to warning level.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- drivers/video/fbdev/omap2/displays-new/encoder-sii9022-audio.c
+++ drivers/video/fbdev/omap2/displays-new/encoder-sii9022-audio.c
@@ -190,9 +190,8 @@ static int sii9022_audio_start(struct snd_soc_dai *dai,
 	ret = sii9022_select_mclk_div(&i2s_config_reg, params_rate(params),
 				      dd->audio->mclk);
 	if (ret)
-		dev_info(dai->dev, "No matching mclk divider %u/%u != %d\n",
-			 dd->audio->mclk, params_rate(params),
-			 ret);
+		dev_warn(dai->dev, "Inaccurate reference clock (%u/%d != %u)\n",
+			 dd->audio->mclk, ret, params_rate(params));
 
 	switch (params_rate(params)) {
 	case 32000:
