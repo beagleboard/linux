23da93a08fe3f8051876619453651ba2b1db0380
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2015-06-12 15:32:55 +0300

    of: overlay: Introduce target root capability.
    The target facility of an overlay allows the target to be any point
    in the live kernel tree, since it usually that's required when
    creating overlays for internal SoC devices. The target ends up
    to be a single node in the tree.
    
    However when we're dealing with probeable busses this is a problem
    since the target node differs according to the bus the plugged
    device lies.
    
    Using an overlay creating method using a target root node allows
    us to use a single overlay for those cases.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -1669,6 +1669,8 @@ int of_overlay_notifier_register(struct notifier_block *nb);
 int of_overlay_notifier_unregister(struct notifier_block *nb);
 
 int of_overlay_create_target_index(struct device_node *tree, int index);
+int of_overlay_create_target_root(struct device_node *tree,
+		struct device_node *target_root);
 
 #else
 
@@ -1699,6 +1701,12 @@ static inline int of_overlay_notifier_unregister(struct notifier_block *nb)
 
 static inline int of_overlay_create_target_index(struct device_node *tree,
 		int index)
+{
+	return -ENOTSUPP;
+}
+
+static inline int of_overlay_create_target_root(struct device_node *tree,
+		struct device_node *target_root)
 {
 	return -ENOTSUPP;
 }
