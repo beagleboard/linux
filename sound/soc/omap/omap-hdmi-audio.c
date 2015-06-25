13d643b31564ddb8312616e494b389bda82b8d48
Misael Lopez Cruz <misael.lopez@ti.com>
2015-06-25 16:00:44 +0300

    ASoC: omap-hdmi-audio: Use HDMI HW IP alias for the card
    The HDMI sound uses a simple card created programatically, so
    the preferred card id cannot be passed through an alias via DT.
    The card id hint is taken from the HDMI HW IP's alias instead,
    which does have a DT entry.
    
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>
    Signed-off-by: Jyri Sarha <jsarha@ti.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- sound/soc/omap/omap-hdmi-audio.c
+++ sound/soc/omap/omap-hdmi-audio.c
@@ -28,6 +28,7 @@
 #include <sound/asoundef.h>
 #include <sound/omap-pcm.h>
 #include <sound/omap-hdmi-audio.h>
+#include <sound/initval.h>
 
 #define DRV_NAME "omap-hdmi-audio"
 
@@ -321,10 +322,18 @@ static int omap_hdmi_audio_probe(struct platform_device *pdev)
 	struct snd_soc_dai_driver *dai_drv;
 	struct snd_soc_card *card;
 	int ret;
+	int id = SNDRV_DEFAULT_IDX1;
 
 	if (!ha) {
 		dev_err(dev, "No platform data\n");
 		return -EINVAL;
+	}
+
+	/* Get the id of the parent (the HDMI HW IP) */
+	if (ha->dev->of_node) {
+		id = of_alias_get_id(ha->dev->of_node, "sound");
+		if (id < 0)
+			id = SNDRV_DEFAULT_IDX1;
 	}
 
 	ad = devm_kzalloc(dev, sizeof(*ad), GFP_KERNEL);
@@ -362,6 +371,7 @@ static int omap_hdmi_audio_probe(struct platform_device *pdev)
 
 	card->name = devm_kasprintf(dev, GFP_KERNEL,
 				    "HDMI %s", dev_name(ad->dssdev));
+	card->id_hint = id;
 	card->owner = THIS_MODULE;
 	card->dai_link =
 		devm_kzalloc(dev, sizeof(*(card->dai_link)), GFP_KERNEL);
