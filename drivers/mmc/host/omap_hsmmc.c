271e528f2cea149fa485bcb0070c70d755d76081
Kishon Vijay Abraham I <kishon@ti.com>
2015-08-05 18:54:32 +0530

    mmc: host: omap_hsmmc: Workaround for errata id i802
    According to errata i802, DCRC error interrupts
    (MMCHS_STAT[21] DCRC=0x1) can occur during the tuning procedure.
    
    The DCRC interrupt, occurs when the last tuning block fails
    (the last ratio tested). The delay from CRC check until the
    interrupt is asserted is bigger than the delay until assertion
    of the tuning end flag. Assertion of tuning end flag is what
    masks the interrupts. Because of this race, an erroneous DCRC
    interrupt occurs.
    
    The suggested  workaround is to disable DCRC interrupts during
    the tuning procedure which is implemented here.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -632,6 +632,19 @@ static void omap_hsmmc_enable_irq(struct omap_hsmmc_host *host,
 {
 	u32 irq_mask = INT_EN_MASK;
 	unsigned long flags;
+	bool is_tuning;
+
+	is_tuning = cmd && ((cmd->opcode == MMC_SEND_TUNING_BLOCK) ||
+		    (cmd->opcode == MMC_SEND_TUNING_BLOCK_HS200));
+
+	if (is_tuning)
+		/*
+		 * OMAP5/DRA74X/DRA72x Errata i802:
+		 * DCRC error interrupts (MMCHS_STAT[21] DCRC=0x1) can occur
+		 * during the tuning procedure. So disable it during the
+		 * tuning procedure.
+		 */
+		irq_mask &= ~DCRC_EN;
 
 	if (host->use_dma)
 		irq_mask &= ~(BRR_EN | BWR_EN);
