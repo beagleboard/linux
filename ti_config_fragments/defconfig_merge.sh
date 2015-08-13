beagleboard/linux

commit 2dd88defcd0b86ecf18dd8c803a1ad66683c6fb5

ti_config_frag/defconfig_merge: Use the defconfig passed in
Use the defconfig file passed in on the command line as an
over ride of the defconfig file that is in the defconfig
extras file.

Signed-off-by: Dan Murphy <dmurphy@ti.com>

ti_config_fragments/defconfig_merge.sh
@@ -402,14 +402,20 @@ if [ $? -ne 0 ]; then
 	 exit 1
 fi
 
-if [ -n "$DEFCONFIG_EXTRAS_FILE"  ]; then
-	DEFCONFIG=`cat $DEFCONFIG_EXTRAS_FILE | grep "use-kernel-config=" | cut -d= -f2`
-	if [ -z "$DEFCONFIG"  ]; then
-		echo -e "\n\tMissing base defconfig in the file\n"
+if [ -z "$DEFCONFIG" ];then
+	if [ -n "$DEFCONFIG_EXTRAS_FILE"  ]; then
+		DEFCONFIG=`cat $DEFCONFIG_EXTRAS_FILE | grep "use-kernel-config=" | cut -d= -f2`
+		if [ -z "$DEFCONFIG"  ]; then
+			echo -e "\n\tMissing base defconfig in the file\n"
+			usage
+			exit 1
+		fi
+		echo "Using base config $DEFCONFIG from the file $DEFCONFIG_EXTRAS_FILE"
+	else
+		echo "Missing a defconfig cannot proceed"
 		usage
 		exit 1
 	fi
-	echo "Using base config $DEFCONFIG from the file $DEFCONFIG_EXTRAS_FILE"
 fi
 
 DEFCONFIG_EXTRAS="$LOGGING_DIRECTORY/merged_$DEFCONFIG"

commit 0f51357456d7493739f3591dfada0cc5a428c569

ti_config_frag: defconfig_merge: Remove hard coded defconfig
Remove the hard coded defconfig at the top of the file.  The
check for a defconfig will always be true and never set the
defconfig from the config fragments file.

Fixes: 2dd88defcd0b ("ti_config_frag/defconfig_merge: Use the defconfig passed in")

Reported-by: Nishanth Menon <nm@ti.com>
Signed-off-by: Dan Murphy <dmurphy@ti.com>

ti_config_fragments/defconfig_merge.sh
@@ -13,11 +13,11 @@ export unix_user=`whoami`
 export user_home_directory=`getent passwd $unix_user |cut -d: -f6`
 
 BUILD_THREADS=`grep -c "^processor" /proc/cpuinfo`
-DEFCONFIG="omap2plus_defconfig"
-CROSS_COMPILE=
+DEFCONFIG=""
+CROSS_COMPILE=""
 WORKING_PATH=""
 LOAD_ADDR=0x80008000
-LOAD_ADDRESS_VALUE=
+LOAD_ADDRESS_VALUE=""
 LOGGING_DIRECTORY="working_config"
 
 prepare_for_exit()