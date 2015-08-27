3e64a0eb076698b1145fcf2159f79e7944b22f94
Nikhil Devshatwar <nikhil.nd@ti.com>
2015-08-26 19:28:53 -0500

    arm: dtsi: dra72: Add VIP mux pinctrl node and bindings
    DRA72 device has only one instance of VIP (VIP1)
    But the SoC has more video pads(total 8) than available ports
    (1a, 2a, 1b, 2b). Each video port can be connected to one of
    the pad group. This is selected via a control module register
    CTRL_CORE_VIP_MUX_SELECT.
    
    Depending on the board design, some of the interfaces would be used
    and this register needs to be programmed.
    As this can be best fit as a pinctrl device, Add a pinctrl node in
    dra72x.dtsi and also add the bindings for the bitfields to be used
    when configuring this mux.
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>
    Signed-off-by: Benoit Parrot <bparrot@ti.com>
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- include/dt-bindings/pinctrl/dra.h
+++ include/dt-bindings/pinctrl/dra.h
@@ -73,5 +73,18 @@
 #define A_DELAY(val)		((val) & 0xFFFF)
 #define G_DELAY(val)		(((val) & 0xFFFF) << 16)
 
+/* DRA72 VIP MUX selection parameters */
+#define VIP_VIN3A		(0x0 << 4)
+#define VIP_VIN5A		(0x1 << 4)
+#define VIP_VIN6A		(0x2 << 4)
+
+#define VIP_VIN4B		(0x0 << 3)
+
+#define VIP_VIN2A		(0x0 << 1)
+#define VIP_VIN4A		(0x1 << 1)
+
+#define VIP_VIN3B		(0x0 << 0)
+#define VIP_VIN2B		(0x1 << 0)
+
 #endif
 
