6885bc4cdbaf153fe147f27cebe75870df79c5b6
Robert Nelson <robertcnelson@gmail.com>
2015-10-16 12:02:42 -0500

    quiet: topology.c use pr_info over pr_err (missing clock-frequency property)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -119,7 +119,7 @@ static void __init parse_dt_topology(void)
 
 		rate = of_get_property(cn, "clock-frequency", &len);
 		if (!rate || len != 4) {
-			pr_err("%s missing clock-frequency property\n",
+			pr_info("%s missing clock-frequency property\n",
 				cn->full_name);
 			continue;
 		}
