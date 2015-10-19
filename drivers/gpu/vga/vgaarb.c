0033dc97e1a88271f9d0f0f37e5f185cc17fb798
Robert Nelson <robertcnelson@gmail.com>
2015-10-19 10:10:42 -0500

    quiet: vgaarb use pr_info over pr_err
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/gpu/vga/vgaarb.c
+++ drivers/gpu/vga/vgaarb.c
@@ -1091,7 +1091,7 @@ static ssize_t vga_arb_write(struct file *file, const char __user * buf,
 		vgadev = vgadev_find(pdev);
 		pr_debug("vgaarb: vgadev %p\n", vgadev);
 		if (vgadev == NULL) {
-			pr_err("vgaarb: this pci device is not a vga device\n");
+			pr_info("vgaarb: this pci device is not a vga device\n");
 			pci_dev_put(pdev);
 			ret_val = -ENODEV;
 			goto done;
