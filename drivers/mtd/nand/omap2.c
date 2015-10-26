67778a180f9eb51c958b12863accc8f310cf400b
Franklin S Cooper Jr <fcooper@ti.com>
2015-10-26 09:37:05 -0500

    mtd: nand: omap2: Support parsing dma channel information from DT
    Switch from dma_request_channel to allow passing dma channel
    information from DT rather than hardcoding a value.
    
    Signed-off-by: Franklin S Cooper Jr <fcooper@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/mtd/nand/omap2.c
+++ drivers/mtd/nand/omap2.c
@@ -1866,7 +1866,9 @@ static int omap_nand_probe(struct platform_device *pdev)
 		dma_cap_zero(mask);
 		dma_cap_set(DMA_SLAVE, mask);
 		sig = OMAP24XX_DMA_GPMC;
-		info->dma = dma_request_channel(mask, omap_dma_filter_fn, &sig);
+		info->dma = dma_request_slave_channel_compat(mask,
+			omap_dma_filter_fn, &sig, pdev->dev.parent, "rxtx");
+
 		if (!info->dma) {
 			dev_err(&pdev->dev, "DMA engine request failed\n");
 			err = -ENXIO;
