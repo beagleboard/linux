71a6a9955d26f0914a548fe4305e72b0bbd38b4e
Suman Anna <s-anna@ti.com>
2015-11-06 23:22:43 -0600

    TEMP: soc: ti: add the keystone_dsp_mem driver
    A very simple keystone_dsp_mem driver has been added for
    TI's Keystone 2 family of SoCs. This driver provides a
    user-space mapping interface for the Multicore Shared
    Memory (MSM) SRAM Memory present on the SoC and a portion
    of the platform board's DDR memory, to enable the Multi
    Proc Manager (MPM) based loader for loading different
    firmware images on the various C66 DSP co-processors on
    the SoC.
    
    The mapping interfaces are provided through a miscdevice,
    expected to have a device name (currently /dev/dspmem,
    derived from DT node name) matching the usage within MPM.
    The mmap logic itself is based on a mechanism used within
    the UIO framework.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

---
+++ include/uapi/linux/keystone_dsp_mem.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015-2016 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _UAPI_KEYSTONE_DSP_MEM_H_
+#define _UAPI_KEYSTONE_DSP_MEM_H_
+
+#define KEYSTONE_DSP_MEM_MAP_INDEX_MASK		(0x7)
+#define KEYSTONE_DSP_MEM_MAP_OFFSET_SHIFT	(3)
+
+#endif /* _UAPI_KEYSTONE_DSP_MEM_H_ */
