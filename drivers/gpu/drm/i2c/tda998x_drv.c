71c33d253f2a04fdf0f5e5e33c6b9a2444924d89
Liviu Dudau (ARM) <Liviu.Dudau@arm.com>
2015-11-23 16:52:42 +0100

    drm/i2c: tda998x: Add support for atomic modesetting
    commit 9736e988d32807d5a186c722928a97f37346fec8 upstream.
    
    When used with a DRIVER_ATOMIC enabled CRTC driver, the tda998x
    will cause crashes due to missing atomic operations. Fill the
    drm_connector_funcs struct with the atomic versions of the required
    functions and add the atomic modeset specific callbacks.
    
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- drivers/gpu/drm/i2c/tda998x_drv.c
+++ drivers/gpu/drm/i2c/tda998x_drv.c
@@ -23,6 +23,7 @@
 #include <sound/hdmi-codec.h>
 
 #include <drm/drmP.h>
+#include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_edid.h>
 #include <drm/drm_of.h>
@@ -1586,10 +1587,13 @@ static void tda998x_connector_destroy(struct drm_connector *connector)
 }
 
 static const struct drm_connector_funcs tda998x_connector_funcs = {
-	.dpms = drm_helper_connector_dpms,
+	.dpms = drm_atomic_helper_connector_dpms,
+	.reset = drm_atomic_helper_connector_reset,
 	.fill_modes = drm_helper_probe_single_connector_modes,
 	.detect = tda998x_connector_detect,
 	.destroy = tda998x_connector_destroy,
+	.atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
+	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
 static int tda998x_bind(struct device *dev, struct device *master, void *data)
