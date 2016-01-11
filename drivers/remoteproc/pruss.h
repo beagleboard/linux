29a44a4c71e92f44ad39d40c14f473477a2a3796
Andrew F. Davis <afd@ti.com>
2016-01-11 16:18:38 -0600

    remoteproc/pruss: add pruss_{request,release}_mem_region() API
    Add two new API - pruss_request_mem_region() & pruss_release_mem_region(),
    to the PRUSS platform driver to allow client drivers to acquire and release
    the common memory resources present within a PRU-ICSS subsystem. This
    allows the client drivers to directly manipulate the respective memories,
    as per their design contract with the associated firmware.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    [s-anna@ti.com: rename functions, add error checking, comments]
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/remoteproc/pruss.h
+++ drivers/remoteproc/pruss.h
@@ -99,32 +99,6 @@
 #define PRUSS_SYSCFG_IDLE_MODE_MASK	3
 
 /**
- * enum pruss_mem - PRUSS memory range identifiers
- */
-enum pruss_mem {
-	PRUSS_MEM_DRAM0 = 0,
-	PRUSS_MEM_DRAM1,
-	PRUSS_MEM_SHRD_RAM2,
-	PRUSS_MEM_INTC,
-	PRUSS_MEM_CFG,
-	PRUSS_MEM_IEP,
-	PRUSS_MEM_MII_RT,
-	PRUSS_MEM_MAX,
-};
-
-/**
- * struct pruss_mem_region - PRUSS memory region structure
- * @va: kernel virtual address of the PRUSS memory region
- * @pa: physical (bus) address of the PRUSS memory region
- * @size: size of the PRUSS memory region
- */
-struct pruss_mem_region {
-	void __iomem *va;
-	phys_addr_t pa;
-	size_t size;
-};
-
-/**
  * struct pruss_intc_config - INTC configuration info
  * @sysev_to_ch: system events to channel mapping information
  * @ch_to_host: interrupt channel to host interrupt information
@@ -141,6 +115,7 @@ struct pru_rproc;
  * @node: list node of this object
  * @dev: pruss device pointer
  * @mem_regions: data for each of the PRUSS memory regions
+ * @mem_in_use: to indicate if memory resource is in use
  * @data: pointer to store PRUSS instance private data
  * @intc_config: local INTC configuration data
  * @host_mask: indicate which HOST IRQs are enabled
@@ -154,6 +129,7 @@ struct pruss {
 	struct list_head node;
 	struct device *dev;
 	struct pruss_mem_region mem_regions[PRUSS_MEM_MAX];
+	struct pruss_mem_region *mem_in_use[PRUSS_MEM_MAX];
 	const struct pruss_private_data *data;
 	struct pruss_intc_config intc_config;
 	u32 host_mask;
--- include/linux/pruss.h
+++ include/linux/pruss.h
@@ -26,6 +26,32 @@ enum pruss_pru_id {
 	PRUSS_NUM_PRUS,
 };
 
+/**
+ * enum pruss_mem - PRUSS memory range identifiers
+ */
+enum pruss_mem {
+	PRUSS_MEM_DRAM0 = 0,
+	PRUSS_MEM_DRAM1,
+	PRUSS_MEM_SHRD_RAM2,
+	PRUSS_MEM_INTC,
+	PRUSS_MEM_CFG,
+	PRUSS_MEM_IEP,
+	PRUSS_MEM_MII_RT,
+	PRUSS_MEM_MAX,
+};
+
+/**
+ * struct pruss_mem_region - PRUSS memory region structure
+ * @va: kernel virtual address of the PRUSS memory region
+ * @pa: physical (bus) address of the PRUSS memory region
+ * @size: size of the PRUSS memory region
+ */
+struct pruss_mem_region {
+	void __iomem *va;
+	phys_addr_t pa;
+	size_t size;
+};
+
 struct pruss;
 
 #if IS_ENABLED(CONFIG_PRUSS_REMOTEPROC)
@@ -35,6 +61,10 @@ void pruss_put(struct pruss *pruss);
 struct rproc *pruss_rproc_get(struct pruss *pruss,
 			      enum pruss_pru_id pru_id);
 void pruss_rproc_put(struct pruss *pruss, struct rproc *rproc);
+int pruss_request_mem_region(struct pruss *pruss, enum pruss_mem mem_id,
+			     struct pruss_mem_region *region);
+int pruss_release_mem_region(struct pruss *pruss,
+			     struct pruss_mem_region *region);
 
 #else
 
@@ -52,6 +82,19 @@ static inline struct rproc *pruss_rproc_get(struct pruss *pruss,
 }
 
 static inline void pruss_rproc_put(struct pruss *pruss, struct rproc *rproc) { }
+
+static inline int pruss_request_mem_region(struct pruss *pruss,
+					   enum pruss_mem mem_id,
+					   struct pruss_mem_region *region)
+{
+	return ERR_PTR(-ENOTSUPP);
+}
+
+static inline int pruss_release_mem_region(struct pruss *pruss,
+					   struct pruss_mem_region *region)
+{
+	return ERR_PTR(-ENOTSUPP);
+}
 
 #endif /* CONFIG_PRUSS_REMOTEPROC */
 
