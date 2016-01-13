5acab7cf4e6bcff834fc58d5128369511b0799bc
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2016-01-13 15:55:02 +0100

    net: move xmit_recursion to per-task variable on -RT
    A softirq on -RT can be preempted. That means one task is in
    __dev_queue_xmit(), gets preempted and another task may enter
    __dev_queue_xmit() aw well. netperf together with a bridge device
    will then trigger the `recursion alert` because each task increments
    the xmit_recursion variable which is per-CPU.
    A virtual device like br0 is required to trigger this warning.
    
    This patch moves the counter to per task instead per-CPU so it counts
    the recursion properly on -RT.
    
    Cc: stable-rt@vger.kernel.org
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1806,6 +1806,9 @@ struct task_struct {
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
 	unsigned long	task_state_change;
 #endif
+#ifdef CONFIG_PREEMPT_RT_FULL
+	int xmit_recursion;
+#endif
 	int pagefault_disabled;
 };
 
