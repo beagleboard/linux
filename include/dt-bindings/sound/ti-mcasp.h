5d61b2678b83ad9fd5ef441ea83744d41a53a122
Peter Ujfalusi <peter.ujfalusi@ti.com>
2016-01-18 15:00:07 +0200

    ASoC: davinci-mcasp: Improve the sysclk selection
    When McASP is master the bclk can be generated from two main source:
    AUXCLK: functional clock for McASP or
    AHCLK: from external source or internal mux in dra7x family
    
    With this patch it is possible to select between the two source. The patch
    is not breaking existing machine drivers since historically the clk_id was
    ignored and left as 0 in all cases.
    
    When output clock is configured - which can be only the AHCLK, we select
    the AUXCLK as source for the internal HCLK. In this case the HCLK rate is
    the same as the output clock.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- include/dt-bindings/sound/ti-mcasp.h
+++ include/dt-bindings/sound/ti-mcasp.h
@@ -1,6 +1,10 @@
 #ifndef _DT_BINDINGS_TI_MCASP_H
 #define _DT_BINDINGS_TI_MCASP_H
 
+/* Source of High-frequency transmit/receive clock */
+#define MCASP_CLK_HCLK_AHCLK		0 /* AHCLKX/R */
+#define MCASP_CLK_HCLK_AUXCLK		1 /* Internal functional clock */
+
 /* clock divider IDs */
 #define MCASP_CLKDIV_AUXCLK		0 /* HCLK divider from AUXCLK */
 #define MCASP_CLKDIV_BCLK		1 /* BCLK divider from HCLK */
