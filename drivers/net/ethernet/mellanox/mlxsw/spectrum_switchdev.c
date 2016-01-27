2b4ff0796f291ef4dae7df42d0e24c37e0fdb3c5
Ido Schimmel <idosch@mellanox.com>
2016-01-27 15:20:19 +0100

    mlxsw: spectrum: Don't forward packets when STP state is DISABLED
    commit 9cb026ebb8ab76829a8d8e4bbd057168ac38fb86 upstream.
    
    When STP state is set to DISABLED the port is assumed to be inactive, but
    currently we forward packets ingressing through it.
    
    Instead, set the port's STP state in hardware to DISCARDING, which means
    it doesn't forward packets or perform any learning, but it does trap
    control packets. However, these packets will be dropped by bridge code,
    which results in the expected behavior.
    
    Fixes: 56ade8fe3fe1 ("mlxsw: spectrum: Add initial support for Spectrum ASIC")
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_switchdev.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_switchdev.c
@@ -87,7 +87,6 @@ static int mlxsw_sp_port_stp_state_set(struct mlxsw_sp_port *mlxsw_sp_port,
 	int err;
 
 	switch (state) {
-	case BR_STATE_DISABLED: /* fall-through */
 	case BR_STATE_FORWARDING:
 		spms_state = MLXSW_REG_SPMS_STATE_FORWARDING;
 		break;
@@ -95,6 +94,7 @@ static int mlxsw_sp_port_stp_state_set(struct mlxsw_sp_port *mlxsw_sp_port,
 	case BR_STATE_LEARNING:
 		spms_state = MLXSW_REG_SPMS_STATE_LEARNING;
 		break;
+	case BR_STATE_DISABLED: /* fall-through */
 	case BR_STATE_BLOCKING:
 		spms_state = MLXSW_REG_SPMS_STATE_DISCARDING;
 		break;
