beagleboard/linux

commit 5e1e88c82b54c306ba06c40ff92de4152170530a

ARM: OMAP2+: Add am33xx_pm_deinit to disable cpuidle
Currently if pm33xx is built as a module and removed, cpuidle does not
get disabled so if it attempts to enter C1 state, the call into pm33xx
will result in a hang. Add hooks to allow unregister of cpuidle33xx
driver so that we can remove it when pm33xx module gets removed.

Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

arch/arm/mach-omap2/pm.h
@@ -17,6 +17,7 @@
 
 #ifdef CONFIG_CPU_IDLE
 extern int am33xx_idle_init(bool ddr3, void (*do_idle)(u32 wfi_flags));
+extern void am33xx_idle_deinit(void);
 extern int am437x_idle_init(void);
 extern int __init omap3_idle_init(void);
 extern int __init omap4_idle_init(void);
@@ -112,6 +113,7 @@ extern void (*omap3_do_wfi_sram)(void);
 /* for sharing core pm ops with amx3 pm modules */
 struct am33xx_pm_ops {
 	int	(*init)(void (*do_sram_cpuidle)(u32 wfi_flags));
+	int	(*deinit)(void);
 	int	(*soc_suspend)(unsigned int state, int (*fn)(unsigned long),
 			       unsigned long args);
 	int	(*cpu_suspend)(int (*fn)(unsigned long), unsigned long args);

arch/arm/mach-omap2/cpuidle33xx.c
@@ -118,3 +118,8 @@ int am33xx_idle_init(bool ddr3, void (*do_idle)(u32 wfi_flags))
 	}
 	return cpuidle_register(&am33xx_idle_driver, NULL);
 }
+
+void am33xx_idle_deinit(void)
+{
+	cpuidle_unregister(&am33xx_idle_driver);
+}

arch/arm/mach-omap2/pm33xx-core.c
@@ -126,6 +126,12 @@ static int am43xx_suspend_init(void (*do_sram_cpuidle)(u32 wfi_flags))
 	return ret;
 }
 
+static int am33xx_pm_deinit(void)
+{
+	am33xx_idle_deinit();
+	return 0;
+}
+
 static void amx3_pre_suspend_common(void)
 {
 	omap_set_pwrdm_state(gfx_pwrdm, PWRDM_POWER_OFF);
@@ -260,6 +266,7 @@ void __iomem *am43xx_get_rtc_base_addr(void)
 
 static struct am33xx_pm_ops am33xx_ops = {
 	.init = am33xx_suspend_init,
+	.deinit = am33xx_pm_deinit,
 	.soc_suspend = am33xx_suspend,
 	.cpu_suspend = am33xx_cpu_suspend,
 	.save_context = am33xx_save_context,

arch/arm/mach-omap2/pm33xx.c
@@ -417,6 +417,8 @@ static int am33xx_pm_probe(struct platform_device *pdev)
 
 static int am33xx_pm_remove(struct platform_device *pdev)
 {
+	if (pm_ops->deinit)
+		pm_ops->deinit();
 	suspend_set_ops(NULL);
 	gen_pool_free(sram_pool, ocmcram_location, *pm_sram->do_wfi_sz);
 	return 0;

commit 9ba1bdb725bec4c78dce1d3ae568da7d39d93b3a

ARM: OMAP2+: Add am33xx_pm_deinit stub for !CONFIG_CPU_IDLE
Add an empty stub for builds done with !CONFIG_CPU_IDLE, otherwise
build will fail with error:

arch/arm/mach-omap2/pm33xx-core.c: In function ‘am33xx_pm_deinit’:
arch/arm/mach-omap2/pm33xx-core.c:131:2: error: implicit declaration of
function ‘am33xx_idle_deinit’ [-Werror=implicit-function-declaration]
  am33xx_idle_deinit();

Fixes: 5e1e88c82b54 ("ARM: OMAP2+: Add am33xx_pm_deinit to disable cpuidle")
Reported-by: Dan Murphy <dmurphy@ti.com>
Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

arch/arm/mach-omap2/pm.h
@@ -28,6 +28,11 @@ static inline int am33xx_idle_init(bool ddr3, void (*do_sram_cpuidle)
 	return 0;
 }
 
+static inline int am33xxx_idle_deinit(void)
+{
+	return 0;
+}
+
 static inline int am437x_idle_init(void)
 {
 	return 0;