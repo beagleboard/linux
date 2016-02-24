beagleboard/linux

commit 8ecaf7cde481bf0af2dd8258c79e21e2d1a5362e

ARM: OMAP2+: Create dummy platform_device for pm33xx
Add amx3_common_pm_init to create a dummy platform_device for
pm33xx so that our pm33xx module can probe and am335x and am437x
platforms to enable basic suspend to mem and standby support.

Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

arch/arm/mach-omap2/pm.c
@@ -13,6 +13,7 @@
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/err.h>
+#include <linux/platform_data/pm33xx.h>
 #include <linux/pm_opp.h>
 #include <linux/export.h>
 #include <linux/suspend.h>
@@ -283,6 +284,18 @@ static inline void omap_init_cpufreq(void)
 		devinfo.name = "omap-cpufreq";
 	else
 		devinfo.name = "cpufreq-dt";
+	platform_device_register_full(&devinfo);
+}
+
+void __init amx3_common_pm_init(void)
+{
+	struct platform_device_info devinfo = { };
+	struct am33xx_pm_platform_data *pdata;
+
+	pdata = am33xx_pm_get_pdata();
+	devinfo.name = "pm33xx";
+	devinfo.data = pdata;
+	devinfo.size_data = sizeof(*pdata);
 	platform_device_register_full(&devinfo);
 }
 

arch/arm/mach-omap2/common.h
@@ -77,6 +77,13 @@ static inline int omap4_pm_init_early(void)
 }
 #endif
 
+#if defined(CONFIG_PM) && (defined(CONFIG_SOC_AM33XX) || \
+	defined(CONFIG_SOC_AM43XX))
+void amx3_common_pm_init(void);
+#else
+static inline void amx3_common_pm_init(void) { }
+#endif
+
 #ifdef CONFIG_OMAP_MUX
 int omap_mux_late_init(void);
 #else

arch/arm/mach-omap2/io.c
@@ -653,6 +653,7 @@ void __init am33xx_init_early(void)
 void __init am33xx_init_late(void)
 {
 	omap_common_late_init();
+	amx3_common_pm_init();
 }
 #endif
 
@@ -677,6 +678,7 @@ void __init am43xx_init_late(void)
 {
 	omap_common_late_init();
 	omap2_clk_enable_autoidle_all();
+	amx3_common_pm_init();
 }
 #endif
 

commit 9eb643ff8c157f76b39a0b7d8e0c12f9b495266e

ARM: OMAP2+: Fix amx3_common_pm_in when AM3 is not selected
Move amx3_common_pm_init to pm33xx-core.c from the pm.c common
file.  This fixes a compilation break

arch/arm/mach-omap2/pm.c:290:116: error: redefinition of 'amx3_common_pm_init'
 void __init amx3_common_pm_init(void)
                                                                                                                    ^
In file included from arch/arm/mach-omap2/pm.c:26:0:
arch/arm/mach-omap2/common.h:84:91: note: previous definition of 'amx3_common_pm_init' was here
 static inline void amx3_common_pm_init(void) { }
                                                                                           ^
make[1]: *** [arch/arm/mach-omap2/pm.o] Error 1

When AM3x and AM4x config flags are not enabled

Fixes: 8ecaf7cde ("ARM: OMAP2+: Create dummy platform_device for pm33xx")
Signed-off-by: Dan Murphy <dmurphy@ti.com>
Acked-by: Dave Gerlach <d-gerlach@ti.com>

arch/arm/mach-omap2/pm.c
@@ -287,18 +287,6 @@ static inline void omap_init_cpufreq(void)
 	platform_device_register_full(&devinfo);
 }
 
-void __init amx3_common_pm_init(void)
-{
-	struct platform_device_info devinfo = { };
-	struct am33xx_pm_platform_data *pdata;
-
-	pdata = am33xx_pm_get_pdata();
-	devinfo.name = "pm33xx";
-	devinfo.data = pdata;
-	devinfo.size_data = sizeof(*pdata);
-	platform_device_register_full(&devinfo);
-}
-
 static int __init omap2_common_pm_init(void)
 {
 	if (!of_have_populated_dt())

arch/arm/mach-omap2/pm33xx-core.c
@@ -170,3 +170,15 @@ struct am33xx_pm_platform_data *am33xx_pm_get_pdata(void)
 	else
 		return NULL;
 }
+
+void __init amx3_common_pm_init(void)
+{
+	struct platform_device_info devinfo = { };
+	struct am33xx_pm_platform_data *pdata;
+
+	pdata = am33xx_pm_get_pdata();
+	devinfo.name = "pm33xx";
+	devinfo.data = pdata;
+	devinfo.size_data = sizeof(*pdata);
+	platform_device_register_full(&devinfo);
+}