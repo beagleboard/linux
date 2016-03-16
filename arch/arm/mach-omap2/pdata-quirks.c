1a1cde485acc14c61358bc1ba8a519ffea7b1805
Suman Anna <s-anna@ti.com>
2016-03-16 14:49:18 -0500

    ARM: OMAP2+: Extend rproc pdata-quirks for IPUs & DSP1 on DRA7
    The remote processors on DRA7xx requires similar device management
    pdata-quirks (reset control, clocking, dmtimer API wrappers for
    enabling both system and watchdog timers) as the IPU and DSP
    processors on OMAP4/OMAP5. All these pdata ops are identical to
    the ones used on OMAP4/OMAP5, so extend the existing rproc pdata
    quirks to the most common remote processor subsystems on DRA7xx
    family of SoCs - IPU1, IPU2 and DSP1.
    
    The pdata quirks need to match the starting address in the auxdata
    for the respective processors, as the same compatible string is used
    for all the instances of the remote processor of the same type. The
    pdata quirks will be removed once the dependencies against arch/arm
    layers are resolved.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -415,7 +415,8 @@ static void __init omap3_pandora_legacy_init(void)
 }
 #endif /* CONFIG_ARCH_OMAP3 */
 
-#if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5)
+#if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5) || \
+	defined(CONFIG_SOC_DRA7XX)
 static struct omap_rproc_timer_ops omap_rproc_dmtimer_ops = {
 	.request_timer = omap_rproc_request_timer,
 	.release_timer = omap_rproc_release_timer,
@@ -430,10 +431,7 @@ static struct omap_rproc_pdata omap4_ipu_dsp_pdata = {
 	.device_shutdown = omap_rproc_device_shutdown,
 	.timer_ops = &omap_rproc_dmtimer_ops,
 };
-#endif
 
-#if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5) || \
-	defined(CONFIG_SOC_DRA7XX)
 static struct iommu_platform_data omap4_iommu_pdata = {
 	.reset_name = "mmu_cache",
 	.assert_reset = omap_device_assert_hardreset,
@@ -599,6 +597,12 @@ static struct of_dev_auxdata omap_auxdata_lookup[] __initdata = {
 		       &omap4_iommu_pdata),
 	OF_DEV_AUXDATA("ti,dra7-iommu", 0x58882000, "58882000.mmu",
 		       &omap4_iommu_pdata),
+	OF_DEV_AUXDATA("ti,dra7-ipu", 0x55020000, "55020000.ipu",
+		       &omap4_ipu_dsp_pdata),
+	OF_DEV_AUXDATA("ti,dra7-ipu", 0x58820000, "58820000.ipu",
+		       &omap4_ipu_dsp_pdata),
+	OF_DEV_AUXDATA("ti,dra7-dsp", 0x40800000, "40800000.dsp",
+		       &omap4_ipu_dsp_pdata),
 #endif
 	/* Common auxdata */
 	OF_DEV_AUXDATA("pinctrl-single", 0, NULL, &pcs_pdata),
