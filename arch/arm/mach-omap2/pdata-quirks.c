7089b7b9820191362933ad96fcfe7462a1b7521a
Suman Anna <s-anna@ti.com>
2016-03-16 19:10:43 -0500

    ARM: OMAP2+: Extend OMAP4 IPU/DSP pdata-quirks to OMAP5
    OMAP5 requires the same device management pdata-quirks as OMAP4
    for the IPU and DSP processor subsystems, so extend the OMAP4 IPU
    and DSP pdata quirks to OMAP5 as well.
    
    The pdata quirks uses the appropriate starting addresses in the
    auxdata as per the current DT node definitions to find the devices
    to add the necessary quirks as platform data. The pdata quirks will
    be removed once the reset dependencies against omap_device and
    omap_hwmod layers are resolved.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -415,7 +415,7 @@ static void __init omap3_pandora_legacy_init(void)
 }
 #endif /* CONFIG_ARCH_OMAP3 */
 
-#ifdef CONFIG_ARCH_OMAP4
+#if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5)
 static struct omap_rproc_timer_ops omap_rproc_dmtimer_ops = {
 	.request_timer = omap_rproc_request_timer,
 	.release_timer = omap_rproc_release_timer,
@@ -576,6 +576,10 @@ static struct of_dev_auxdata omap_auxdata_lookup[] __initdata = {
 #ifdef CONFIG_ARCH_OMAP4
 	OF_DEV_AUXDATA("ti,omap4-dsp", 0, "dsp", &omap4_ipu_dsp_pdata),
 	OF_DEV_AUXDATA("ti,omap4-ipu", 0x55020000, "ipu", &omap4_ipu_dsp_pdata),
+#endif
+#ifdef CONFIG_SOC_OMAP5
+	OF_DEV_AUXDATA("ti,omap5-dsp", 0, "dsp", &omap4_ipu_dsp_pdata),
+	OF_DEV_AUXDATA("ti,omap5-ipu", 0x55020000, "ipu", &omap4_ipu_dsp_pdata),
 #endif
 #if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5)
 	OF_DEV_AUXDATA("ti,omap4-iommu", 0x4a066000, "4a066000.mmu",
