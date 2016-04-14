ac6193fa6f46e460f7ccca0ae2c06c96bc5e635a
Robert Nelson <robertcnelson@gmail.com>
2016-04-13 20:59:36 -0500

    Delete gcc_too_old.sh
    this hack is no longer used, with moving to trusty.

--- gcc_too_old.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-arm-linux-gnueabihf-gcc -v
-
-echo "disabling: CONFIG_THUMB2_KERNEL"
-sed -i -e 's:CONFIG_THUMB2_KERNEL=y:# CONFIG_THUMB2_KERNEL is not set:g' .config
-sed -i -e 's:CONFIG_THUMB2_AVOID_R_ARM_THM_JUMP11=y::g' .config
-sed -i -e 's:CONFIG_ARM_ASM_UNIFIED=y::g' .config
