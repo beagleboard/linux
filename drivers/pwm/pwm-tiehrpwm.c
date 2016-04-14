7b003964b11de647ee14e259dbe8c630179b5157
Robert Nelson <robertcnelson@gmail.com>
2016-04-14 12:24:14 -0500

    Revert "pwms: pwm-ti*: Remove support for local clock gating"
    This reverts commit aa0156707cc55c1ea4bfd7c48eb2b40934823dd6.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/pwm/pwm-tiehrpwm.c
+++ drivers/pwm/pwm-tiehrpwm.c
@@ -27,6 +27,8 @@
 #include <linux/pm_runtime.h>
 #include <linux/of_device.h>
 
+#include "pwm-tipwmss.h"
+
 /* EHRPWM registers and bits definitions */
 
 /* Time base module registers */
@@ -435,6 +437,7 @@ static int ehrpwm_pwm_probe(struct platform_device *pdev)
 	struct resource *r;
 	struct clk *clk;
 	struct ehrpwm_pwm_chip *pc;
+	u16 status;
 
 	pc = devm_kzalloc(&pdev->dev, sizeof(*pc), GFP_KERNEL);
 	if (!pc)
@@ -484,9 +487,27 @@ static int ehrpwm_pwm_probe(struct platform_device *pdev)
 	}
 
 	pm_runtime_enable(&pdev->dev);
+	pm_runtime_get_sync(&pdev->dev);
+
+	status = pwmss_submodule_state_change(pdev->dev.parent,
+			PWMSS_EPWMCLK_EN);
+	if (!(status & PWMSS_EPWMCLK_EN_ACK)) {
+		dev_err(&pdev->dev, "PWMSS config space clock enable failed\n");
+		ret = -EINVAL;
+		goto pwmss_clk_failure;
+	}
+
+	pm_runtime_put_sync(&pdev->dev);
 
 	platform_set_drvdata(pdev, pc);
 	return 0;
+
+pwmss_clk_failure:
+	pm_runtime_put_sync(&pdev->dev);
+	pm_runtime_disable(&pdev->dev);
+	pwmchip_remove(&pc->chip);
+	clk_unprepare(pc->tbclk);
+	return ret;
 }
 
 static int ehrpwm_pwm_remove(struct platform_device *pdev)
@@ -494,6 +515,14 @@ static int ehrpwm_pwm_remove(struct platform_device *pdev)
 	struct ehrpwm_pwm_chip *pc = platform_get_drvdata(pdev);
 
 	clk_unprepare(pc->tbclk);
+
+	pm_runtime_get_sync(&pdev->dev);
+	/*
+	 * Due to hardware misbehaviour, acknowledge of the stop_req
+	 * is missing. Hence checking of the status bit skipped.
+	 */
+	pwmss_submodule_state_change(pdev->dev.parent, PWMSS_EPWMCLK_STOP_REQ);
+	pm_runtime_put_sync(&pdev->dev);
 
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
