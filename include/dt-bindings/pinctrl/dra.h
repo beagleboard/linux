2814b96e337a7dee509662f561ceb12ebed0b623
Javier Martinez Canillas <javier@osg.samsung.com>
2016-04-21 11:40:42 +0530

    pinctrl: Move am4372 and dra7 macros to the the SoC header files
    commit fc63efdf4bbcc23968bc71cfbc7dedf56c5ff01e upstream.
    
    The <dt-bindings/pinctrl/omap.h> header file defines a set of macros
    for different SoCs families that falls under the OMAP sub-arch, that
    allow to define the padconf register physical address instead of the
    register offset from the padconf base.
    
    But the am43xx and dra7xx SoCs families have their own pinctrl header
    file so the DTS using these SoCs aren't able to use the AM4372_IOPAD()
    and DRA7XX_CORE_IOPAD() macros since <dt-bindings/pinctrl/omap.h> is
    not included.
    
    Move the macros to the correct header files so can be used by the DTS.
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- include/dt-bindings/pinctrl/dra.h
+++ include/dt-bindings/pinctrl/dra.h
@@ -69,6 +69,12 @@
 #define PIN_INPUT_PULLUP	(PULL_ENA | INPUT_EN | PULL_UP)
 #define PIN_INPUT_PULLDOWN	(PULL_ENA | INPUT_EN)
 
+/*
+ * Macro to allow using the absolute physical address instead of the
+ * padconf registers instead of the offset from padconf base.
+ */
+#define DRA7XX_CORE_IOPAD(pa, val)	(((pa) & 0xffff) - 0x3400) (val)
+
 /* DRA7 IODELAY configuration parameters */
 #define A_DELAY(val)		((val) & 0xFFFF)
 #define G_DELAY(val)		(((val) & 0xFFFF) << 16)
