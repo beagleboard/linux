cb87194e4f4909e90ca4d5de7c3bf3aaeb4dde3c
Andrew F. Davis <afd@ti.com>
2016-05-04 17:01:36 -0500

    rpmsg: add THIS_MODULE to rpmsg_driver in rpmsg core
    [ Upstream commit bc3c57c13256c748a4e71d2ea7481d0c953e88e1 ]
    
    Add register_rpmsg_driver helper macro that adds THIS_MODULE to
    rpmsg_driver for the registering driver. We rename and modify
    the existing register_rpmsg_driver to enable this.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Acked-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    [s-anna@ti.com: cherry-pick commit 'bc3c57c13256' from v4.7]
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- include/linux/rpmsg.h
+++ include/linux/rpmsg.h
@@ -179,7 +179,7 @@ struct rpmsg_driver {
 
 int register_rpmsg_device(struct rpmsg_channel *dev);
 void unregister_rpmsg_device(struct rpmsg_channel *dev);
-int register_rpmsg_driver(struct rpmsg_driver *drv);
+int __register_rpmsg_driver(struct rpmsg_driver *drv, struct module *owner);
 void unregister_rpmsg_driver(struct rpmsg_driver *drv);
 void rpmsg_destroy_ept(struct rpmsg_endpoint *);
 struct rpmsg_endpoint *rpmsg_create_ept(struct rpmsg_channel *,
@@ -191,6 +191,10 @@ struct rpmsg_channel *rpmsg_create_channel(struct virtproc_info *vrp,
 					   const char *name, const char *desc,
 					   int src, int dst);
 int rpmsg_destroy_channel(struct rpmsg_channel *rpdev);
+
+/* use a macro to avoid include chaining to get THIS_MODULE */
+#define register_rpmsg_driver(drv) \
+	__register_rpmsg_driver(drv, THIS_MODULE)
 
 /**
  * rpmsg_send() - send a message across to the remote processor
