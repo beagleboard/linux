06684c24bd0751add069270ce5e90486ef615c2d
Robert Nelson <robertcnelson@gmail.com>
2016-07-15 09:28:33 -0500

    backports: iio: from: linux.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- include/uapi/linux/iio/types.h
+++ include/uapi/linux/iio/types.h
@@ -37,6 +37,8 @@ enum iio_chan_type {
 	IIO_VELOCITY,
 	IIO_CONCENTRATION,
 	IIO_RESISTANCE,
+	IIO_PH,
+	IIO_UVINDEX,
 };
 
 enum iio_modifier {
@@ -76,6 +78,7 @@ enum iio_modifier {
 	IIO_MOD_Q,
 	IIO_MOD_CO2,
 	IIO_MOD_VOC,
+	IIO_MOD_LIGHT_UV,
 };
 
 enum iio_event_type {
