7ff1063a6854038e7ec89f21643f4018bef643ce
Grygorii Strashko <grygorii.strashko@ti.com>
2016-07-21 12:57:20 +0300

    Revert "drivers: net: cpsw: ethtool: fix accessing to suspended device"
    This reverts commit d63c824088246b6e91839dafc577d56047e36805 which
    will be replaced by more correct upstream version and to avoid future
    merge conflicts.
    
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/net/ethernet/ti/cpsw.c
+++ drivers/net/ethernet/ti/cpsw.c
@@ -933,13 +933,6 @@ static int cpsw_set_coalesce(struct net_device *ndev,
 	u32 prescale = 0;
 	u32 addnl_dvdr = 1;
 	u32 coal_intvl = 0;
-	int ret;
-
-	ret = pm_runtime_get_sync(&priv->pdev->dev);
-	if (ret < 0) {
-		pm_runtime_put_noidle(&priv->pdev->dev);
-		return ret;
-	}
 
 	coal_intvl = coal->rx_coalesce_usecs;
 
@@ -994,8 +987,6 @@ update_return:
 		priv->coal_intvl = coal_intvl;
 	}
 
-	pm_runtime_put(&priv->pdev->dev);
-
 	return 0;
 }
 
@@ -1033,13 +1024,7 @@ static void cpsw_get_ethtool_stats(struct net_device *ndev,
 	struct cpdma_chan_stats tx_stats;
 	u32 val;
 	u8 *p;
-	int i, ret;
-
-	ret = pm_runtime_get_sync(&priv->pdev->dev);
-	if (ret < 0) {
-		pm_runtime_put_noidle(&priv->pdev->dev);
-		return;
-	}
+	int i;
 
 	/* Collect Davinci CPDMA stats for Rx and Tx Channel */
 	cpdma_chan_get_stats(priv->rxch, &rx_stats);
@@ -1066,8 +1051,6 @@ static void cpsw_get_ethtool_stats(struct net_device *ndev,
 			break;
 		}
 	}
-
-	pm_runtime_put(&priv->pdev->dev);
 }
 
 static int cpsw_common_res_usage_state(struct cpsw_priv *priv)
@@ -2103,20 +2086,11 @@ static void cpsw_get_regs(struct net_device *ndev,
 {
 	struct cpsw_priv *priv = netdev_priv(ndev);
 	u32 *reg = p;
-	int ret;
-
-	ret = pm_runtime_get_sync(&priv->pdev->dev);
-	if (ret < 0) {
-		pm_runtime_put_noidle(&priv->pdev->dev);
-		return;
-	}
 
 	/* update CPSW IP version */
 	regs->version = priv->version;
 
 	cpsw_ale_dump(priv->ale, reg);
-
-	pm_runtime_put(&priv->pdev->dev);
 }
 
 static void cpsw_get_drvinfo(struct net_device *ndev,
@@ -2234,20 +2208,12 @@ static int cpsw_set_pauseparam(struct net_device *ndev,
 {
 	struct cpsw_priv *priv = netdev_priv(ndev);
 	bool link;
-	int ret;
-
-	ret = pm_runtime_get_sync(&priv->pdev->dev);
-	if (ret < 0) {
-		pm_runtime_put_noidle(&priv->pdev->dev);
-		return ret;
-	}
 
 	priv->rx_pause = pause->rx_pause ? true : false;
 	priv->tx_pause = pause->tx_pause ? true : false;
 
 	for_each_slave(priv, _cpsw_adjust_link, priv, &link);
 
-	pm_runtime_put(&priv->pdev->dev);
 	return 0;
 }
 
