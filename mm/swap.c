7838b0a16ae429ad74e540620b6ffdc57e940ab7
Robert Nelson <robertcnelson@gmail.com>
2016-08-04 14:42:20 -0500

    fix: 4.1.29+rt, big opps
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mm/swap.c
+++ mm/swap.c
@@ -636,7 +636,7 @@ static void __lru_cache_add(struct page *page)
 	struct pagevec *pvec = &get_locked_var(swapvec_lock, lru_add_pvec);
 
 	page_cache_get(page);
-	if (!pagevec_add(pvec, page) || PageCompound(page))
+	if (!pagevec_space(pvec) || PageCompound(page))
 		__pagevec_lru_add(pvec);
 	pagevec_add(pvec, page);
 	put_locked_var(swapvec_lock, lru_add_pvec);
