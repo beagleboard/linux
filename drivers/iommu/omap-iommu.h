46100f90509300d63f93ec8ad22a6924ae7adacd
Suman Anna <s-anna@ti.com>
2016-08-06 11:31:53 -0500

    iommu/omap: Make omap_iommu_arch_data structure private
    The omap_iommu_arch_data is used to bind a IOMMU user device to an
    OMAP IOMMU device, and was exported to support non-DT usecases. The
    OMAP IOMMU driver does not support non-DT usage anymore, so make
    this structure private.
    
    While at this, also remove an unneeded MMU_REG_SIZE macro from the
    OMAP IOMMU platform data header file.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/iommu/omap-iommu.h
+++ drivers/iommu/omap-iommu.h
@@ -52,6 +52,21 @@ struct omap_iommu {
 	u32 id;
 };
 
+/**
+ * struct omap_iommu_arch_data - omap iommu private data
+ * @name: name of the iommu device
+ * @iommu_dev: handle of the iommu device
+ *
+ * This is an omap iommu private data object, which binds an iommu user
+ * to its iommu device. This object should be placed at the iommu user's
+ * dev_archdata so generic IOMMU API can be used without having to
+ * utilize omap-specific plumbing anymore.
+ */
+struct omap_iommu_arch_data {
+	const char *name;
+	struct omap_iommu *iommu_dev;
+};
+
 struct cr_regs {
 	u32 cam;
 	u32 ram;
