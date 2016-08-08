bf27fb6c37a78a801b70b0645d99a9a8a4b7e9da
Tomi Valkeinen <tomi.valkeinen@ti.com>
2016-08-08 09:02:57 +0300

    drm/omap: fix plane update warning when crtc is disabled
    If omap_plane_atomic_update() is called when the crtc is disabled, and
    the timings are zero, we'll see the following warning:
    
    omapdss DISPC error: cannot calculate scaling settings: pclk is zero
    omapdrm omapdrm.0: Failed to setup plane vid2
    
    It shouldn't cause any issues, as the crtc is disabled so the plane is
    not used by the HW.
    
    To remove the warning, check whether the crtc is enabled at the
    beginning of omap_plane_atomic_update(), and bail out if not.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- drivers/gpu/drm/omapdrm/omap_plane.c
+++ drivers/gpu/drm/omapdrm/omap_plane.c
@@ -86,6 +86,11 @@ static void omap_plane_atomic_update(struct drm_plane *plane,
 
 	DBG("%s, crtc=%p fb=%p", omap_plane->name, state->crtc, state->fb);
 
+	if (!state->crtc->state->enable) {
+		priv->dispc_ops->ovl_enable(omap_plane->id, false);
+		return;
+	}
+
 	memset(&info, 0, sizeof(info));
 	info.rotation_type = OMAP_DSS_ROT_DMA;
 	info.rotation = OMAP_DSS_ROT_0;
