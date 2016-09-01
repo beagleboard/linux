28dd6bd01060e53db499929842646eb3d8c7c933
Cooper Jr., Franklin <fcooper@ti.com>
2016-09-01 10:50:13 +0530

    pwm: pwm-ti*: Get the clock from the PWMSS parent when using old bindings
    commit ae5200d2271691b28aff8d0409fbefd4644ff507 upstream.
    
    When using the old eCAP and ePWM bindings for AM335x and AM437x the clock
    can be retrieved from the PWMSS parent. Newer bindings will insure that
    this clock is provided via device tree.
    
    Therefore, update this driver to support the newer and older bindings. In
    the case of the older binding being used give a warning.
    
    Signed-off-by: Franklin S Cooper Jr <fcooper@ti.com>
    [thierry.reding@gmail.com: rewrite slightly for readability]
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>
    
    Signed-off-by: Vignesh R <vigneshr@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/pwm/pwm-tiecap.c
+++ drivers/pwm/pwm-tiecap.c
@@ -193,6 +193,7 @@ static const struct pwm_ops ecap_pwm_ops = {
 };
 
 static const struct of_device_id ecap_of_match[] = {
+	{ .compatible	= "ti,am3352-ecap" },
 	{ .compatible	= "ti,am33xx-ecap" },
 	{},
 };
@@ -200,6 +201,7 @@ MODULE_DEVICE_TABLE(of, ecap_of_match);
 
 static int ecap_pwm_probe(struct platform_device *pdev)
 {
+	struct device_node *np = pdev->dev.of_node;
 	int ret;
 	struct resource *r;
 	struct clk *clk;
@@ -209,7 +211,14 @@ static int ecap_pwm_probe(struct platform_device *pdev)
 	if (!pc)
 		return -ENOMEM;
 
-	clk = devm_clk_get(pdev->dev.parent, "fck");
+	clk = devm_clk_get(&pdev->dev, "fck");
+	if (IS_ERR(clk)) {
+		if (of_device_is_compatible(np, "ti,am33xx-ecap")) {
+			dev_warn(&pdev->dev, "Binding is obsolete.\n");
+			clk = devm_clk_get(pdev->dev.parent, "fck");
+		}
+	}
+
 	if (IS_ERR(clk)) {
 		dev_err(&pdev->dev, "failed to get clock\n");
 		return PTR_ERR(clk);
