6b76b483aa0561e7deb3a7400211eb254881a4d9
Grygorii Strashko <grygorii.strashko@ti.com>
2016-09-26 22:27:34 +0300

    net: ethernet: ti: cpts: fix overflow check period
    The CPTS drivers uses 8sec period for overflow checking with
    assumption that CPTS retclk will not exceed 500MHz. But that's not
    true on some TI platforms (Kesytone 2). As result, it is possible that
    CPTS counter will overflow more than once between two readings.
    
    Hence, fix it by selecting overflow check period dynamically as
    max_sec_before_overflow/2, where
     max_sec_before_overflow = max_counter_val / rftclk_freq.
    
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/net/ethernet/ti/cpts.c
+++ drivers/net/ethernet/ti/cpts.c
@@ -247,7 +247,7 @@ static void cpts_overflow_check(struct work_struct *work)
 	cpts_write32(cpts, TS_PEND_EN, int_enable);
 	cpts_ptp_gettime(&cpts->info, &ts);
 	pr_debug("cpts overflow check at %lld.%09lu\n", ts.tv_sec, ts.tv_nsec);
-	schedule_delayed_work(&cpts->overflow_work, CPTS_OVERFLOW_PERIOD);
+	schedule_delayed_work(&cpts->overflow_work, cpts->ov_check_period);
 }
 
 static int cpts_match(struct sk_buff *skb, unsigned int ptp_class,
@@ -386,7 +386,7 @@ int cpts_register(struct cpts *cpts)
 	}
 	cpts->phc_index = ptp_clock_index(cpts->clock);
 
-	schedule_delayed_work(&cpts->overflow_work, CPTS_OVERFLOW_PERIOD);
+	schedule_delayed_work(&cpts->overflow_work, cpts->ov_check_period);
 	return 0;
 
 err_ptp:
@@ -425,6 +425,11 @@ static void cpts_calc_mult_shift(struct cpts *cpts)
 	do_div(maxsec, freq);
 	if (maxsec > 600 && cpts->cc.mask > UINT_MAX)
 		maxsec = 600;
+
+	/* Calc overflow check period (maxsec / 2) */
+	cpts->ov_check_period = (HZ * maxsec) / 2;
+	dev_info(cpts->dev, "cpts: overflow check period %lu\n",
+		 cpts->ov_check_period);
 
 	if (cpts->cc_mult || cpts->cc.shift)
 		return;
