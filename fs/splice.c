a392825998b0de0f6dd0afc384f09da4b17d36a9
Robert Nelson <robertcnelson@gmail.com>
2016-09-29 09:16:04 -0500

    merge: aufs4-standalone
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fs/splice.c
+++ fs/splice.c
@@ -1126,6 +1126,7 @@ long do_splice_from(struct pipe_inode_info *pipe, struct file *out,
 
 	return splice_write(pipe, out, ppos, len, flags);
 }
+EXPORT_SYMBOL_GPL(do_splice_from);
 
 /*
  * Attempt to initiate a splice from a file to a pipe.
@@ -1152,6 +1153,7 @@ long do_splice_to(struct file *in, loff_t *ppos,
 
 	return splice_read(in, ppos, pipe, len, flags);
 }
+EXPORT_SYMBOL_GPL(do_splice_to);
 
 /**
  * splice_direct_to_actor - splices data directly between two non-pipes
