2f81eefb4638fa6d666505721204445dec48507d
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2016-10-04 12:10:27 +0530

    mtd: nand: omap2: fix return value check in omap_nand_probe()
    commit de3bfc4a16165cfc5f1504981f836d39a5f39a64 upstream
    
    In case of error, the function dma_request_chan() returns ERR_PTR() and
    never returns NULL. The NULL test in the return value check should be
    replaced with IS_ERR().
    
    Fixes: aa7abd312c11 ('mtd: nand: omap2: Support parsing dma channel
    information from DT')
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/mtd/nand/omap2.c
+++ drivers/mtd/nand/omap2.c
@@ -1844,9 +1844,9 @@ static int omap_nand_probe(struct platform_device *pdev)
 		sig = OMAP24XX_DMA_GPMC;
 		info->dma = dma_request_chan(pdev->dev.parent, "rxtx");
 
-		if (!info->dma) {
+		if (IS_ERR(info->dma)) {
 			dev_err(&pdev->dev, "DMA engine request failed\n");
-			err = -ENXIO;
+			err = PTR_ERR(info->dma);
 			goto return_error;
 		} else {
 			struct dma_slave_config cfg;
