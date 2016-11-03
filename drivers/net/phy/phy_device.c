9fe690a428abf54e0fe7238cf65dee8f99b189da
Johan Hovold <johan@kernel.org>
2016-11-03 18:40:19 +0100

    phy: fix device reference leaks
    commit 17ae1c650c1ecf8dc8e16d54b0f68a345965f43f upstream.
    
    Make sure to drop the reference taken by bus_find_device_by_name()
    before returning from phy_connect() and phy_attach().
    
    Note that both function still take a reference to the phy device
    through phy_attach_direct().
    
    Fixes: e13934563db0 ("[PATCH] PHY Layer fixup")
    Cc: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -522,6 +522,7 @@ struct phy_device *phy_connect(struct net_device *dev, const char *bus_id,
 	phydev = to_phy_device(d);
 
 	rc = phy_connect_direct(dev, phydev, handler, interface);
+	put_device(d);
 	if (rc)
 		return ERR_PTR(rc);
 
@@ -721,6 +722,7 @@ struct phy_device *phy_attach(struct net_device *dev, const char *bus_id,
 	phydev = to_phy_device(d);
 
 	rc = phy_attach_direct(dev, phydev, phydev->dev_flags, interface);
+	put_device(d);
 	if (rc)
 		return ERR_PTR(rc);
 
