b49836a9afb0bdc1f5611cfdf9ddc181b9a7ad2a
Robert Nelson <robertcnelson@gmail.com>
2016-11-10 11:30:30 -0600

    brcmfmac: ism4343
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/net/wireless/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/brcm80211/brcmfmac/sdio.c
@@ -3717,6 +3717,11 @@ brcmf_sdio_drivestrengthinit(struct brcmf_sdio_dev *sdiodev,
 		str_mask = 0x00001800;
 		str_shift = 11;
 		break;
+	case SDIOD_DRVSTR_KEY(BRCM_CC_43430_CHIP_ID, 24):
+		str_tab = sdiod_drvstr_tab6_1v8;
+		str_mask = 0x00001800;
+		str_shift = 11;
+		break;
 	case SDIOD_DRVSTR_KEY(BRCM_CC_43143_CHIP_ID, 17):
 		/* note: 43143 does not support tristate */
 		i = ARRAY_SIZE(sdiod_drvstr_tab2_3v3) - 1;
