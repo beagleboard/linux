0176d4767c7bd6c6419fed801cf515ec73746ffa
Robert Nelson <robertcnelson@gmail.com>
2016-12-15 13:38:42 -0600

    backports: fbtft: from: linux.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/staging/fbtft/fb_ssd1305.c
+++ drivers/staging/fbtft/fb_ssd1305.c
@@ -1,7 +1,7 @@
 /*
- * FB driver for the SSD1306 OLED Controller
+ * FB driver for the SSD1305 OLED Controller
  *
- * Copyright (C) 2013 Noralf Tronnes
+ * based on SSD1306 driver by Noralf Tronnes
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,19 +22,20 @@
 
 #include "fbtft.h"
 
-#define DRVNAME		"fb_ssd1306"
-#define WIDTH		128
-#define HEIGHT		64
+#define DRVNAME		"fb_ssd1305"
+
+#define WIDTH 128
+#define HEIGHT 64
 
 /*
  * write_reg() caveat:
  *
- * This doesn't work because D/C has to be LOW for both values:
- * write_reg(par, val1, val2);
+ *    This doesn't work because D/C has to be LOW for both values:
+ *      write_reg(par, val1, val2);
  *
- * Do it like this:
- * write_reg(par, val1);
- * write_reg(par, val2);
+ *    Do it like this:
+ *      write_reg(par, val1);
+ *      write_reg(par, val2);
  */
 
 /* Init sequence taken from the Adafruit SSD1306 Arduino library */
@@ -82,39 +83,42 @@ static int init_display(struct fbtft_par *par)
 	/* Vertical addressing mode  */
 	write_reg(par, 0x01);
 
-	/*Set Segment Re-map */
-	/* column address 127 is mapped to SEG0 */
-	write_reg(par, 0xA0 | 0x1);
+	/*
+	 * Set Segment Re-map
+	 * column address 127 is mapped to SEG0
+	 */
+	write_reg(par, 0xA0 | ((par->info->var.rotate == 180) ? 0x0 : 0x1));
 
-	/* Set COM Output Scan Direction */
-	/* remapped mode. Scan from COM[N-1] to COM0 */
-	write_reg(par, 0xC8);
+	/*
+	 * Set COM Output Scan Direction
+	 * remapped mode. Scan from COM[N-1] to COM0
+	 */
+	write_reg(par, ((par->info->var.rotate == 180) ? 0xC8 : 0xC0));
 
 	/* Set COM Pins Hardware Configuration */
 	write_reg(par, 0xDA);
-	if (par->info->var.yres == 64)
+	if (par->info->var.yres == 64) {
 		/* A[4]=1b, Alternative COM pin configuration */
 		write_reg(par, 0x12);
-	else
+	} else {
 		/* A[4]=0b, Sequential COM pin configuration */
 		write_reg(par, 0x02);
+	}
 
 	/* Set Pre-charge Period */
 	write_reg(par, 0xD9);
 	write_reg(par, 0xF1);
 
-	/* Set VCOMH Deselect Level */
-	write_reg(par, 0xDB);
-	/* according to the datasheet, this value is out of bounds */
-	write_reg(par, 0x40);
-
-	/* Entire Display ON */
-	/* Resume to RAM content display. Output follows RAM content */
+	/*
+	 * Entire Display ON
+	 * Resume to RAM content display. Output follows RAM content
+	 */
 	write_reg(par, 0xA4);
 
-	/* Set Normal Display
-	 * 0 in RAM: OFF in display panel
-	 * 1 in RAM: ON in display panel
+	/*
+	 * Set Normal Display
+	 *  0 in RAM: OFF in display panel
+	 *  1 in RAM: ON in display panel
 	 */
 	write_reg(par, 0xA6);
 
@@ -127,7 +131,7 @@ static int init_display(struct fbtft_par *par)
 static void set_addr_win(struct fbtft_par *par, int xs, int ys, int xe, int ye)
 {
 	/* Set Lower Column Start Address for Page Addressing Mode */
-	write_reg(par, 0x00 | 0x0);
+	write_reg(par, 0x00 | ((par->info->var.rotate == 180) ? 0x0 : 0x4));
 	/* Set Higher Column Start Address for Page Addressing Mode */
 	write_reg(par, 0x10 | 0x0);
 	/* Set Display Start Line */
@@ -136,9 +140,6 @@ static void set_addr_win(struct fbtft_par *par, int xs, int ys, int xe, int ye)
 
 static int blank(struct fbtft_par *par, bool on)
 {
-	fbtft_par_dbg(DEBUG_BLANK, par, "%s(blank=%s)\n",
-		__func__, on ? "true" : "false");
-
 	if (on)
 		write_reg(par, 0xAE);
 	else
@@ -149,9 +150,7 @@ static int blank(struct fbtft_par *par, bool on)
 /* Gamma is used to control Contrast */
 static int set_gamma(struct fbtft_par *par, unsigned long *curves)
 {
-	/* apply mask */
 	curves[0] &= 0xFF;
-
 	/* Set Contrast Control for BANK0 */
 	write_reg(par, 0x81);
 	write_reg(par, curves[0]);
@@ -164,10 +163,10 @@ static int write_vmem(struct fbtft_par *par, size_t offset, size_t len)
 	u16 *vmem16 = (u16 *)par->info->screen_buffer;
 	u8 *buf = par->txbuf.buf;
 	int x, y, i;
-	int ret = 0;
+	int ret;
 
 	for (x = 0; x < par->info->var.xres; x++) {
-		for (y = 0; y < par->info->var.yres/8; y++) {
+		for (y = 0; y < par->info->var.yres / 8; y++) {
 			*buf = 0x00;
 			for (i = 0; i < 8; i++)
 				*buf |= (vmem16[(y * 8 + i) *
@@ -185,7 +184,6 @@ static int write_vmem(struct fbtft_par *par, size_t offset, size_t len)
 	if (ret < 0)
 		dev_err(par->info->device, "write failed and returned: %d\n",
 			ret);
-
 	return ret;
 }
 
@@ -193,6 +191,7 @@ static struct fbtft_display display = {
 	.regwidth = 8,
 	.width = WIDTH,
 	.height = HEIGHT,
+	.txbuflen = WIDTH * HEIGHT / 8,
 	.gamma_num = 1,
 	.gamma_len = 1,
 	.gamma = "00",
@@ -205,13 +204,13 @@ static struct fbtft_display display = {
 	},
 };
 
-FBTFT_REGISTER_DRIVER(DRVNAME, "solomon,ssd1306", &display);
+FBTFT_REGISTER_DRIVER(DRVNAME, "solomon,ssd1305", &display);
 
 MODULE_ALIAS("spi:" DRVNAME);
 MODULE_ALIAS("platform:" DRVNAME);
-MODULE_ALIAS("spi:ssd1306");
-MODULE_ALIAS("platform:ssd1306");
+MODULE_ALIAS("spi:ssd1305");
+MODULE_ALIAS("platform:ssd1305");
 
-MODULE_DESCRIPTION("SSD1306 OLED Driver");
-MODULE_AUTHOR("Noralf Tronnes");
+MODULE_DESCRIPTION("SSD1305 OLED Driver");
+MODULE_AUTHOR("Alexey Mednyy");
 MODULE_LICENSE("GPL");
