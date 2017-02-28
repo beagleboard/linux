9abd4dfec49ce2cfd64a2e7358095b446a03b898
Robert Nelson <robertcnelson@gmail.com>
2017-02-28 11:05:05 -0600

    merge: aufs4-standalone
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -64,6 +64,7 @@ int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
 	mutex_unlock(&dentry->d_inode->i_mutex);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(do_truncate);
 
 long vfs_truncate(struct path *path, loff_t length)
 {
@@ -678,6 +679,7 @@ int open_check_o_direct(struct file *f)
 	}
 	return 0;
 }
+EXPORT_SYMBOL_GPL(open_check_o_direct);
 
 static int do_dentry_open(struct file *f,
 			  struct inode *inode,
