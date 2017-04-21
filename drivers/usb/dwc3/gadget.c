da7147b98ba33fead77f3421c9456ffaa25ab7c9
Roger Quadros <rogerq@ti.com>
2017-04-21 15:38:09 +0300

    usb: dwc3: gadget: Fix ISO transfer performance
    Commit 08a36b543803 ("usb: dwc3: gadget: simplify __dwc3_gadget_ep_queue()")
    caused a small change in the way ISO transfer is handled in the case
    when XferInProgress event happens on Isoc EP with an active transfer.
    This caused a performance degradation of 50%. e.g. using g_webcam on DUT
    and luvcview on host the video frame rate dropped from 16fps to 8fps
    @high-speed.
    
    Make the ISO transfer handling equivalent to that prior to that commit
    to get back the original ISO performance numbers.
    
    Fixes: 08a36b543803 ("usb: dwc3: gadget: simplify __dwc3_gadget_ep_queue()")
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1104,18 +1104,30 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 	 * errors which will force us issue EndTransfer command.
 	 */
 	if (usb_endpoint_xfer_isoc(dep->endpoint.desc)) {
-		if ((dep->flags & DWC3_EP_PENDING_REQUEST) &&
-				list_empty(&dep->started_list)) {
-			dwc3_stop_active_transfer(dwc, dep->number, true);
-			dep->flags = DWC3_EP_ENABLED;
+		if (dep->flags & DWC3_EP_PENDING_REQUEST) {
+			if (list_empty(&dep->started_list)) {
+				dwc3_stop_active_transfer(dwc, dep->number,
+							  true);
+				dep->flags = DWC3_EP_ENABLED;
+			}
+			return 0;
 		}
-		return 0;
+
+		if ((dep->flags & DWC3_EP_BUSY) &&
+		    !(dep->flags & DWC3_EP_MISSED_ISOC)) {
+			WARN_ON_ONCE(!dep->resource_index);
+			ret = __dwc3_gadget_kick_transfer(dep,
+							  dep->resource_index);
+		}
+
+		goto out;
 	}
 
 	if (!dwc3_calc_trbs_left(dep))
 		return 0;
 
 	ret = __dwc3_gadget_kick_transfer(dep, 0);
+out:
 	if (ret && ret != -EBUSY)
 		dwc3_trace(trace_dwc3_gadget,
 				"%s: failed to kick transfers",
