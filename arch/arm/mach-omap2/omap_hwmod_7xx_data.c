f5228c8dcbb3f8476443c15e52f54ea739b34178
Lokesh Vutla <lokeshvutla@ti.com>
2017-07-31 11:22:28 +0530

    ARM: dra762: hwmod: Add MCAN support
    commit d28d665d21d6 ("ARM: dra762: hwmod: Add MCAN support") branch ti-linux-4.9.y
    
    Add MCAN hwmod data and register it for dra762 silicons.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_7xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_7xx_data.c
@@ -1667,6 +1667,29 @@ static struct omap_hwmod dra7xx_mailbox13_hwmod = {
 };
 
 /*
+ * 'mcan' class
+ *
+ */
+static struct omap_hwmod_class dra76x_mcan_hwmod_class = {
+	.name	= "mcan",
+};
+
+/* mcan */
+static struct omap_hwmod dra76x_mcan_hwmod = {
+	.name		= "mcan",
+	.class		= &dra76x_mcan_hwmod_class,
+	.clkdm_name	= "wkupaon_clkdm",
+	.main_clk	= "mcan_clk",
+	.prcm = {
+		.omap4 = {
+			.clkctrl_offs = DRA7XX_CM_WKUPAON_ADC_CLKCTRL_OFFSET,
+			.context_offs = DRA7XX_RM_WKUPAON_ADC_CONTEXT_OFFSET,
+			.modulemode   = MODULEMODE_SWCTRL,
+		},
+	},
+};
+
+/*
  * 'mcspi' class
  *
  */
@@ -3294,7 +3317,6 @@ static struct omap_hwmod dra7xx_wd_timer2_hwmod = {
 	},
 };
 
-
 /*
  * Interfaces
  */
@@ -4576,6 +4598,14 @@ static struct omap_hwmod_ocp_if dra7xx_l4_wkup__wd_timer2 = {
 	.user		= OCP_USER_MPU | OCP_USER_SDMA,
 };
 
+/* l3_main_1 -> mcan */
+static struct omap_hwmod_ocp_if dra76x_l3_main_1__mcan = {
+	.master		= &dra7xx_l3_main_1_hwmod,
+	.slave		= &dra76x_mcan_hwmod,
+	.clk		= "l3_iclk_div",
+	.user		= OCP_USER_MPU | OCP_USER_SDMA,
+};
+
 static struct omap_hwmod_ocp_if *dra7xx_hwmod_ocp_ifs[] __initdata = {
 	&dra7xx_l3_main_1__dmm,
 	&dra7xx_l3_main_2__l3_instr,
@@ -4732,6 +4762,7 @@ static struct omap_hwmod_ocp_if *dra76x_hwmod_ocp_ifs[] __initdata = {
 	&dra7xx_l3_main_1__mmu0_dsp2,
 	&dra7xx_l3_main_1__mmu1_dsp2,
 	&dra7xx_dsp2__l3_main_1,
+	&dra76x_l3_main_1__mcan,
 	&dra76x_l4_per3__cal,
 	NULL,
 };
