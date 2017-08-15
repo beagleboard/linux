376a06bf47cf6382f7771fb1221742b32c6b293e
Chi-Hsien Lin <chi-hsien.lin@cypress.com>
2017-08-15 00:25:14 -0500

    brcmfmac: Add sg parameters dts parsing
    broken_sg_support, sd_head_align, and sd_sgentry_align are used in
    brcmfmac code but not configurable in dts file. Add the parsing logic.
    Now they can be configured like below in dts:
    	brcm,broken_sg_support;
    	brcm,sd_head_align = /bits/ 16 <4>;
    	brcm,sd_sgentry_align = /bits/ 16 <4>;
    
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.c
@@ -31,6 +31,7 @@ void brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 	int irq;
 	u32 irqf;
 	u32 val;
+	u16 val16;
 
 	if (!np || bus_type != BRCMF_BUSTYPE_SDIO ||
 	    !of_device_is_compatible(np, "brcm,bcm4329-fmac"))
@@ -38,6 +39,13 @@ void brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 
 	if (of_property_read_u32(np, "brcm,drive-strength", &val) == 0)
 		sdio->drive_strength = val;
+
+	sdio->broken_sg_support = of_property_read_bool(np,
+			"brcm,broken_sg_support");
+	if (of_property_read_u16(np, "brcm,sd_head_align", &val16) == 0)
+		sdio->sd_head_align = val16;
+	if (of_property_read_u16(np, "brcm,sd_sgentry_align", &val16) == 0)
+		sdio->sd_sgentry_align = val16;
 
 	/* make sure there are interrupts defined in the node */
 	if (!of_find_property(np, "interrupts", NULL))
