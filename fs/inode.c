ebc0d0a4063af64163035d9f867ef480beef38c4
Robert Nelson <robertcnelson@gmail.com>
2017-10-03 22:09:01 -0500

    merge: aufs4-standalone
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fs/inode.c
+++ fs/inode.c
@@ -1652,6 +1652,7 @@ int update_time(struct inode *inode, struct timespec *time, int flags)
 
 	return update_time(inode, time, flags);
 }
+EXPORT_SYMBOL_GPL(update_time);
 
 /**
  *	touch_atime	-	update the access time
