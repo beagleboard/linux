1a51b3c3d3bca5889cdba23e0961385e83981976
Robert Nelson <robertcnelson@gmail.com>
2017-10-23 12:41:25 -0500

    merge: aufs4-standalone
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -1300,6 +1300,7 @@ rename_retry:
 	seq = 1;
 	goto again;
 }
+EXPORT_SYMBOL_GPL(d_walk);
 
 /*
  * Search for at least 1 mount point in the dentry's subdirs.
@@ -2886,6 +2887,7 @@ void d_exchange(struct dentry *dentry1, struct dentry *dentry2)
 
 	write_sequnlock(&rename_lock);
 }
+EXPORT_SYMBOL_GPL(d_exchange);
 
 /**
  * d_ancestor - search for an ancestor
