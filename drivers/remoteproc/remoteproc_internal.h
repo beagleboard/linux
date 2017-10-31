a5b40e544777063ae7a943fa89d86c91f448e92c
Suman Anna <s-anna@ti.com>
2017-10-31 14:30:44 +0530

    remoteproc: move rproc_da_to_va declaration to remoteproc.h
    The rproc_da_to_va() API is an exported function, so move its
    declaration from the remoteproc local remoteproc_internal.h
    to the public remoteproc.h file.
    
    This will allow drivers outside of the remoteproc folder to be
    able to use this API. Without this, a build issue is seen when
    this API is used from the wkup_m3_ipc driver on TI AM335x/AM437x
    SoCs.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Keerthy <j-keerthy@ti.com>

--- drivers/remoteproc/remoteproc_internal.h
+++ drivers/remoteproc/remoteproc_internal.h
@@ -71,7 +71,6 @@ void rproc_exit_sysfs(void);
 void rproc_free_vring(struct rproc_vring *rvring);
 int rproc_alloc_vring(struct rproc_vdev *rvdev, int i);
 
-void *rproc_da_to_va(struct rproc *rproc, u64 da, int len);
 int rproc_trigger_recovery(struct rproc *rproc);
 
 static inline
