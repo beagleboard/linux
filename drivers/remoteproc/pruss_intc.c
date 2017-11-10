daf02af249dbd9124b14fb65b8ca13206e28eb75
Suman Anna <s-anna@ti.com>
2017-11-10 11:57:06 -0600

    remoteproc/pruss: add support for PRU-ICSS subsystems on 66AK2G SoC
    The 66AK2G SoC supports two PRU-ICSS instances, named PRUSS0 and PRUSS1,
    each of which has two PRU processor cores. The two PRU-ICSS instances
    are identical to each other with few minor SoC integration differences,
    and are very similar to the PRU-ICSS1 of AM57xx/AM43xx. The Shared Data
    RAM size is larger and the number of interrupts coming into MPU INTC
    is like the instances on AM437x. There are also few other differences
    attributing to integration in Keystone architecture (like no SYSCFG
    register or PRCM handshake protocols). Other IP level differences
    include different constant table, differences in system event interrupt
    input sources etc. They also do not have a programmable module reset
    line like those present on AM33xx/AM43xx SoCs. The modules are reset
    just like any other IP with the SoC's global cold/warm resets.
    
    The existing PRUSS platform drivers have been enhanced to support
    these 66AK2G PRU-ICSS instances through new 66AK2G specific compatibles
    for properly probing and booting all the different PRU cores in each
    PRU-ICSS processor subsystem. The SoC-level integration differences
    are dealt with using match data within the PRUSS INTC and PRUSS SoC
    bus drivers. A build dependency with ARCH_KEYSTONE is added to enable
    the driver to be built in K2G-only configuration. The initial names
    for the firmware images for each PRU core are retrieved from DT nodes,
    and can be adjusted through sysfs if required.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/remoteproc/pruss_intc.c
+++ drivers/remoteproc/pruss_intc.c
@@ -535,6 +535,10 @@ static const struct pruss_intc_match_data am437x_pruss_intc_data = {
 	.no_host7_intr = true,
 };
 
+static const struct pruss_intc_match_data k2g_pruss_intc_data = {
+	.no_host7_intr = true,
+};
+
 static const struct of_device_id pruss_intc_of_match[] = {
 	{
 		.compatible = "ti,am3356-pruss-intc",
@@ -547,6 +551,10 @@ static const struct of_device_id pruss_intc_of_match[] = {
 	{
 		.compatible = "ti,am5728-pruss-intc",
 		.data = NULL,
+	},
+	{
+		.compatible = "ti,k2g-pruss-intc",
+		.data = &k2g_pruss_intc_data,
 	},
 	{ /* sentinel */ },
 };
