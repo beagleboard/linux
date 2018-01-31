e887f2d287b44f49e10b2b2997e2023dc4981bd0
Madhan Mohan R <MadhanMohan.R@cypress.com>
2018-01-31 15:35:45 +0530

    brcmfmac: Set SDIO F1 MesBusyCtrl for CYW4373
    Along with F2 watermark (existing) configuration, F1 MesBusyCtrl
    should be enabled & configured to avoid overflow errors.
    
    Signed-off-by: Madhan Mohan R <madhanmohan.r@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -53,6 +53,7 @@
 
 #define DEFAULT_F2_WATERMARK    0x8
 #define CY_4373_F2_WATERMARK    0x40
+#define CY_4373_F1_MESBUSYCTRL  (CY_4373_F2_WATERMARK | SBSDIO_MESBUSYCTRL_ENAB)
 #define CY_43012_F2_WATERMARK    0x60
 
 #ifdef DEBUG
@@ -4323,6 +4324,7 @@ static void brcmf_sdio_firmware_callback(struct device *dev, int err,
 			devctl = brcmf_sdiod_regrb(sdiodev, SBSDIO_DEVICE_CTL, &err);
 			devctl |= SBSDIO_DEVCTL_F2WM_ENAB;
 			brcmf_sdiod_regwb(sdiodev, SBSDIO_DEVICE_CTL, devctl, &err);
+			brcmf_sdiod_regwb(sdiodev, SBSDIO_FUNC1_MESBUSYCTRL, CY_4373_F1_MESBUSYCTRL, &err);
 			break;
 		case SDIO_DEVICE_ID_CYPRESS_43012:
 			brcmf_dbg(INFO, "set F2 watermark to 0x%x*4 bytes for 43012\n",
