952ddf3400724301cb17cfbe20112e0cce04b7b6
Wright Feng <wright.feng@cypress.com>
2018-03-02 00:45:32 -0600

    brcmfmac: make firmware eap_restrict a module parameter
    When eap_restrict is enabled, firmware will toss non-802.1x frames from
    tx/rx data path if station not yet authorized.
    Internal firmware eap_restrict is disabled by default. This patch makes
    it possible to enable firmware eap_restrict by specifying
    eap_restrict=1 as module parameter.
    
    Signed-off-by: Wright Feng <wright.feng@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
@@ -49,6 +49,7 @@ extern struct brcmf_mp_global_t brcmf_mp_global;
  * @feature_disable: Feature_disable bitmask.
  * @fcmode: FWS flow control.
  * @roamoff: Firmware roaming off?
+ * @eap_restrict: Not allow data tx/rx until 802.1X auth succeeds
  * @ignore_probe_fail: Ignore probe failure.
  * @country_codes: If available, pointer to struct for translating country codes
  * @bus: Bus specific platform data. Only SDIO at the mmoment.
@@ -59,6 +60,7 @@ struct brcmf_mp_device {
 	int		fcmode;
 	bool		roamoff;
 	bool		iapp;
+	bool		eap_restrict;
 	bool		ignore_probe_fail;
 	struct brcmfmac_pd_cc *country_codes;
 	union {
