d39292d33ad8f575db3b266fead6ee5dcf5d74ab
Robert Nelson <robertcnelson@gmail.com>
2018-05-07 15:57:45 -0500

    merge: WireGuard
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ net/wireguard/netlink.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2015-2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ */
+
+#ifndef _WG_NETLINK_H
+#define _WG_NETLINK_H
+
+int genetlink_init(void);
+void genetlink_uninit(void);
+
+#endif /* _WG_NETLINK_H */
