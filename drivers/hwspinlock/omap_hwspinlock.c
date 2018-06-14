RobertCNelson/ti-linux-kernel

hwspinlock/omap: Add support for TI K3 SoCs
The HwSpinlock IP is also present on the newer TI K3 AM6x family of
SoCs. Reuse the existing OMAP Hwspinlock driver to extend the support
for this IP on K3 AM6x SoCs as well.

TODO:
Add missing power-domains property when ti-sci becomes available

Signed-off-by: Suman Anna <s-anna@ti.com>

drivers/hwspinlock/omap_hwspinlock.c
@@ -179,6 +179,7 @@ static int omap_hwspinlock_remove(struct platform_device *pdev)
 
 static const struct of_device_id omap_hwspinlock_of_match[] = {
 	{ .compatible = "ti,omap4-hwspinlock", },
+	{ .compatible = "ti,k3-hwspinlock", },
 	{ /* end */ },
 };
 MODULE_DEVICE_TABLE(of, omap_hwspinlock_of_match);

beagleboard/linux

drivers/hwspinlock/omap_hwspinlock.c
@@ -182,6 +182,7 @@ static int omap_hwspinlock_remove(struct platform_device *pdev)
 
 static const struct of_device_id omap_hwspinlock_of_match[] = {
 	{ .compatible = "ti,omap4-hwspinlock", },
+	{ .compatible = "ti,am654-hwspinlock", },
 	{ /* end */ },
 };
 MODULE_DEVICE_TABLE(of, omap_hwspinlock_of_match);