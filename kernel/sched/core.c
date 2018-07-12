40cd433f48d41b99e176d4fafe4a1666bc8f351d
Robert Nelson <robertcnelson@gmail.com>
2018-07-12 14:06:49 -0500

    Revert "sched/core: Require cpu_active() in select_task_rq(), for user tasks"
    This reverts commit 0d5e04e239ad5b18c4099ef942843bf510af1122.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1573,7 +1573,8 @@ int select_task_rq(struct task_struct *p, int cpu, int sd_flags, int wake_flags)
 	 * [ this allows ->select_task() to simply return task_cpu(p) and
 	 *   not worry about this generic constraint ]
 	 */
-	if (unlikely(!is_cpu_allowed(p, cpu)))
+	if (unlikely(!cpumask_test_cpu(cpu, &p->cpus_allowed) ||
+		     !cpu_online(cpu)))
 		cpu = select_fallback_rq(task_cpu(p), p);
 
 	return cpu;
