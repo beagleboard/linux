ac558d8362b9fcaa5e209fe7b62edba7f7e6a732
Lokesh Vutla <lokeshvutla@ti.com>
2018-08-28 15:35:30 +0530

    irqchip: ti-sci-inta: Update docs and misc fixes
    Update the doc for each structure and function within the driver.
    Also drop a redundant variable from ti_sci_inta_irq_desc structure.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- drivers/irqchip/irq-ti-sci-inta.c
+++ drivers/irqchip/irq-ti-sci-inta.c
@@ -18,6 +18,17 @@
 
 #define MAX_EVENTS_PER_VINT	64
 
+/**
+ * struct ti_sci_inta_irq_domain - Structure representing a TISCI based
+ *				   Interrupt Aggregator IRQ domain.
+ * @sci:	Pointer to TISCI handle
+ * @vint:	TISCI resource pointer representing IA inerrupts.
+ * @global_event:TISCI resource pointer representing global events.
+ * @dst_irq:	TISCI resource pointer representing destination irq controller.
+ * @base:	Base address of the memory mapped IO registers
+ * @ia_id:	TISCI device ID of this Interrupt Aggregator.
+ * @dst_id:	TISCI device ID of the destination irq controller.
+ */
 struct ti_sci_inta_irq_domain {
 	const struct ti_sci_handle *sci;
 	struct ti_sci_resource *vint;
@@ -28,14 +39,29 @@ struct ti_sci_inta_irq_domain {
 	u16 dst_id;
 };
 
+/**
+ * struct ti_sci_inta_event_desc - Description of an event coming to
+ *				   Interrupt Aggregator.
+ * @global_event:	Global event number corresponding to this event
+ * @src_id:		TISCI device ID of the event source
+ * @src_index:		Event source index within the device.
+ */
 struct ti_sci_inta_event_desc {
 	u16 global_event;
 	u16 src_id;
 	u16 src_index;
 };
 
+/**
+ * struct ti_sci_inta_vint_desc - Description of a virtual interrupt coming out
+ *				  of Interrupt Aggregator.
+ * @dst_host_irq:	Destination host IRQ.
+ * @vint:		interrupt number with in the Interrupt Aggregator.
+ * @lock:		lock to guard the event map
+ * @event_map:		Bitmap to manage the allocation of events to vint.
+ * @events:		Array of event descriptors assigned to this vint.
+ */
 struct ti_sci_inta_vint_desc {
-	u16 dst_id;
 	u16 dst_host_irq;
 	u16 vint;
 	raw_spinlock_t lock;
@@ -53,6 +79,16 @@ static struct irq_chip ti_sci_inta_irq_chip = {
 	.irq_set_affinity	= irq_chip_set_affinity_parent,
 };
 
+/**
+ * ti_sci_inta_irq_domain_translate() - Retrieve hwirq and type from
+ *					IRQ firmware specific handler.
+ * @domain:	Pointer to IRQ domain
+ * @fwspec:	Pointer to IRQ specific firmware structure
+ * @hwirq:	IRQ number identified by hardware
+ * @type:	IRQ type
+ *
+ * Return 0 if all went ok else appropriate error.
+ */
 static int ti_sci_inta_irq_domain_translate(struct irq_domain *domain,
 					    struct irq_fwspec *fwspec,
 					    unsigned long *hwirq,
@@ -71,6 +107,12 @@ static int ti_sci_inta_irq_domain_translate(struct irq_domain *domain,
 	return -EINVAL;
 }
 
+/**
+ * ti_sci_free_event_irq() - Free an event from vint
+ * @inta:	Pointer to Interrupt Aggregator IRQ domain
+ * @vint:	Virtual interrupt descriptor containing the event.
+ * @event_index:	Event Index within the vint.
+ */
 static void ti_sci_free_event_irq(struct ti_sci_inta_irq_domain *inta,
 				  struct ti_sci_inta_vint_desc *vint,
 				  u32 event_index)
@@ -85,14 +127,14 @@ static void ti_sci_free_event_irq(struct ti_sci_inta_irq_domain *inta,
 	inta->sci->ops.rm_irq_ops.free_event_irq(inta->sci,
 						 event->src_id,
 						 event->src_index,
-						 vint->dst_id,
+						 inta->dst_id,
 						 vint->dst_host_irq,
 						 inta->ia_id, vint->vint,
 						 event->global_event,
 						 event_index);
 
 	pr_debug("%s: Event released from src = %d, index = %d, to dst = %d, irq = %d,via ia_id = %d, vint = %d, global event = %d,status_bit = %d\n",
-		 __func__, event->src_id, event->src_index, vint->dst_id,
+		 __func__, event->src_id, event->src_index, inta->dst_id,
 		 vint->dst_host_irq, inta->ia_id, vint->vint,
 		 event->global_event, event_index);
 
@@ -103,6 +145,12 @@ static void ti_sci_free_event_irq(struct ti_sci_inta_irq_domain *inta,
 	ti_sci_release_resource(inta->global_event, event->global_event);
 }
 
+/**
+ * ti_sci_inta_irq_domain_free() - Free an IRQ from the IRQ domain
+ * @domain:	Domain to which the irqs belong
+ * @virq:	base linux virtual IRQ to be freed.
+ * @nr_irqs:	Number of continuous irqs to be freed
+ */
 static void ti_sci_inta_irq_domain_free(struct irq_domain *domain,
 					unsigned int virq, unsigned int nr_irqs)
 {
@@ -127,6 +175,15 @@ static void ti_sci_inta_irq_domain_free(struct irq_domain *domain,
 	kfree(vint_desc);
 }
 
+/**
+ * ti_sci_allocate_event_irq() - Allocate an event to a IA vint.
+ * @inta:	Pointer to Interrupt Aggregator IRQ domain
+ * @vint_desc:	Virtual interrupt descriptor to which the event gets attached.
+ * @src_id:	TISCI device id of the event source
+ * @src_index:	Event index with in the device.
+ *
+ * Return 0 if all went ok else appropriate error value.
+ */
 static int ti_sci_allocate_event_irq(struct ti_sci_inta_irq_domain *inta,
 				     struct ti_sci_inta_vint_desc *vint_desc,
 				     u16 src_id, u16 src_index)
@@ -153,13 +210,13 @@ static int ti_sci_allocate_event_irq(struct ti_sci_inta_irq_domain *inta,
 	event->global_event = ti_sci_get_free_resource(inta->global_event);
 
 	pr_debug("%s: Event requested from src = %d, index = %d, to dst = %d,irq = %d,via ia_id = %d, vint = %d, global event = %d,status_bit = %d\n",
-		 __func__, src_id, src_index, vint_desc->dst_id,
+		 __func__, src_id, src_index, inta->dst_id,
 		 vint_desc->dst_host_irq, inta->ia_id, vint_desc->vint,
 		 event->global_event, free_bit);
 
 	err = inta->sci->ops.rm_irq_ops.set_event_irq(inta->sci,
 						       src_id, src_index,
-						       vint_desc->dst_id,
+						       inta->dst_id,
 						       vint_desc->dst_host_irq,
 						       inta->ia_id,
 						       vint_desc->vint,
@@ -167,7 +224,7 @@ static int ti_sci_allocate_event_irq(struct ti_sci_inta_irq_domain *inta,
 						       free_bit);
 	if (err) {
 		pr_err("%s: Event allocation failed from src = %d, index = %d, to dst = %d,irq = %d,via ia_id = %d, vint = %d,global event = %d, status_bit = %d\n",
-		       __func__, src_id, src_index, vint_desc->dst_id,
+		       __func__, src_id, src_index, inta->dst_id,
 		       vint_desc->dst_host_irq, inta->ia_id, vint_desc->vint,
 		       event->global_event, free_bit);
 		return err;
@@ -176,6 +233,18 @@ static int ti_sci_allocate_event_irq(struct ti_sci_inta_irq_domain *inta,
 	return 0;
 }
 
+/**
+ * alloc_parent_irq() - Allocate parent irq to Interrupt aggregator
+ * @domain:	IRQ domain corresponding to Interrupt Aggregator
+ * @virq:	Linux virtual IRQ number
+ * @src_id:	TISCI device id of the event source
+ * @src_index:	Event index with in the device.
+ * @vint:	Virtual interrupt number within IA
+ * @flags:	Corresponding IRQ flags
+ *
+ * Return pointer to vint descriptor if all went well else corresponding
+ * error pointer.
+ */
 static struct ti_sci_inta_vint_desc *alloc_parent_irq(struct irq_domain *domain,
 						      unsigned int virq,
 						      u32 src_id, u32 src_index,
@@ -243,6 +312,15 @@ err_irqs:
 	return ERR_PTR(err);
 }
 
+/**
+ * ti_sci_inta_irq_domain_alloc() - Allocate Interrupt aggregator IRQs
+ * @domain:	Point to the interrupt aggregator IRQ domain
+ * @virq:	Corresponding Linux virtual IRQ number
+ * @nr_irqs:	Continuous irqs to be allocated
+ * @data:	Pointer to firmware specifier
+ *
+ * Return 0 if all went well else appropriate error value.
+ */
 static int ti_sci_inta_irq_domain_alloc(struct irq_domain *domain,
 					unsigned int virq, unsigned int nr_irqs,
 					void *data)
@@ -353,6 +431,7 @@ static int ti_sci_inta_irq_domain_probe(struct platform_device *pdev)
 		}
 		/* If dst is not specified, assume INTR is the parent */
 		inta->dst_irq = NULL;
+		inta->dst_id = ti_sci_intr_get_dst_id(parent_domain);
 	}
 
 	domain = irq_domain_add_hierarchy(parent_domain, 0, 0, dev_of_node(dev),
@@ -365,6 +444,18 @@ static int ti_sci_inta_irq_domain_probe(struct platform_device *pdev)
 	return 0;
 }
 
+/**
+ * ti_sci_inta_register_event() - Register a event to an interrupt aggregator
+ * @dev:	Device pointer to source generating the event
+ * @src_id:	TISCI device ID of the event source
+ * @src_index:	Event source index within the device.
+ * @virq:	Linux Virtual IRQ number
+ * @flags:	Corresponding IRQ flags
+ *
+ * Creates a new irq and attaches to IA domain if virq is not specified
+ * else attaches the event to vint corresponding to virq.
+ * Return virq if all went well else appropriate error value.
+ */
 int ti_sci_inta_register_event(struct device *dev, u16 src_id, u16 src_index,
 			       unsigned int virq, u32 flags)
 {
@@ -413,6 +504,14 @@ int ti_sci_inta_register_event(struct device *dev, u16 src_id, u16 src_index,
 }
 EXPORT_SYMBOL_GPL(ti_sci_inta_register_event);
 
+/**
+ * get_event_index() - Helper api to get the event index with the vint.
+ * @vint_desc:	Pointer to the IA irq descriptor
+ * @src_id:	TISCI device ID of the event source
+ * @src_index:	Event source index within the device.
+ *
+ * Return the index if all went well else MAX_EVENTS_PER_VINT.
+ */
 static u8 get_event_index(struct ti_sci_inta_vint_desc *vint_desc, u16 src_id,
 			  u16 src_index)
 {
@@ -427,6 +526,14 @@ static u8 get_event_index(struct ti_sci_inta_vint_desc *vint_desc, u16 src_id,
 	return MAX_EVENTS_PER_VINT;
 }
 
+/**
+ * is_event_last() -  Helper api to determine if the event is the last
+ *		      event within the vint
+ * @vint_desc:	Pointer to the IA irq descriptor
+ * @event_index:	Event index within the vint of IA.
+ *
+ * Return true if specified index is the last event else false.
+ */
 static bool is_event_last(struct ti_sci_inta_vint_desc *vint_desc,
 			  u8 event_index)
 {
@@ -446,6 +553,16 @@ static bool is_event_last(struct ti_sci_inta_vint_desc *vint_desc,
 	return status;
 }
 
+/**
+ * ti_sci_inta_unregister_event() - Unregister a event from an IA.
+ * @dev:	Device pointer to source generating the event
+ * @src_id:	TISCI device ID of the event source
+ * @src_index:	Event source index within the device.
+ * @virq:	Linux Virtual IRQ number
+ *
+ * Deletes the event from IA interrupt descriptor. If this event is the
+ * last event then delete the vint as well.
+ */
 void ti_sci_inta_unregister_event(struct device *dev, u16 src_id,
 				  u16 src_index, unsigned int virq)
 {
