1e187ed354ce925ffd42008fa3c7bd799eb01d21
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-08-31 10:14:46 +0300

    dmaengine: ti: k3-udma: Exclude rchan1 as it is reserved for sysfw use
    Based on the latest information rchan1 needs to be added to the list of
    masked resources. rchan1 implies that it's default flow must be locked as
    well.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- drivers/dma/ti/k3-udma.c
+++ drivers/dma/ti/k3-udma.c
@@ -3203,10 +3203,12 @@ static int udma_probe(struct platform_device *pdev)
 	    !ud->rflow_map || !ud->tchans || !ud->rchans || !ud->rflows)
 		return -ENOMEM;
 
-	/* tchan0, rchan0 and rflow0 is dedicated to sysfw */
+	/* tchan0, rchan0,1 and rflow0,1 is dedicated to sysfw */
 	set_bit(0, ud->tchan_map);
 	set_bit(0, ud->rchan_map);
+	set_bit(1, ud->rchan_map);
 	set_bit(0, ud->rflow_map);
+	set_bit(1, ud->rflow_map);
 
 	spin_lock_init(&ud->lock);
 	INIT_WORK(&ud->purge_work, udma_purge_desc_work);
