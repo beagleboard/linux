c9605282efe925ee0f142e1c77db76cfa8e09218
Robert Nelson <robertcnelson@gmail.com>
2018-10-05 16:24:26 -0500

    merge: WireGuard
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ net/wireguard/crypto/zinc/selftest/run.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-2.0 OR MIT */
+/*
+ * Copyright (C) 2015-2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ */
+
+#ifndef _ZINC_SELFTEST_RUN_H
+#define _ZINC_SELFTEST_RUN_H
+
+static inline bool selftest_run(const char *name, bool (*fn)(void),
+				bool *const nobs[], unsigned int nobs_len)
+{
+	unsigned long subset = 0, set = 0;
+	unsigned int i;
+	bool ret = true;
+
+	BUILD_BUG_ON(!__builtin_constant_p(nobs_len) ||
+		     nobs_len >= BITS_PER_LONG);
+
+	if (!IS_ENABLED(CONFIG_ZINC_SELFTEST))
+		return true;
+
+	for (i = 0; i < nobs_len; ++i)
+		set |= ((unsigned long)*nobs[i]) << i;
+
+	do {
+		for (i = 0; i < nobs_len; ++i)
+			*nobs[i] = (subset >> i) & 1;
+		if (!fn()) {
+			pr_err("%s self-test combo 0x%lx: FAIL\n", name,
+			       subset);
+			ret = false;
+		}
+		subset = (subset - set) & set;
+	} while (subset);
+
+	for (i = 0; i < nobs_len; ++i)
+		*nobs[i] = (set >> i) & 1;
+
+	if (!WARN_ON(!ret))
+		pr_info("%s self-tests: pass\n", name);
+
+	return ret;
+}
+
+#endif
