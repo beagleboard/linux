4ee8bc219050c8df5918bfa7b1a8f299fed5376d
Tomi Valkeinen <tomi.valkeinen@ti.com>
2018-10-11 11:17:19 +0300

    drm/omap: accept NULL for dma_addr in omap_gem_pin
    Allow NULL to be passed in 'dma_addr' for omap_gem_pin(), in case the
    caller does not need the dma_addr.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/omap_gem.c
+++ drivers/gpu/drm/omapdrm/omap_gem.c
@@ -826,9 +826,11 @@ int omap_gem_pin(struct drm_gem_object *obj, dma_addr_t *dma_addr)
 
 		omap_obj->dma_addr_cnt++;
 
-		*dma_addr = omap_obj->dma_addr;
+		if (dma_addr)
+			*dma_addr = omap_obj->dma_addr;
 	} else if (omap_gem_is_contiguous(omap_obj)) {
-		*dma_addr = omap_obj->dma_addr;
+		if (dma_addr)
+			*dma_addr = omap_obj->dma_addr;
 	} else {
 		ret = -EINVAL;
 		goto fail;
