3081f5c1a169e938b522511fff91659f7f91525d
Robert Trask <robert.trask@cypress.com>
2018-12-20 11:55:30 -0600

    brcmfmac: Enable, Process, and forward PHY_TEMP event.
    New code enables PHY_TEMP event in firmware, receives it, processes
    into cfg80211 vendor specific event, and forwards to waiting host.
    
    -1- Enable rx PHY_TEMP event from underlying hardware
    -2- Process PHY_TEMP event into vendor specific event
    -3- Forward vendor specific event to host layer
    
    Signed-off-by: Robert Trask <robert.trask@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/vendor.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/vendor.h
@@ -25,6 +25,11 @@ enum brcmf_vndr_cmds {
 	BRCMF_VNDR_CMDS_LAST
 };
 
+enum brcmf_vndr_evts {
+	BRCMF_VNDR_EVTS_PHY_TEMP,
+	BRCMF_VNDR_EVTS_LAST
+};
+
 /**
  * enum brcmf_nlattrs - nl80211 message attributes
  *
@@ -36,10 +41,20 @@ enum brcmf_nlattrs {
 
 	BRCMF_NLATTR_LEN,
 	BRCMF_NLATTR_DATA,
+	BRCMF_NLATTR_VERS,
+	BRCMF_NLATTR_PHY_TEMP,
+	BRCMF_NLATTR_PHY_TEMPDELTA,
 
 	__BRCMF_NLATTR_AFTER_LAST,
 	BRCMF_NLATTR_MAX = __BRCMF_NLATTR_AFTER_LAST - 1
 };
+
+/* structure of event sent up by firmware: is this the right place for it? */
+struct brcmf_phy_temp_evt {
+	__le32 version;
+	__le32 temp;
+	__le32 tempdelta;
+} __packed;
 
 /**
  * struct brcmf_vndr_dcmd_hdr - message header for cfg80211 vendor command dcmd
@@ -60,5 +75,9 @@ struct brcmf_vndr_dcmd_hdr {
 };
 
 extern const struct wiphy_vendor_command brcmf_vendor_cmds[];
+extern const struct nl80211_vendor_cmd_info brcmf_vendor_events[];
+s32 brcmf_wiphy_phy_temp_evt_handler(struct brcmf_if *ifp,
+				     const struct brcmf_event_msg *e,
+				     void *data);
 
 #endif /* _vendor_h_ */
