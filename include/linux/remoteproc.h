8541b4dfdfbac7fbdf6d4190ec9e94328dd96593
Suman Anna <s-anna@ti.com>
2019-02-20 17:52:00 -0600

    remoteproc: Add a rproc_set_firmware() API
    A new API, rproc_set_firmware() is added to allow the remoteproc platform
    drivers and remoteproc client drivers to be able to configure a custom
    firmware name that is different from the default name used during
    remoteproc registration. This function is being introduced to provide
    a kernel-level equivalent of the current sysfs interface to remoteproc
    client drivers, and can only change firmwares when the remoteproc is
    offline. This allows some remoteproc drivers to choose different firmwares
    at runtime based on the functionality the remote processor is providing.
    The TI PRU Ethernet driver will be an example of such usage as it
    requires to use different firmwares for different supported protocols.
    
    Also, update the firmware_store() function used by the sysfs interface
    to reuse this function to avoid code duplication.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -564,6 +564,7 @@ void rproc_free(struct rproc *rproc);
 
 int rproc_boot(struct rproc *rproc);
 void rproc_shutdown(struct rproc *rproc);
+int rproc_set_firmware(struct rproc *rproc, const char *fw_name);
 void rproc_report_crash(struct rproc *rproc, enum rproc_crash_type type);
 int rproc_coredump_add_segment(struct rproc *rproc, dma_addr_t da, size_t size);
 int rproc_get_id(struct rproc *rproc);
