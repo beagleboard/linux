470d11a06360d5d5f1530524d3da1fe13d4f9f1b
Suman Anna <s-anna@ti.com>
2019-02-20 17:52:00 -0600

    remoteproc: Add a rproc_set_firmware() API
    A new API, rproc_set_firmware() is added to allow the remoteproc platform
    drivers and remoteproc client drivers to be able to configure a custom
    firmware name that is different from the default name used during
    remoteproc registration. This function is being introduced to provide
    a kernel-level equivalent of the current sysfs interface to remoteproc
    client drivers, and can only change firmwares when the remoteproc is
    offline. This allows some remoteproc drivers to choose different firmwares
    at runtime based on the functionality the remote processor is providing.
    The TI PRU Ethernet driver will be an example of such usage as it
    requires to use different firmwares for different supported protocols.
    
    Also, update the firmware_store() function used by the sysfs interface
    to reuse this function to avoid code duplication.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/remoteproc/remoteproc_sysfs.c
+++ drivers/remoteproc/remoteproc_sysfs.c
@@ -25,42 +25,13 @@ static ssize_t firmware_store(struct device *dev,
 			      const char *buf, size_t count)
 {
 	struct rproc *rproc = to_rproc(dev);
-	char *p;
-	int err, len = count;
+	int err;
 
 	/* restrict sysfs operations if not allowed by remoteproc drivers */
 	if (rproc->deny_sysfs_ops)
 		return -EPERM;
 
-	err = mutex_lock_interruptible(&rproc->lock);
-	if (err) {
-		dev_err(dev, "can't lock rproc %s: %d\n", rproc->name, err);
-		return -EINVAL;
-	}
-
-	if (rproc->state != RPROC_OFFLINE) {
-		dev_err(dev, "can't change firmware while running\n");
-		err = -EBUSY;
-		goto out;
-	}
-
-	len = strcspn(buf, "\n");
-	if (!len) {
-		dev_err(dev, "can't provide a NULL firmware\n");
-		err = -EINVAL;
-		goto out;
-	}
-
-	p = kstrndup(buf, len, GFP_KERNEL);
-	if (!p) {
-		err = -ENOMEM;
-		goto out;
-	}
-
-	kfree(rproc->firmware);
-	rproc->firmware = p;
-out:
-	mutex_unlock(&rproc->lock);
+	err = rproc_set_firmware(rproc, buf);
 
 	return err ? err : count;
 }
