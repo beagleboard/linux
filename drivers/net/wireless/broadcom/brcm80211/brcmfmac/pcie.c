ab3cf8d56a5b32b1c7d78101cb649d60db1ba388
Wright Feng <wright.feng@cypress.com>
2019-03-07 01:37:27 -0600

    brcmfmac: add sleep in bus suspend and cfg80211 resume functions
    With asynchronous suspend/resume feature, suspend and resume callbacks to
    be executed in parallel with each other. It makes bus changes the state to
    BRCMF_BUS_DOWN before all brcmf_cfg80211_suspend IOVAR executions.
    The same situation also happens in resume procedure and causes PM mode
    keeps in PM_MAX after resume. In order to fix the race condition, We add
    one second sleep in bus suspend and cfg80211 resume function.
    
    Signed-off-by: Wright Feng <wright.feng@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/pcie.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/pcie.c
@@ -38,6 +38,7 @@
 #include "chip.h"
 #include "core.h"
 #include "common.h"
+#include "cfg80211.h"
 
 
 enum brcmf_pcie_state {
@@ -2011,11 +2012,22 @@ static int brcmf_pcie_pm_enter_D3(struct device *dev)
 {
 	struct brcmf_pciedev_info *devinfo;
 	struct brcmf_bus *bus;
+	struct brcmf_cfg80211_info *config;
+	int retry = BRCMF_PM_WAIT_MAXRETRY;
 
 	brcmf_dbg(PCIE, "Enter\n");
 
 	bus = dev_get_drvdata(dev);
 	devinfo = bus->bus_priv.pcie->devinfo;
+	config = bus->drvr->config;
+
+	while (retry &&
+	       config->pm_state == BRCMF_CFG80211_PM_STATE_SUSPENDING) {
+		usleep_range(10000, 20000);
+		retry--;
+	}
+	if (!retry && config->pm_state == BRCMF_CFG80211_PM_STATE_SUSPENDING)
+		brcmf_err(bus, "timed out wait for cfg80211 suspended\n");
 
 	brcmf_bus_change_state(bus, BRCMF_BUS_DOWN);
 
