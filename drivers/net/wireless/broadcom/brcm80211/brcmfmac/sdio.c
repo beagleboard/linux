6b4eb0b23d6672fe95e2449947135d5f3e790a95
Wright Feng <wright.feng@cypress.com>
2019-03-07 21:13:02 -0600

    brcmfmac: fix 43455 CRC error under SDIO 3.0 SDR104 mode
    This patch fixes 43455 CRC error while running throughput test with
    suspend/resume stress test.
    
    The continuous failure messages before system crash:
    brcmfmac: brcmf_sdiod_sglist_rw: CMD53 sg block read failed -84
    brcmfmac: brcmf_sdio_rxglom: glom read of 25600 bytes failed: -5
    brcmfmac: brcmf_sdio_rxfail: abort command, terminate frame
    brcmfmac: brcmf_sdiod_sglist_rw: CMD53 sg block read failed -84
    brcmfmac: brcmf_sdio_rxglom: glom read of 24576 bytes failed: -5
    brcmfmac: brcmf_sdio_rxfail: abort command, terminate frame
    
    Signed-off-by: Wright Feng <wright.feng@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -55,6 +55,10 @@
 #define CY_4373_F2_WATERMARK    0x40
 #define CY_4373_F1_MESBUSYCTRL  (CY_4373_F2_WATERMARK | SBSDIO_MESBUSYCTRL_ENAB)
 #define CY_43012_F2_WATERMARK    0x60
+#define CY_43455_F2_WATERMARK	0x60
+#define CY_43455_MES_WATERMARK	0x50
+#define CY_43455_MESBUSYCTRL	(CY_43455_MES_WATERMARK | \
+				 SBSDIO_MESBUSYCTRL_ENAB)
 #define CY_4339_F2_WATERMARK    48
 #define CY_4339_MES_WATERMARK	80
 #define CY_4339_MESBUSYCTRL	(CY_4339_MES_WATERMARK | SBSDIO_MESBUSYCTRL_ENAB)
@@ -4353,6 +4357,19 @@ static void brcmf_sdio_firmware_callback(struct device *dev, int err,
 			/* Register for ULP events */
 			brcmf_fweh_register(bus_if->drvr, BRCMF_E_ULP,
 					    brcmf_ulp_event_notify);
+			break;
+		case SDIO_DEVICE_ID_BROADCOM_43455:
+			brcmf_dbg(INFO, "set F2 watermark to 0x%x*4 bytes for 43455\n",
+				  CY_43455_F2_WATERMARK);
+			brcmf_sdiod_regwb(sdiodev, SBSDIO_WATERMARK,
+					  CY_43455_F2_WATERMARK, &err);
+			devctl = brcmf_sdiod_regrb(sdiodev, SBSDIO_DEVICE_CTL,
+						   &err);
+			devctl |= SBSDIO_DEVCTL_F2WM_ENAB;
+			brcmf_sdiod_regwb(sdiodev, SBSDIO_DEVICE_CTL, devctl,
+					  &err);
+			brcmf_sdiod_regwb(sdiodev, SBSDIO_FUNC1_MESBUSYCTRL,
+					  CY_43455_MESBUSYCTRL, &err);
 			break;
 		case SDIO_DEVICE_ID_BROADCOM_4339:
 			brcmf_sdiod_regwb(sdiodev,
