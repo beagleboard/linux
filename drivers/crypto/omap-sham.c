b4e22cd8a4ad3489bfbc06fc37d7d997976a8610
Tero Kristo <t-kristo@ti.com>
2019-03-18 10:23:14 +0200

    crypto: omap-sham: remove the sysfs group during driver removal
    The driver removal should also cleanup the created sysfs group. If not,
    the driver fails the subsequent probe as the files exist already.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- drivers/crypto/omap-sham.c
+++ drivers/crypto/omap-sham.c
@@ -2276,6 +2276,8 @@ static int omap_sham_remove(struct platform_device *pdev)
 	if (!dd->polling_mode)
 		dma_release_channel(dd->dma_lch);
 
+	sysfs_remove_group(&dd->dev->kobj, &omap_sham_attr_group);
+
 	return 0;
 }
 
