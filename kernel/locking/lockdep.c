edef2269711ac3726e3fefbc2034f149d8e0670f
Robert Nelson <robertcnelson@gmail.com>
2019-03-25 09:21:21 -0500

    merge: CONFIG_PREEMPT_RT Patch Set
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -3916,6 +3916,7 @@ static void check_flags(unsigned long flags)
 		}
 	}
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 	/*
 	 * We dont accurately track softirq state in e.g.
 	 * hardirq contexts (such as on 4KSTACKS), so only
@@ -3930,6 +3931,7 @@ static void check_flags(unsigned long flags)
 			DEBUG_LOCKS_WARN_ON(!current->softirqs_enabled);
 		}
 	}
+#endif
 
 	if (!debug_locks)
 		print_irqtrace_events(current);
