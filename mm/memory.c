8b6d70a2b71181db0642df7810e1d865850d3a9e
Robert Nelson <robertcnelson@gmail.com>
2019-04-03 12:41:49 -0500

    merge: aufs-mmap
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2122,7 +2122,7 @@ static inline int wp_page_reuse(struct mm_struct *mm,
 		}
 
 		if (!page_mkwrite)
-			file_update_time(vma->vm_file);
+			vma_file_update_time(vma);
 	}
 
 	return VM_FAULT_WRITE;
