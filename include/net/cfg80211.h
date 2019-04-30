48e8e90b5274413ca64c2643842391ca47ab10f7
Chung-Hsien Hsu <stanley.hsu@cypress.com>
2019-04-30 17:52:41 -0500

    nl80211: add authorized flag to CONNECT event
    Add authorized flag to CONNECT event. It is used for 802.1X 4-way
    handshake offload with PMK caching.
    
    Signed-off-by: Chung-Hsien Hsu <stanley.hsu@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -6410,6 +6410,8 @@ struct cfg80211_fils_resp_params {
  *	not known. This value is used only if @status < 0 to indicate that the
  *	failure is due to a timeout and not due to explicit rejection by the AP.
  *	This value is ignored in other cases (@status >= 0).
+ * @authorized: Indicates whether the connection is ready to transport
+ *	data packets.
  */
 struct cfg80211_connect_resp_params {
 	int status;
@@ -6421,6 +6423,7 @@ struct cfg80211_connect_resp_params {
 	size_t resp_ie_len;
 	struct cfg80211_fils_resp_params fils;
 	enum nl80211_timeout_reason timeout_reason;
+	bool authorized;
 };
 
 /**
