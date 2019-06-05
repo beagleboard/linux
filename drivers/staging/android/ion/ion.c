7f733823c35620e085cfe38c0b7407cf644f91be
Andrew F. Davis <afd@ti.com>
2019-06-05 12:01:11 -0400

    staging: android: ion: Allow heap name to be null
    The heap name can be used for debugging but otherwise does not seem
    to be required and no other part of the code will fail if left NULL
    except here. We can make it required and check for it at some point,
    for now lets just prevent this from causing a NULL pointer exception.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -468,7 +468,7 @@ static int ion_query_heaps(struct ion_heap_query *query)
 	max_cnt = query->cnt;
 
 	plist_for_each_entry(heap, &dev->heaps, node) {
-		strncpy(hdata.name, heap->name, MAX_HEAP_NAME);
+		strncpy(hdata.name, heap->name ?: "(null)", MAX_HEAP_NAME);
 		hdata.name[sizeof(hdata.name) - 1] = '\0';
 		hdata.type = heap->type;
 		hdata.heap_id = heap->id;
