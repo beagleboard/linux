8c693cc6aebba59a644df4eed06384d24602348c
Kishon Vijay Abraham I <kishon@ti.com>
2019-06-20 16:18:43 +0530

    misc: pci_endpoint_test: Enable legacy interrupt
    PCI core does not enable legacy interrupt if it finds MSI or
    MSIX interrupt. Explicitly enable legacy interrupt here in order
    to perform legacy interrupt tests.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/misc/pci_endpoint_test.c
+++ drivers/misc/pci_endpoint_test.c
@@ -774,6 +774,7 @@ static int pci_endpoint_test_probe(struct pci_dev *pdev,
 	}
 
 	pci_set_master(pdev);
+	pci_intx(pdev, true);
 
 	if (!pci_endpoint_test_alloc_irq_vectors(test, irq_type))
 		goto err_disable_irq;
