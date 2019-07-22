83a6a61af8ce633a56cb7db25741300cb97ee093
Wolfram Sang <wsa@the-dreams.de>
2019-07-22 09:28:24 +0530

    i2c: core: remove use of in_atomic()
    commit bae1d3a05a8b99bd748168bbf8155a1d047c562e upstream
    
    Commit cea443a81c9c ("i2c: Support i2c_transfer in atomic contexts")
    added in_atomic() to the I2C core. However, the use of in_atomic()
    outside of core kernel code is discouraged and was already[1] when this
    code was added in early 2008. The above commit was a preparation for
    commit b7a3670131c7 ("i2c-pxa: Add polling transfer"). Its commit
    message says explicitly it was added "for cases where I2C transactions
    have to occur at times interrup[t]s are disabled". So, the intention was
    'disabled interrupts'. This matches the use cases for atomic I2C
    transfers I have seen so far: very late communication (mostly to a PMIC)
    to powerdown or reboot the system. For those cases, interrupts are
    disabled then. It doesn't seem that in_atomic() adds value.
    
    After a discussion with Peter Zijlstra[2], we came up with a better set
    of conditionals to match the use case.
    
    The I2C core will soon gain an extra callback into bus drivers
    especially for atomic transfers to make them more generic. The code
    deciding which transfer to use (atomic/non-atomic) should mimic the
    behaviour which locking to use (trylock/lock). This is why we add a
    helper for it.
    
    [1] https://lwn.net/Articles/274695/
    [2] http://patchwork.ozlabs.org/patch/1067437/
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Tested-by: Stefan Lengfeld <contact@stefanchrist.eu>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -1954,7 +1954,7 @@ int i2c_transfer(struct i2c_adapter *adap, struct i2c_msg *msgs, int num)
 		}
 #endif
 
-		if (in_atomic() || irqs_disabled()) {
+		if (i2c_in_atomic_xfer_mode()) {
 			ret = i2c_trylock_bus(adap, I2C_LOCK_SEGMENT);
 			if (!ret)
 				/* I2C activity is ongoing. */
