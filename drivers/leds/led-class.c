c45873ad9ef0bc980e5a49eaeffd5e24c77d3eeb
Jean-Jacques Hiblot <jjhiblot@ti.com>
2019-10-09 10:51:22 +0200

    leds: populate the device's of_node
    If initialization data is available and its fwnode is actually a of_node,
    store this information in the led device's structure. This will allow the
    device to use or provide OF-based API such (devm_xxx).
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -14,6 +14,7 @@
 #include <linux/leds.h>
 #include <linux/list.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/property.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
@@ -276,8 +277,10 @@ int led_classdev_register_ext(struct device *parent,
 		mutex_unlock(&led_cdev->led_access);
 		return PTR_ERR(led_cdev->dev);
 	}
-	if (init_data && init_data->fwnode)
+	if (init_data && init_data->fwnode) {
 		led_cdev->dev->fwnode = init_data->fwnode;
+		led_cdev->dev->of_node = to_of_node(init_data->fwnode);
+	}
 
 	if (ret)
 		dev_warn(parent, "Led %s renamed to %s due to name collision",
