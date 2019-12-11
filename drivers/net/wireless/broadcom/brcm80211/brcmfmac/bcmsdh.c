2f542a10f2834f58b5b54f022214be618f80d3d0
Chung-Hsien Hsu <stanley.hsu@cypress.com>
2019-12-12 00:52:46 +0100

    brcmfmac: set F2 blocksize and watermark for 4359
    commit e0029595fa0f7afe284b51dcf6d43704c2f9f88b upstream.
    
    Set F2 blocksize to 256 bytes and watermark to 0x40 for 4359. Also
    enable and configure F1 MesBusyCtrl. It fixes DMA error while having
    UDP bi-directional traffic.
    
    Signed-off-by: Chung-Hsien Hsu <stanley.hsu@cypress.com>
    [slightly adapted for rebase on mainline linux]
    Signed-off-by: Soeren Moch <smoch@web.de>
    [use switch-case to replace if-else for F2 blksz setting]
    Reviewed-by: Chi-Hsien Lin <chi-hsien.lin@cypress.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/bcmsdh.c
@@ -45,6 +45,7 @@
 #define SDIO_FUNC1_BLOCKSIZE		64
 #define SDIO_FUNC2_BLOCKSIZE		512
 #define SDIO_4373_FUNC2_BLOCKSIZE	256
+#define SDIO_4359_FUNC2_BLOCKSIZE	256
 /* Maximum milliseconds to wait for F2 to come up */
 #define SDIO_WAIT_F2RDY	3000
 
@@ -916,8 +917,16 @@ static int brcmf_sdiod_probe(struct brcmf_sdio_dev *sdiodev)
 		goto out;
 	}
 
-	if (sdiodev->func2->device == SDIO_DEVICE_ID_CYPRESS_4373)
+	switch (sdiodev->func2->device) {
+	case SDIO_DEVICE_ID_CYPRESS_4373:
 		f2_blksz = SDIO_4373_FUNC2_BLOCKSIZE;
+		break;
+	case SDIO_DEVICE_ID_BROADCOM_4359:
+		f2_blksz = SDIO_4359_FUNC2_BLOCKSIZE;
+		break;
+	default:
+		break;
+	}
 
 	ret = sdio_set_block_size(sdiodev->func2, f2_blksz);
 	if (ret) {
