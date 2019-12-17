425b149bfad02b38a5e25c5e7b0ba97754babe96
Robert Nelson <robertcnelson@gmail.com>
2019-12-17 11:38:09 -0600

    enable: Jenkins: http://gfnd.rcn-ee.org:8080
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ jenkins_build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#gcc:
+
+#
+#https://releases.linaro.org/components/toolchain/binaries/7.1-2017.05/arm-linux-gnueabihf/gcc-linaro-7.1.1-2017.05-x86_64_arm-linux-gnueabihf.tar.xz
+#https://releases.linaro.org/components/toolchain/binaries/7.1-2017.08/arm-linux-gnueabihf/gcc-linaro-7.1.1-2017.08-x86_64_arm-linux-gnueabihf.tar.xz
+#https://releases.linaro.org/components/toolchain/binaries/7.2-2017.11/arm-linux-gnueabihf/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf.tar.xz
+#https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
+#https://releases.linaro.org/components/toolchain/binaries/7.4-2019.02/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
+#https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
+#
+
+site="https://snapshots.linaro.org"
+
+gcc_version="7.5"
+gcc_minor=".0"
+release="19.12"
+target="arm-linux-gnueabihf"
+
+version="components/toolchain/binaries/${gcc_version}-20${release}/${target}"
+filename="gcc-linaro-${gcc_version}${gcc_minor}-20${release}-x86_64_${target}.tar.xz"
+directory="gcc-linaro-${gcc_version}${gcc_minor}-20${release}-x86_64_${target}"
+
+datestamp="${gcc_version}-20${release}-${target}"
+
+binary="bin/${target}-"
+#
+
+if [ ! -d ${directory}/ ] ; then
+	rm -rf ./gcc-* || true
+	#wget -c ${site}/${version}/${filename}
+	wget -c http://gfnd.rcn-ee.org/farm/jenkins-dl/${filename}
+	tar xf ${filename}
+fi
+
+export CC=`pwd`/${directory}/bin/arm-linux-gnueabihf-
+
+make ARCH=arm clean
+make ARCH=arm bb.org_defconfig
+
+echo "[make ARCH=arm -j2 CROSS_COMPILE=\"${binary}\" zImage]"
+make ARCH=arm -j2 CROSS_COMPILE="ccache ${CC}" zImage
+if [ ! -f arch/arm/boot/zImage ] ; then
+	echo "failed: [arch/arm/boot/zImage]"
+	exit 1
+fi
+
+echo "[make ARCH=arm -j2 CROSS_COMPILE=\"${binary}\" modules]"
+make ARCH=arm -j2 CROSS_COMPILE="ccache ${CC}" modules
+if [ ! -f drivers/spi/spidev.ko ] ; then
+	echo "failed: [drivers/spi/spidev.ko]"
+	exit 1
+fi
+
+echo "[make ARCH=arm -j2 CROSS_COMPILE=\"${binary}\" dtbs]"
+make ARCH=arm -j2 CROSS_COMPILE="ccache ${CC}" dtbs
+if [ ! -f arch/arm/boot/dts/am335x-boneblack.dtb ] ; then
+	echo "failed: [arch/arm/boot/dts/am335x-boneblack.dtb]"
+	exit 1
+else
+	if [ -f arch/arm/boot/dts/am335x-pocketbeagle.dts ] ; then
+		if [ ! -f arch/arm/boot/dts/am335x-pocketbeagle.dtb ] ; then
+			echo "failed: [arch/arm/boot/dts/am335x-pocketbeagle.dtb]"
+			exit 1
+		fi
+	fi
+fi
+
+make ARCH=arm clean
