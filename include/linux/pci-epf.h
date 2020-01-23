eb80d90c8479cac02ff6cc3a413803c4bf2a9c7f
Kishon Vijay Abraham I <kishon@ti.com>
2020-01-23 15:37:45 +0530

    PCI: dwc: Fix dw_pcie_ep_raise_msix_irq() to get correct MSI-X table address
    commit beb4641a787d ("PCI: dwc: Add MSI-X callbacks handler"),
    in order to raise MSI-X interrupt, obtained MSIX table address from
    Base Address Register (BAR). However BAR only holds PCI address
    programmed by the host whereas the MSI-X table should be in the local
    memory.
    
    Store the MSI-X table address (virtual address) as part of ->set_bar()
    callback and use that to get the message address and message data
    here.
    
    Fixes: beb4641a787d ("PCI: dwc: Add MSI-X callbacks handler")
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- include/linux/pci-epf.h
+++ include/linux/pci-epf.h
@@ -92,10 +92,12 @@ struct pci_epf_driver {
 /**
  * struct pci_epf_bar - represents the BAR of EPF device
  * @phys_addr: physical address that should be mapped to the BAR
+ * @addr: virtual address corresponding to the @phys_addr
  * @size: the size of the address space present in BAR
  */
 struct pci_epf_bar {
 	dma_addr_t	phys_addr;
+	void		*addr;
 	size_t		size;
 	enum pci_barno	barno;
 	int		flags;
@@ -125,6 +127,19 @@ struct pci_epf {
 	struct pci_epc		*epc;
 	struct pci_epf_driver	*driver;
 	struct list_head	list;
+};
+
+/**
+ * struct pci_epf_msix_tbl - represents the MSI-X table entry structure
+ * @msg_addr: Writes to this address will trigger MSI-X interrupt in host
+ * @msg_data: Data that is be written to @msg_addr to trigger MSI-X interrupt
+ * @vector_ctrl: Identifies if the function is prohibited from sending a message
+ * using this MSI-X table entry
+ */
+struct pci_epf_msix_tbl {
+	u64 msg_addr;
+	u32 msg_data;
+	u32 vector_ctrl;
 };
 
 #define to_pci_epf(epf_dev) container_of((epf_dev), struct pci_epf, dev)
