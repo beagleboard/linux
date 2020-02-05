da809517beed3805c15e3c7cf73086b7de953e0b
Suman Anna <s-anna@ti.com>
2020-02-04 20:33:12 -0600

    soc: ti: pruss: Add a platform driver for PRUSS in TI SoCs
    The Programmable Real-Time Unit - Industrial Communication
    Subsystem (PRU-ICSS) is present on various TI SoCs such as
    AM335x or AM437x or the Keystone 66AK2G. Each SoC can have
    one or more PRUSS instances that may or may not be identical.
    For example, AM335x SoCs have a single PRUSS, while AM437x has
    two PRUSS instances PRUSS1 and PRUSS0, with the PRUSS0 being
    a cut-down version of the PRUSS1.
    
    The PRUSS consists of dual 32-bit RISC cores called the
    Programmable Real-Time Units (PRUs), some shared, data and
    instruction memories, some internal peripheral modules, and
    an interrupt controller. The programmable nature of the PRUs
    provide flexibility to implement custom peripheral interfaces,
    fast real-time responses, or specialized data handling.
    
    The PRU-ICSS functionality is achieved through three different
    platform drivers addressing a specific portion of the PRUSS.
    Some sub-modules of the PRU-ICSS IP reuse some of the existing
    drivers (like davinci mdio driver or the generic syscon driver).
    This design provides flexibility in representing the different
    modules of PRUSS accordingly, and at the same time allowing the
    PRUSS driver to add some instance specific configuration within
    an SoC.
    
    The PRUSS platform driver deals with the overall PRUSS and is
    used for managing the subsystem level resources like various
    memories and the CFG module. It is responsible for the creation
    and deletion of the platform devices for the child PRU devices
    and other child devices (like Interrupt Controller, MDIO node
    and some syscon nodes) so that they can be managed by specific
    platform drivers. The PRUSS interrupt controller is managed by
    an irqchip driver, while the individual PRU RISC cores are
    managed by a PRU remoteproc driver.
    
    The driver currently supports the AM335x SoC, and support for
    other TI SoCs will be added in subsequent patches.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

---
+++ drivers/soc/ti/pruss.c
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * PRU-ICSS platform driver for various TI SoCs
+ *
+ * Copyright (C) 2014-2020 Texas Instruments Incorporated - http://www.ti.com/
+ *	Suman Anna <s-anna@ti.com>
+ *	Andrew F. Davis <afd@ti.com>
+ */
+
+#include <linux/dma-mapping.h>
+#include <linux/io.h>
+#include <linux/mfd/syscon.h>
+#include <linux/module.h>
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/pruss_driver.h>
+
+static int pruss_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct device_node *node = dev->of_node;
+	struct device_node *np;
+	struct pruss *pruss;
+	struct resource res;
+	int ret, i, index;
+	const char *mem_names[PRUSS_MEM_MAX] = { "dram0", "dram1", "shrdram2" };
+
+	if (!node) {
+		dev_err(dev, "Non-DT platform device not supported\n");
+		return -ENODEV;
+	}
+
+	ret = dma_set_coherent_mask(dev, DMA_BIT_MASK(32));
+	if (ret) {
+		dev_err(dev, "dma_set_coherent_mask: %d\n", ret);
+		return ret;
+	}
+
+	pruss = devm_kzalloc(dev, sizeof(*pruss), GFP_KERNEL);
+	if (!pruss)
+		return -ENOMEM;
+
+	pruss->dev = dev;
+
+	np = of_get_child_by_name(node, "memories");
+	if (!np) {
+		dev_err(dev, "%pOF is missing memories node\n", np);
+		return -ENODEV;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(mem_names); i++) {
+		index = of_property_match_string(np, "reg-names", mem_names[i]);
+		if (index < 0) {
+			of_node_put(np);
+			return index;
+		}
+
+		if (of_address_to_resource(np, index, &res)) {
+			of_node_put(np);
+			return -EINVAL;
+		}
+
+		pruss->mem_regions[i].va = devm_ioremap(dev, res.start,
+							resource_size(&res));
+		if (!pruss->mem_regions[i].va) {
+			dev_err(dev, "failed to parse and map memory resource %d %s\n",
+				i, mem_names[i]);
+			of_node_put(np);
+			return -ENOMEM;
+		}
+		pruss->mem_regions[i].pa = res.start;
+		pruss->mem_regions[i].size = resource_size(&res);
+
+		dev_dbg(dev, "memory %8s: pa %pa size 0x%zx va %pK\n",
+			mem_names[i], &pruss->mem_regions[i].pa,
+			pruss->mem_regions[i].size, pruss->mem_regions[i].va);
+	}
+	of_node_put(np);
+
+	platform_set_drvdata(pdev, pruss);
+
+	pm_runtime_enable(dev);
+	ret = pm_runtime_get_sync(dev);
+	if (ret < 0) {
+		dev_err(dev, "couldn't enable module\n");
+		pm_runtime_put_noidle(dev);
+		goto rpm_disable;
+	}
+
+	np = of_get_child_by_name(node, "cfg");
+	if (!np) {
+		dev_err(dev, "%pOF is missing cfg node\n", np);
+		ret = -ENODEV;
+		goto rpm_put;
+	}
+
+	pruss->cfg = syscon_node_to_regmap(np);
+	of_node_put(np);
+	if (IS_ERR(pruss->cfg)) {
+		ret = -ENODEV;
+		goto rpm_put;
+	}
+
+	dev_dbg(&pdev->dev, "creating PRU cores and other child platform devices\n");
+	ret = of_platform_populate(node, NULL, NULL, &pdev->dev);
+	if (ret) {
+		dev_err(dev, "of_platform_populate failed\n");
+		goto rpm_put;
+	}
+
+	return 0;
+
+rpm_put:
+	pm_runtime_put_sync(dev);
+rpm_disable:
+	pm_runtime_disable(dev);
+	return ret;
+}
+
+static int pruss_remove(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+
+	dev_dbg(dev, "remove PRU cores and other child platform devices\n");
+	of_platform_depopulate(dev);
+
+	pm_runtime_put_sync(dev);
+	pm_runtime_disable(dev);
+
+	return 0;
+}
+
+static const struct of_device_id pruss_of_match[] = {
+	{ .compatible = "ti,am3356-pruss", },
+	{ /* sentinel */ },
+};
+MODULE_DEVICE_TABLE(of, pruss_of_match);
+
+static struct platform_driver pruss_driver = {
+	.driver = {
+		.name = "pruss",
+		.of_match_table = pruss_of_match,
+	},
+	.probe  = pruss_probe,
+	.remove = pruss_remove,
+};
+module_platform_driver(pruss_driver);
+
+MODULE_AUTHOR("Suman Anna <s-anna@ti.com>");
+MODULE_DESCRIPTION("PRU-ICSS Subsystem Driver");
+MODULE_LICENSE("GPL v2");
