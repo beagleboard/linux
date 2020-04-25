a10cd3dfbc0a40601cc41fa0688edf108cca1be6
Suman Anna <s-anna@ti.com>
2020-04-25 04:10:26 +0000

    remoteproc/pru: Add support for Tx PRU cores on K3 AM65x SR2.0 SoCs
    The AM65x SR2.0 SoCs have a revised ICSSG IP that is based off the
    subsequent IP revision used on J721E SoCs. This IP instance has two
    new custom auxiliary PRU cores called Transmit PRUs (Tx_PRUs) in
    addition to the existing PRUs and RTUs.
    
    The Tx_PRU cores have their own dedicated IRAM (smaller than a PRU
    or RTY), Control and debug feature sets. The RTU and Tx_PRU cores
    though share the same Data RAMs as the PRU cores, so the memories
    have to be partitioned carefully between different applications.
    
    Enhance the existing PRU remoteproc driver to support these new Tx
    PRU cores by using specific compatibles.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/remoteproc/pru_rproc.c
+++ drivers/remoteproc/pru_rproc.c
@@ -1349,6 +1349,7 @@ static const struct of_device_id pru_rproc_match[] = {
 	{ .compatible = "ti,k2g-pru",      .data = &pru_data, },
 	{ .compatible = "ti,am654-pru",    .data = &k3_pru_data, },
 	{ .compatible = "ti,am654-rtu",    .data = &k3_rtu_data, },
+	{ .compatible = "ti,am654-tx-pru", .data = &k3_tx_pru_data, },
 	{ .compatible = "ti,j721e-pru",    .data = &k3_pru_data, },
 	{ .compatible = "ti,j721e-rtu",    .data = &k3_rtu_data, },
 	{ .compatible = "ti,j721e-tx-pru", .data = &k3_tx_pru_data, },
