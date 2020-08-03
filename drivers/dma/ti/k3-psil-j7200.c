000d2a4e069ddb57da5a7aa515826a67f6fa5a1b
Peter Ujfalusi <peter.ujfalusi@ti.com>
2020-08-03 12:49:14 +0000

    dmaengine: ti: k3-psil-j7200: Add entries for 2nd port of MCU SA2UL
    The security accelerator within MCU domain supports two ports.
    
    Add endpoint configuration for the two ingress and one egress threads of
    the second port.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- drivers/dma/ti/k3-psil-j7200.c
+++ drivers/dma/ti/k3-psil-j7200.c
@@ -137,6 +137,8 @@ static struct psil_ep j7200_src_ep_map[] = {
 	/* SA2UL */
 	PSIL_SA2UL(0x7500, 0),
 	PSIL_SA2UL(0x7501, 0),
+	PSIL_SA2UL(0x7502, 0),
+	PSIL_SA2UL(0x7503, 0),
 };
 
 /* PSI-L destination thread IDs, used for TX (DMA_MEM_TO_DEV) */
@@ -161,6 +163,7 @@ static struct psil_ep j7200_dst_ep_map[] = {
 	PSIL_ETHERNET(0xf007),
 	/* SA2UL */
 	PSIL_SA2UL(0xf500, 1),
+	PSIL_SA2UL(0xf501, 1),
 };
 
 struct psil_ep_map j7200_ep_map = {
