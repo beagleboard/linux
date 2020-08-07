beagleboard/linux

commit 8c2782d9cf8577929a707c0a68e3313bc71f5c23

net: ethernet: ti: icss-iep: Make icss-iep a separate driver
Similar to ICSS_INTC, ICSS IEP is a separate module inside ICSS. So make
it a separate driver. Plat data is introduced to differentiate the below
features among multiple platforms:
- 64 Bit counter
- Slow compensation support
- Shadow mode
- Regmap for register offsets differences.

This driver abstracts all features of IEP and other drivers cannot
access IEP registers directly.

Acked-by: Roger Quadros <rogerq@ti.com>
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

drivers/net/ethernet/ti/icss_iep.c
@@ -6,10 +6,12 @@
  */
 
 #include <linux/bitops.h>
+#include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/timekeeping.h>
 
@@ -18,9 +20,6 @@
 #define IEP_MAX_DEF_INC		0xf
 #define IEP_MAX_COMPEN_INC		0xfff
 #define IEP_MAX_COMPEN_COUNT	0xffffff
-
-#define IEP_CMP_REG0(n)		(0x78 + (n) * 8)
-#define IEP_CMP_REG1(n)		(0x7c + (n) * 8)
 
 #define IEP_GLOBAL_CFG_CNT_ENABLE	BIT(0)
 #define IEP_GLOBAL_CFG_DEFAULT_INC_MASK		GENMASK(7, 4)
@@ -39,19 +38,138 @@
 #define IEP_MIN_CMP	0
 #define IEP_MAX_CMP	15
 
-static void icss_iep_settime(struct icss_iep *iep, u64 ns)
+#define ICSS_IEP_64BIT_COUNTER_SUPPORT		BIT(0)
+#define ICSS_IEP_SLOW_COMPEN_REG_SUPPORT	BIT(1)
+#define ICSS_IEP_SHADOW_MODE_SUPPORT		BIT(2)
+
+enum {
+	ICSS_IEP_GLOBAL_CFG_REG,
+	ICSS_IEP_GLOBAL_STATUS_REG,
+	ICSS_IEP_COMPEN_REG,
+	ICSS_IEP_SLOW_COMPEN_REG,
+	ICSS_IEP_COUNT_REG0,
+	ICSS_IEP_COUNT_REG1,
+	ICSS_IEP_CAPTURE_CFG_REG,
+	ICSS_IEP_CAPTURE_STAT_REG,
+
+	ICSS_IEP_CAP6_RISE_REG0,
+	ICSS_IEP_CAP6_RISE_REG1,
+	ICSS_IEP_CAP6_FALL_REG0,
+	ICSS_IEP_CAP6_FALL_REG1,
+
+	ICSS_IEP_CAP7_RISE_REG0,
+	ICSS_IEP_CAP7_RISE_REG1,
+	ICSS_IEP_CAP7_FALL_REG0,
+	ICSS_IEP_CAP7_FALL_REG1,
+
+	ICSS_IEP_CMP_CFG_REG,
+	ICSS_IEP_CMP_STAT_REG,
+	ICSS_IEP_CMP0_REG0,
+	ICSS_IEP_CMP0_REG1,
+
+	ICSS_IEP_CMP8_REG0,
+	ICSS_IEP_CMP8_REG1,
+	ICSS_IEP_SYNC_CTRL_REG,
+	ICSS_IEP_SYNC0_STAT_REG,
+	ICSS_IEP_SYNC1_STAT_REG,
+	ICSS_IEP_SYNC_PWIDTH_REG,
+	ICSS_IEP_SYNC0_PERIOD_REG,
+	ICSS_IEP_SYNC1_DELAY_REG,
+	ICSS_IEP_SYNC_START_REG,
+	ICSS_IEP_MAX_REGS,
+};
+
+/**
+ * struct icss_iep_plat_data - Plat data to handle SoC variants
+ * @config: Regmap configuration data
+ * @reg_offs: register offsets to capture offset differences across SoCs
+ * @flags: Flags to represent IEP properties
+ */
+struct icss_iep_plat_data {
+	struct regmap_config *config;
+	u32 reg_offs[ICSS_IEP_MAX_REGS];
+	u32 flags;
+};
+
+struct icss_iep {
+	struct device *dev;
+	void __iomem *base;
+	const struct icss_iep_plat_data *plat_data;
+	struct regmap *map;
+	struct device_node *client_np;
+	unsigned long refclk_freq;
+	int clk_tick_time;	/* one refclk tick time in ns */
+	struct ptp_clock_info ptp_info;
+	struct ptp_clock *ptp_clock;
+	struct mutex ptp_clk_mutex;	/* PHC access serializer */
+	u32 def_inc;
+	s16 slow_cmp_inc;
+	u32 slow_cmp_count;
+	const struct icss_iep_clockops *ops;
+	void *clockops_data;
+	u32 cycle_time_ns;
+};
+
+/**
+ * icss_iep_get_count_hi() - Get the upper 32 bit IEP counter
+ * @iep: Pointer to structure representing IEP.
+ *
+ * Return: upper 32 bit IEP counter
+ */
+int icss_iep_get_count_hi(struct icss_iep *iep)
+{
+	u32 val = 0;
+
+	if (iep->plat_data->flags & ICSS_IEP_64BIT_COUNTER_SUPPORT)
+		regmap_read(iep->map, ICSS_IEP_COUNT_REG1, &val);
+
+	return val;
+}
+EXPORT_SYMBOL_GPL(icss_iep_get_count_hi);
+
+/**
+ * icss_iep_get_count_low() - Get the lower 32 bit IEP counter
+ * @iep: Pointer to structure representing IEP.
+ *
+ * Return: lower 32 bit IEP counter
+ */
+int icss_iep_get_count_low(struct icss_iep *iep)
 {
 	u32 val;
 
+	regmap_read(iep->map, ICSS_IEP_COUNT_REG0, &val);
+
+	return val;
+}
+EXPORT_SYMBOL_GPL(icss_iep_get_count_low);
+
+/**
+ * icss_iep_get_ptp_clock() - Get PTP clock registered using IEP driver
+ * @iep: Pointer to structure representing IEP.
+ *
+ * Return: pointer to ptp_clock
+ */
+struct ptp_clock *icss_iep_get_ptp_clock(struct icss_iep *iep)
+{
+	return iep->ptp_clock;
+}
+EXPORT_SYMBOL_GPL(icss_iep_get_ptp_clock);
+
+static void icss_iep_set_counter(struct icss_iep *iep, u64 ns)
+{
+	if (iep->plat_data->flags & ICSS_IEP_64BIT_COUNTER_SUPPORT)
+		regmap_write(iep->map, ICSS_IEP_COUNT_REG1, upper_32_bits(ns));
+	regmap_write(iep->map, ICSS_IEP_COUNT_REG0, lower_32_bits(ns));
+}
+
+static void icss_iep_settime(struct icss_iep *iep, u64 ns)
+{
 	if (iep->ops && iep->ops->settime) {
-		iep->ops->settime(iep, ns);
+		iep->ops->settime(iep->clockops_data, ns);
 		return;
 	}
 
-	val = upper_32_bits(ns);
-	regmap_write(iep->map, IEP_COUNT_HIGH_REG, val);
-	val = lower_32_bits(ns);
-	regmap_write(iep->map, IEP_COUNT_LOW_REG, val);
+	icss_iep_set_counter(iep, ns);
 }
 
 static u64 icss_iep_gettime(struct icss_iep *iep)
@@ -60,26 +178,28 @@ static u64 icss_iep_gettime(struct icss_iep *iep)
 	u32 tmp;
 
 	if (iep->ops && iep->ops->gettime)
-		return iep->ops->gettime(iep);
+		return iep->ops->gettime(iep->clockops_data);
 
-	regmap_read(iep->map, IEP_COUNT_LOW_REG, &tmp);
+	regmap_read(iep->map, ICSS_IEP_COUNT_REG0, &tmp);
 	val = tmp;
-	regmap_read(iep->map, IEP_COUNT_HIGH_REG, &tmp);
-	val |= (u64)tmp << 32;
+	if (iep->plat_data->flags & ICSS_IEP_64BIT_COUNTER_SUPPORT) {
+		regmap_read(iep->map, ICSS_IEP_COUNT_REG1, &tmp);
+		val |= (u64)tmp << 32;
+	}
 
 	return val;
 }
 
 static void icss_iep_enable(struct icss_iep *iep)
 {
-	regmap_update_bits(iep->map, IEP_GLOBAL_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_GLOBAL_CFG_REG,
 			   IEP_GLOBAL_CFG_CNT_ENABLE,
 			   IEP_GLOBAL_CFG_CNT_ENABLE);
 }
 
 static void icss_iep_disable(struct icss_iep *iep)
 {
-	regmap_update_bits(iep->map, IEP_GLOBAL_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_GLOBAL_CFG_REG,
 			   IEP_GLOBAL_CFG_CNT_ENABLE,
 			   0);
 }
@@ -95,47 +215,46 @@ static void icss_iep_enable_shadow_mode(struct icss_iep *iep, u32 cycle_time_ns)
 	icss_iep_disable(iep);
 
 	/* disable shadow mode */
-	regmap_update_bits(iep->map, IEP_CMP_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_CMP_CFG_REG,
 			   IEP_CMP_CFG_SHADOW_EN, 0);
 
 	/* enable shadow mode */
-	regmap_update_bits(iep->map, IEP_CMP_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_CMP_CFG_REG,
 			   IEP_CMP_CFG_SHADOW_EN, IEP_CMP_CFG_SHADOW_EN);
 
 	/* clear counters */
-	regmap_write(iep->map, IEP_COUNT_HIGH_REG, 0);
-	regmap_write(iep->map, IEP_COUNT_LOW_REG, 0);
+	icss_iep_set_counter(iep, 0);
 
 	/* clear overflow status */
-	regmap_update_bits(iep->map, IEP_GLOBAL_STATUS_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_GLOBAL_STATUS_REG,
 			   IEP_GLOBAL_STATUS_CNT_OVF,
 			   IEP_GLOBAL_STATUS_CNT_OVF);
 
 	/* clear compare status */
 	for (cmp = IEP_MIN_CMP; cmp < IEP_MAX_CMP; cmp++) {
-		regmap_update_bits(iep->map, IEP_CMP_STATUS_REG,
+		regmap_update_bits(iep->map, ICSS_IEP_CMP_STAT_REG,
 				   IEP_CMP_STATUS(cmp), IEP_CMP_STATUS(cmp));
 	}
 
 	/* enable reset counter on CMP0 event */
-	regmap_update_bits(iep->map, IEP_CMP_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_CMP_CFG_REG,
 			   IEP_CMP_CFG_CMP0_RST_CNT_EN,
 			   IEP_CMP_CFG_CMP0_RST_CNT_EN);
 	/* enable compare */
-	regmap_update_bits(iep->map, IEP_CMP_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_CMP_CFG_REG,
 			   IEP_CMP_CFG_CMP_EN(0),
 			   IEP_CMP_CFG_CMP_EN(0));
 
 	/* set CMP0 value to cycle time */
-	regmap_write(iep->map, IEP_CMP_REG0(0), cycle_time);
-	regmap_write(iep->map, IEP_CMP_REG1(0), cycle_time);
+	regmap_write(iep->map, ICSS_IEP_CMP0_REG0, cycle_time);
+	regmap_write(iep->map, ICSS_IEP_CMP0_REG1, cycle_time);
 
 	icss_iep_enable(iep);
 }
 
 static void icss_iep_set_default_inc(struct icss_iep *iep, u8 def_inc)
 {
-	regmap_update_bits(iep->map, IEP_GLOBAL_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_GLOBAL_CFG_REG,
 			   IEP_GLOBAL_CFG_DEFAULT_INC_MASK,
 			   def_inc << IEP_GLOBAL_CFG_DEFAULT_INC_SHIFT);
 }
@@ -150,7 +269,7 @@ static void icss_iep_set_compensation_inc(struct icss_iep *iep, u16 compen_inc)
 		compen_inc = IEP_MAX_COMPEN_INC;
 	}
 
-	regmap_update_bits(iep->map, IEP_GLOBAL_CFG_REG,
+	regmap_update_bits(iep->map, ICSS_IEP_GLOBAL_CFG_REG,
 			   IEP_GLOBAL_CFG_COMPEN_INC_MASK,
 			   compen_inc << IEP_GLOBAL_CFG_COMPEN_INC_SHIFT);
 }
@@ -166,13 +285,13 @@ static void icss_iep_set_compensation_count(struct icss_iep *iep,
 		compen_count = IEP_MAX_COMPEN_COUNT;
 	}
 
-	regmap_write(iep->map, IEP_COMPEN_REG, compen_count);
+	regmap_write(iep->map, ICSS_IEP_COMPEN_REG, compen_count);
 }
 
 static void icss_iep_set_slow_compensation_count(struct icss_iep *iep,
 						 u32 compen_count)
 {
-	regmap_write(iep->map, IEP_SLOW_COMPEN_REG, compen_count);
+	regmap_write(iep->map, ICSS_IEP_SLOW_COMPEN_REG, compen_count);
 }
 
 /* PTP PHC operations */
@@ -222,7 +341,6 @@ static int icss_iep_ptp_adjfreq(struct ptp_clock_info *ptp, s32 ppb)
 	cmp_inc = iep->clk_tick_time + iep->slow_cmp_inc;
 	icss_iep_set_compensation_inc(iep, cmp_inc);
 	icss_iep_set_slow_compensation_count(iep, iep->slow_cmp_count);
-	iep->slow_cmp_active = 1;
 
 	mutex_unlock(&iep->ptp_clk_mutex);
 
@@ -236,7 +354,7 @@ static int icss_iep_ptp_adjtime(struct ptp_clock_info *ptp, s64 delta)
 
 	mutex_lock(&iep->ptp_clk_mutex);
 	if (iep->ops && iep->ops->adjtime) {
-		iep->ops->adjtime(iep, delta);
+		iep->ops->adjtime(iep->clockops_data, delta);
 	} else {
 		ns = icss_iep_gettime(iep);
 		ns += delta;
@@ -292,16 +410,57 @@ static struct ptp_clock_info icss_iep_ptp_info = {
 	.enable		= icss_iep_ptp_enable,
 };
 
-int icss_iep_init(struct icss_iep *iep, struct device *parent_dev,
-		  struct regmap *iep_map, u32 refclk_freq,
-		  u32 cycle_time_ns)
+struct icss_iep *icss_iep_get(struct device_node *np)
 {
-	int ret;
-	u32 def_inc;
+	struct platform_device *pdev;
+	struct device_node *iep_np;
+	struct icss_iep *iep;
 
-	iep->map = iep_map;
-	iep->refclk_freq = refclk_freq;
-	mutex_init(&iep->ptp_clk_mutex);
+	iep_np = of_parse_phandle(np, "iep", 0);
+	if (!iep_np || !of_device_is_available(iep_np))
+		return ERR_PTR(-ENODEV);
+
+	pdev = of_find_device_by_node(iep_np);
+	of_node_put(iep_np);
+
+	if (!pdev)
+		/* probably IEP not yet probed */
+		return ERR_PTR(-EPROBE_DEFER);
+
+	iep = platform_get_drvdata(pdev);
+	if (!iep)
+		return ERR_PTR(-EPROBE_DEFER);
+
+	device_lock(iep->dev);
+	if (iep->client_np) {
+		device_unlock(iep->dev);
+		dev_err(iep->dev, "IEP is already acquired by %s",
+			iep->client_np->name);
+		return ERR_PTR(-EBUSY);
+	}
+	iep->client_np = np;
+	device_unlock(iep->dev);
+	get_device(iep->dev);
+
+	return iep;
+}
+EXPORT_SYMBOL_GPL(icss_iep_get);
+
+void icss_iep_put(struct icss_iep *iep)
+{
+	device_lock(iep->dev);
+	iep->client_np = NULL;
+	device_unlock(iep->dev);
+	put_device(iep->dev);
+}
+EXPORT_SYMBOL_GPL(icss_iep_put);
+
+int icss_iep_init(struct icss_iep *iep, const struct icss_iep_clockops *clkops,
+		  void *clockops_data, u32 cycle_time_ns)
+{
+	struct device *dev = iep->dev;
+	u32 def_inc;
+	int ret;
 
 	def_inc = NSEC_PER_SEC / iep->refclk_freq;	/* ns per clock tick */
 	if (def_inc > IEP_MAX_DEF_INC)
@@ -309,30 +468,31 @@ int icss_iep_init(struct icss_iep *iep, struct device *parent_dev,
 		return -EINVAL;
 
 	iep->def_inc = def_inc;
+	iep->ops = clkops;
+	iep->clockops_data = clockops_data;
 	icss_iep_set_default_inc(iep, def_inc);
 	icss_iep_set_compensation_inc(iep, def_inc);
 	icss_iep_set_compensation_count(iep, 0);
-	icss_iep_set_slow_compensation_count(iep, 0);
+	if (iep->plat_data->flags & ICSS_IEP_SLOW_COMPEN_REG_SUPPORT)
+		icss_iep_set_slow_compensation_count(iep, 0);
 	if (cycle_time_ns)
 		icss_iep_enable_shadow_mode(iep, cycle_time_ns);
 	else
 		icss_iep_enable(iep);
 
 	iep->cycle_time_ns = cycle_time_ns;
-	regmap_write(iep->map, IEP_COUNT_HIGH_REG, 0);
-	regmap_write(iep->map, IEP_COUNT_LOW_REG, 0);
+	icss_iep_set_counter(iep, 0);
 
 	iep->clk_tick_time = def_inc;
 	iep->ptp_info = icss_iep_ptp_info;
 
-	iep->ptp_clock = ptp_clock_register(&iep->ptp_info, parent_dev);
+	iep->ptp_clock = ptp_clock_register(&iep->ptp_info, dev);
 	if (IS_ERR(iep->ptp_clock)) {
 		ret = PTR_ERR(iep->ptp_clock);
 		iep->ptp_clock = NULL;
-		dev_err(parent_dev, "Failed to register ptp clk %d\n", ret);
+		dev_err(dev, "Failed to register ptp clk %d\n", ret);
 		goto err_disable;
 	}
-	iep->ptp_index = ptp_clock_index(iep->ptp_clock);
 
 	return 0;
 
@@ -341,6 +501,7 @@ err_disable:
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(icss_iep_init);
 
 int icss_iep_exit(struct icss_iep *iep)
 {
@@ -350,3 +511,151 @@ int icss_iep_exit(struct icss_iep *iep)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(icss_iep_exit);
+
+static const struct of_device_id icss_iep_of_match[];
+
+int icss_iep_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct icss_iep *iep;
+	struct resource *res;
+	struct clk *iep_clk;
+
+	iep = devm_kzalloc(dev, sizeof(*iep), GFP_KERNEL);
+	if (!iep)
+		return -ENOMEM;
+
+	iep->dev = dev;
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	iep->base = devm_ioremap_resource(dev, res);
+	if (IS_ERR(iep->base))
+		return -ENODEV;
+
+	iep_clk = devm_clk_get(dev, NULL);
+	if (IS_ERR(iep_clk))
+		return PTR_ERR(iep_clk);
+
+	iep->refclk_freq = clk_get_rate(iep_clk);
+
+	iep->plat_data = of_device_get_match_data(dev);
+	if (!iep->plat_data)
+		return -EINVAL;
+
+	iep->map = devm_regmap_init(dev, NULL, iep, iep->plat_data->config);
+	if (IS_ERR(iep->map)) {
+		dev_err(dev, "Failed to create regmap for IEP %ld\n",
+			PTR_ERR(iep->map));
+		return PTR_ERR(iep->map);
+	}
+
+	mutex_init(&iep->ptp_clk_mutex);
+	dev_set_drvdata(dev, iep);
+	icss_iep_disable(iep);
+
+	return 0;
+}
+
+static bool am654_icss_iep_valid_reg(struct device *dev, unsigned int reg)
+{
+	switch (reg) {
+	case ICSS_IEP_GLOBAL_CFG_REG ... ICSS_IEP_SYNC_START_REG:
+		return true;
+	default:
+		return false;
+	}
+
+	return false;
+}
+
+static int icss_iep_regmap_write(void *context, unsigned int reg,
+				 unsigned int val)
+{
+	struct icss_iep *iep = context;
+
+	writel(val, iep->base + iep->plat_data->reg_offs[reg]);
+
+	return 0;
+}
+
+static int icss_iep_regmap_read(void *context, unsigned int reg,
+				unsigned int *val)
+{
+	struct icss_iep *iep = context;
+
+	*val = readl(iep->base + iep->plat_data->reg_offs[reg]);
+
+	return 0;
+}
+
+static struct regmap_config am654_icss_iep_regmap_config = {
+	.name = "icss iep",
+	.reg_stride = 1,
+	.reg_write = icss_iep_regmap_write,
+	.reg_read = icss_iep_regmap_read,
+	.writeable_reg = am654_icss_iep_valid_reg,
+	.readable_reg = am654_icss_iep_valid_reg,
+};
+
+static const struct icss_iep_plat_data am654_icss_iep_plat_data = {
+	.flags = ICSS_IEP_64BIT_COUNTER_SUPPORT |
+		 ICSS_IEP_SLOW_COMPEN_REG_SUPPORT |
+		 ICSS_IEP_SHADOW_MODE_SUPPORT,
+	.reg_offs = {
+		[ICSS_IEP_GLOBAL_CFG_REG] = 0x00,
+		[ICSS_IEP_COMPEN_REG] = 0x08,
+		[ICSS_IEP_SLOW_COMPEN_REG] = 0x0C,
+		[ICSS_IEP_COUNT_REG0] = 0x10,
+		[ICSS_IEP_COUNT_REG1] = 0x14,
+		[ICSS_IEP_CAPTURE_CFG_REG] = 0x18,
+		[ICSS_IEP_CAPTURE_STAT_REG] = 0x1c,
+
+		[ICSS_IEP_CAP6_RISE_REG0] = 0x50,
+		[ICSS_IEP_CAP6_RISE_REG1] = 0x54,
+		[ICSS_IEP_CAP6_FALL_REG0] = 0x58,
+		[ICSS_IEP_CAP6_FALL_REG1] = 0x5c,
+
+		[ICSS_IEP_CAP7_RISE_REG0] = 0x60,
+		[ICSS_IEP_CAP7_RISE_REG1] = 0x64,
+		[ICSS_IEP_CAP7_FALL_REG0] = 0x68,
+		[ICSS_IEP_CAP7_FALL_REG1] = 0x6c,
+
+		[ICSS_IEP_CMP_CFG_REG] = 0x70,
+		[ICSS_IEP_CMP_STAT_REG] = 0x74,
+		[ICSS_IEP_CMP0_REG0] = 0x78,
+		[ICSS_IEP_CMP0_REG1] = 0x7c,
+
+		[ICSS_IEP_CMP8_REG0] = 0xc0,
+		[ICSS_IEP_CMP8_REG1] = 0xc4,
+		[ICSS_IEP_SYNC_CTRL_REG] = 0x180,
+		[ICSS_IEP_SYNC0_STAT_REG] = 0x188,
+		[ICSS_IEP_SYNC1_STAT_REG] = 0x18c,
+		[ICSS_IEP_SYNC_PWIDTH_REG] = 0x190,
+		[ICSS_IEP_SYNC0_PERIOD_REG] = 0x194,
+		[ICSS_IEP_SYNC1_DELAY_REG] = 0x198,
+		[ICSS_IEP_SYNC_START_REG] = 0x19c,
+	},
+	.config = &am654_icss_iep_regmap_config,
+};
+
+static const struct of_device_id icss_iep_of_match[] = {
+	{
+		.compatible = "ti,am654-icss-iep",
+		.data = &am654_icss_iep_plat_data,
+	},
+	{},
+};
+MODULE_DEVICE_TABLE(of, icss_iep_of_match);
+
+static struct platform_driver icss_iep_driver = {
+	.driver = {
+		.name = "icss-iep",
+		.of_match_table = of_match_ptr(icss_iep_of_match),
+	},
+	.probe = icss_iep_probe,
+};
+module_platform_driver(icss_iep_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("TI ICSS IEP driver");
+MODULE_AUTHOR("Roger Quadros <rogerq@ti.com>");

drivers/net/ethernet/ti/Kconfig
@@ -162,10 +162,19 @@ config TI_PRUETH
 	---help---
 	  Support single or dual EMAC over PRUSS.
 
+config TI_ICSS_IEP
+	tristate "TI PRU ICSS IEP driver"
+	depends on TI_PRUSS
+	default TI_PRUSS
+	help
+	  This enables support for the PRU-ICSS Industrial Ethernet Peripheral
+	  within a PRU-ICSS subsystem present on various TI SoCs.
+
 config TI_ICSSG_PRUETH
 	tristate "TI Gigabit PRU Ethernet driver"
 	select TI_DAVINCI_MDIO
 	select NET_PTP_CLASSIFY
+	select TI_ICSS_IEP
 	imply PTP_1588_CLOCK
 	depends on PRU_REMOTEPROC
 	depends on ARCH_K3 && OF && TI_K3_UDMA_GLUE_LAYER

drivers/net/ethernet/ti/Makefile
@@ -30,4 +30,5 @@ obj-$(CONFIG_TI_RDEV_ETH_SWITCH_VIRT_EMAC) += j721e-cpsw-virt-mac.o
 obj-$(CONFIG_TI_PRUETH) += prueth.o
 prueth-y := prueth_core.o prueth_qos.o prueth_switch.o
 obj-$(CONFIG_TI_ICSSG_PRUETH) += icssg-prueth.o
-icssg-prueth-y := icssg_prueth.o icssg_classifier.o icssg_ethtool.o icss_iep.o icssg_queues.o icssg_config.o
+icssg-prueth-y := icssg_prueth.o icssg_classifier.o icssg_ethtool.o icssg_queues.o icssg_config.o
+obj-$(CONFIG_TI_ICSS_IEP) += icss_iep.o

drivers/net/ethernet/ti/icss_iep.h
@@ -12,45 +12,22 @@
 #include <linux/ptp_clock_kernel.h>
 #include <linux/regmap.h>
 
-#define IEP_GLOBAL_CFG_REG	0x0
-#define IEP_GLOBAL_STATUS_REG	0x4
-#define IEP_COMPEN_REG		0x8
-#define IEP_SLOW_COMPEN_REG	0xc
-#define IEP_COUNT_LOW_REG	0x10
-#define IEP_COUNT_HIGH_REG	0x14
-#define IEP_CAP_CFG_REG		0x18
-#define IEP_CAP_STATUS_REG	0x1c
-#define IEP_CMP_CFG_REG		0x70
-#define IEP_CMP_STATUS_REG	0x74
-
 struct icss_iep;
 
 /* Firmware specific clock operations */
 struct icss_iep_clockops {
-	void (*settime)(struct icss_iep *iep, u64 ns);
-	void (*adjtime)(struct icss_iep *iep, s64 delta);
-	u64 (*gettime)(struct icss_iep *iep);
+	void (*settime)(void *clockops_data, u64 ns);
+	void (*adjtime)(void *clockops_data, s64 delta);
+	u64 (*gettime)(void *clockops_data);
 };
 
-struct icss_iep {
-	struct regmap *map;
-	u32 refclk_freq;
-	int clk_tick_time;	/* one refclk tick time in ns */
-	struct ptp_clock_info ptp_info;
-	struct ptp_clock *ptp_clock;
-	int ptp_index;
-	struct mutex ptp_clk_mutex;	/* PHC access serializer */
-	unsigned int slow_cmp_active;
-	u32 def_inc;
-	s16 slow_cmp_inc;
-	u32 slow_cmp_count;
-	const struct icss_iep_clockops *ops;
-	u32 cycle_time_ns;
-};
-
-int icss_iep_init(struct icss_iep *iep, struct device *parent_dev,
-		  struct regmap *iep_map, u32 refclk_freq,
-		  u32 cycle_time_ns);
+struct icss_iep *icss_iep_get(struct device_node *np);
+void icss_iep_put(struct icss_iep *iep);
+int icss_iep_init(struct icss_iep *iep, const struct icss_iep_clockops *clkops,
+		  void *clockops_data, u32 cycle_time_ns);
 int icss_iep_exit(struct icss_iep *iep);
+int icss_iep_get_count_low(struct icss_iep *iep);
+int icss_iep_get_count_hi(struct icss_iep *iep);
+struct ptp_clock *icss_iep_get_ptp_clock(struct icss_iep *iep);
 
 #endif /* __NET_TI_ICSS_IEP_H */

drivers/net/ethernet/ti/icssg_ethtool.c
@@ -294,7 +294,7 @@ static int emac_get_ts_info(struct net_device *ndev,
 		SOF_TIMESTAMPING_SOFTWARE |
 		SOF_TIMESTAMPING_RAW_HARDWARE;
 
-	info->phc_index = ptp_clock_index(emac->iep.ptp_clock);
+	info->phc_index = ptp_clock_index(icss_iep_get_ptp_clock(emac->iep));
 	info->tx_types = BIT(HWTSTAMP_TX_OFF) | BIT(HWTSTAMP_TX_ON);
 	info->rx_filters = BIT(HWTSTAMP_FILTER_NONE) | BIT(HWTSTAMP_FILTER_ALL);
 

drivers/net/ethernet/ti/icssg_prueth.c
@@ -55,7 +55,6 @@
 				 NETIF_MSG_WOL)
 
 #define prueth_napi_to_emac(napi) container_of(napi, struct prueth_emac, napi)
-#define prueth_iep_to_emac(iepx) container_of(iepx, struct prueth_emac, iep)
 
 /* CTRLMMR_ICSSG_RGMII_CTRL register bits */
 #define ICSSG_CTRL_RGMII_ID_MODE		BIT(24)
@@ -310,7 +309,7 @@ static void emac_rx_timestamp(struct prueth_emac *emac,
 		u32 hi_sw = readl(emac->dram.va +
 				  TIMESYNC_FW_COUNT_HI_SW_OFFSET_OFFSET);
 		ns = icssg_ts_to_ns(hi_sw, psdata[1], psdata[0],
-				    emac->iep.cycle_time_ns);
+				    IEP_DEFAULT_CYCLE_TIME_NS);
 	}
 
 	ssh = skb_hwtstamps(skb);
@@ -619,7 +618,7 @@ static void tx_ts_work(struct prueth_emac *emac)
 
 	hi_sw = readl(emac->dram.va + TIMESYNC_FW_COUNT_HI_SW_OFFSET_OFFSET);
 	ns = icssg_ts_to_ns(hi_sw, tsr.hi_ts, tsr.lo_ts,
-			    emac->iep.cycle_time_ns);
+			    IEP_DEFAULT_CYCLE_TIME_NS);
 
 	if (tsr.cookie != emac->tx_ts_cookie) {
 		netdev_err(emac->ndev, "TX TS cookie mismatch 0x%x:0x%x\n",
@@ -1623,9 +1622,9 @@ static void emac_ndo_set_rx_mode(struct net_device *ndev)
 	}
 }
 
-static u64 prueth_iep_gettime(struct icss_iep *iep)
+static u64 prueth_iep_gettime(void *clockops_data)
 {
-	struct prueth_emac *emac = prueth_iep_to_emac(iep);
+	struct prueth_emac *emac = clockops_data;
 	u64 ts = 0;
 	u32 iepcount_hi, iepcount_lo, hi_rollover_count;
 	u32 iepcount_hi_r, hi_rollover_count_r;
@@ -1635,27 +1634,27 @@ static u64 prueth_iep_gettime(struct icss_iep *iep)
 					TIMESYNC_FW_HI_ROLLOVER_COUNT_OFFSET;
 
 	do {
-		regmap_read(iep->map, IEP_COUNT_HIGH_REG, &iepcount_hi);
+		iepcount_hi = icss_iep_get_count_hi(emac->iep);
 		iepcount_hi += *fw_count_hi_offset_addr;
 		hi_rollover_count = *hi_rollover_count_addr;
-		regmap_read(iep->map, IEP_COUNT_LOW_REG, &iepcount_lo);
+		iepcount_lo = icss_iep_get_count_low(emac->iep);
 
-		regmap_read(iep->map, IEP_COUNT_HIGH_REG, &iepcount_hi_r);
+		iepcount_hi_r = icss_iep_get_count_hi(emac->iep);
 		iepcount_hi_r += *fw_count_hi_offset_addr;
 		hi_rollover_count_r = *hi_rollover_count_addr;
 	} while ((iepcount_hi_r != iepcount_hi) ||
 		 (hi_rollover_count != hi_rollover_count_r));
 
 	ts = ((u64)hi_rollover_count) << 23 | iepcount_hi;
-	ts = ts * iep->cycle_time_ns + iepcount_lo;
+	ts = ts * (u64)IEP_DEFAULT_CYCLE_TIME_NS + iepcount_lo;
 
 	return ts;
 }
 
-static void prueth_iep_settime(struct icss_iep *iep, u64 ns)
+static void prueth_iep_settime(void *clockops_data, u64 ns)
 {
-	struct prueth_emac *emac = prueth_iep_to_emac(iep);
 	struct icssg_setclock_desc sc_desc, *sc_descp;
+	struct prueth_emac *emac = clockops_data;
 	u32 cycletime;
 	u64 cyclecount;
 	int timeout;
@@ -1665,7 +1664,7 @@ static void prueth_iep_settime(struct icss_iep *iep, u64 ns)
 
 	sc_descp = emac->dram.va + TIMESYNC_FW_SETCLOCK_DESC_OFFSET;
 
-	cycletime = iep->cycle_time_ns;
+	cycletime = IEP_DEFAULT_CYCLE_TIME_NS;
 	cyclecount = ns / cycletime;
 
 	memset(&sc_desc, 0, sizeof(sc_desc));
@@ -1819,7 +1818,6 @@ static int prueth_netdev_init(struct prueth *prueth,
 	struct prueth_emac *emac;
 	const u8 *mac_addr;
 	int ret;
-	struct regmap *iep_map;
 
 	port = prueth_node_port(eth_node);
 	if (port < 0)
@@ -1903,30 +1901,21 @@ skip_irq:
 		goto free;
 	}
 
-	if (!of_property_read_bool(eth_node, "iep"))
+	emac->iep = icss_iep_get(eth_node);
+	if (IS_ERR(emac->iep)) {
+		ret = PTR_ERR(emac->iep);
+		if (ret == -EPROBE_DEFER)
+			goto free;
 		goto skip_iep;
-
-	iep_map = syscon_regmap_lookup_by_phandle(eth_node, "iep");
-	if (IS_ERR(iep_map)) {
-		ret = PTR_ERR(iep_map);
-		if (ret != -EPROBE_DEFER)
-			dev_err(prueth->dev, "couldn't get iep regmap\n");
-		goto free;
 	}
 
-	if (prueth->is_sr1) {
-		ret = icss_iep_init(&emac->iep, prueth->dev, iep_map,
-				    IEP_REFCLK_FREQ, 0);
-	} else {
-		emac->iep.ops = &prueth_iep_clockops;
-		ret = icss_iep_init(&emac->iep, prueth->dev, iep_map,
-				    IEP_REFCLK_FREQ,
-				    IEP_DEFAULT_CYCLE_TIME_NS);
-	}
-
+	if (prueth->is_sr1)
+		ret = icss_iep_init(emac->iep, NULL, NULL, 0);
+	else
+		ret = icss_iep_init(emac->iep, &prueth_iep_clockops,
+				    emac, IEP_DEFAULT_CYCLE_TIME_NS);
 	if (ret) {
 		dev_err(prueth->dev, "failed to init iep\n");
-		emac->iep.ops = NULL;
 		goto free;
 	}
 
@@ -1987,8 +1976,9 @@ static void prueth_netdev_exit(struct prueth *prueth,
 	phy_disconnect(emac->phydev);
 
 	if (emac->iep_initialized) {
-		icss_iep_exit(&emac->iep);
+		icss_iep_exit(emac->iep);
 		emac->iep_initialized = 0;
+		icss_iep_put(emac->iep);
 	}
 
 	if (of_phy_is_fixed_link(emac->phy_node))

drivers/net/ethernet/ti/icssg_prueth.h
@@ -127,7 +127,7 @@ struct prueth_emac {
 	int phy_if;
 	struct phy_device *phydev;
 	enum prueth_port port_id;
-	struct icss_iep iep;
+	struct icss_iep *iep;
 	bool iep_initialized;
 	struct hwtstamp_config tstamp_config;
 	unsigned int rx_ts_enabled : 1;

commit 93b768aad9f53a2c856d022bcecf7d5b854a4fff

net: ethernet: ti: icss-iep: make icss_iep_probe() static
Fix below compilation warning when compiled with W=1 by making
icss_iep_probe() static.

drivers/net/ethernet/ti/icss_iep.c:518:5: warning: no previous prototype
for ‘icss_iep_probe’ [-Wmissing-prototypes]
int icss_iep_probe(struct platform_device *pdev)

Fixes: commit 8c2782d9cf85 ("net: ethernet: ti: icss-iep: Make icss-iep a separate driver")
Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

drivers/net/ethernet/ti/icss_iep.c
@@ -515,7 +515,7 @@ EXPORT_SYMBOL_GPL(icss_iep_exit);
 
 static const struct of_device_id icss_iep_of_match[];
 
-int icss_iep_probe(struct platform_device *pdev)
+static int icss_iep_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct icss_iep *iep;