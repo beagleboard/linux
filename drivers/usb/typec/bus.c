0a05f90d2e89eda5ad02516431a058fdfce0644b
Robert Nelson <robertcnelson@gmail.com>
2021-05-14 13:05:51 -0500

    backports: typec: from: linux.git
    Reference: v5.5.19
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/usb/typec/bus.c
+++ drivers/usb/typec/bus.c
@@ -192,10 +192,7 @@ EXPORT_SYMBOL_GPL(typec_altmode_vdm);
 const struct typec_altmode *
 typec_altmode_get_partner(struct typec_altmode *adev)
 {
-	if (!adev || !to_altmode(adev)->partner)
-		return NULL;
-
-	return &to_altmode(adev)->partner->adev;
+	return adev ? &to_altmode(adev)->partner->adev : NULL;
 }
 EXPORT_SYMBOL_GPL(typec_altmode_get_partner);
 
