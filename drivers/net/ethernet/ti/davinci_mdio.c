0c47dd439990e16556532522f510b53b13a63492
Will Eccles <will@eccles.dev>
2021-05-24 12:48:51 -0400

    cpsw: fix undefined function with PM disabled
    When CONFIG_PM is not enabled, the function
    davinci_mdio_update_dt_from_phymask is not defined. This patch fixes
    this so that the build does not fail with CONFIG_PM disabled.
    
    Signed-off-by: Will Eccles <will@eccles.dev>

--- drivers/net/ethernet/ti/davinci_mdio.c
+++ drivers/net/ethernet/ti/davinci_mdio.c
@@ -484,6 +484,8 @@ static int davinci_mdio_runtime_resume(struct device *dev)
 	davinci_mdio_enable(data);
 	return 0;
 }
+#endif
+
 static void davinci_mdio_update_dt_from_phymask(u32 phy_mask)
 {
 	int i, len, skip;
@@ -571,7 +573,6 @@ err_out:
 		kfree(phy_id_property);
 	}
 }
-#endif
 
 #ifdef CONFIG_PM_SLEEP
 static int davinci_mdio_suspend(struct device *dev)
