8a53c21fca1bb4c07cb2b91009b46b52f3126752
Robert Nelson <robertcnelson@gmail.com>
2021-09-07 17:28:46 -0500

    Revert "kthread: Do not preempt current task if it is going to call schedule()"
    This reverts commit 26c7295be0c5e6da3fa45970e9748be983175b1b.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -243,15 +243,8 @@ static void __kthread_parkme(struct kthread *self)
 		if (!test_bit(KTHREAD_SHOULD_PARK, &self->flags))
 			break;
 
-		/*
-		 * Thread is going to call schedule(), do not preempt it,
-		 * or the caller of kthread_park() may spend more time in
-		 * wait_task_inactive().
-		 */
-		preempt_disable();
 		complete(&self->parked);
-		schedule_preempt_disabled();
-		preempt_enable();
+		schedule();
 	}
 	__set_current_state(TASK_RUNNING);
 }
@@ -305,14 +298,8 @@ static int kthread(void *_create)
 	/* OK, tell user we're spawned, wait for stop or wakeup */
 	__set_current_state(TASK_UNINTERRUPTIBLE);
 	create->result = current;
-	/*
-	 * Thread is going to call schedule(), do not preempt it,
-	 * or the creator may spend more time in wait_task_inactive().
-	 */
-	preempt_disable();
 	complete(done);
-	schedule_preempt_disabled();
-	preempt_enable();
+	schedule();
 
 	ret = -EINTR;
 	if (!test_bit(KTHREAD_SHOULD_STOP, &self->flags)) {
