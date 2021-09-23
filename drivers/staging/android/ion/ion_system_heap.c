e05408813ec2755fcd4d66fac6adea93b2943135
Cheng Chao <cs.os.kernel@gmail.com>
2021-09-23 22:18:14 +0800

    staging: android: ion: fix page is NULL
    Fixes: commit e7f63771b60e ("ION: Sys_heap: Add cached pool to spead up cached buffer alloc")
    the commit e7f63771b60e introduced the bug which didn't test page which maybe NULL.
    and previous logic was right.
    
    the e7f63771b60e has been merged in v4.8-rc3, only longterm 4.9.x has this bug,
    and other longterm/stable version have not.
    
    kernel panic is here when page is NULL:
    
    Unable to handle kernel paging request at virtual address b0380000
    pgd = d9d94000
    [b0380000] *pgd=00000000
    Internal error: Oops: 2805 [#1] PREEMPT SMP ARM
    ...
    task: daa2dd00 task.stack: da194000
    PC is at v7_dma_clean_range+0x1c/0x34
    LR is at arm_dma_sync_single_for_device+0x44/0x58
    pc : [<c011aa0c>]    lr : [<c011645c>]    psr: 200f0013
    sp : da195da0  ip : dc1f9000  fp : c1043dc4
    r10: 00000000  r9 : c16f1f58  r8 : 00000001
    r7 : c1621f94  r6 : c0116418  r5 : 00000000  r4 : c011aa58
    r3 : 0000003f  r2 : 00000040  r1 : b0480000  r0 : b0380000
    Flags: nzCv  IRQs on  FIQs on  Mode SVC_32  ISA ARM  Segment none
    Control: 10c5383d  Table: 19d9406a  DAC: 00000051
    ...
    [<c011aa0c>] (v7_dma_clean_range) from [<c011645c>] (arm_dma_sync_single_for_device+0x44/0x58)
    [<c011645c>] (arm_dma_sync_single_for_device) from [<c0117088>] (arm_dma_sync_sg_for_device+0x50/0x7c)
    [<c0117088>] (arm_dma_sync_sg_for_device) from [<c0c033c4>] (ion_pages_sync_for_device+0xb0/0xec)
    [<c0c033c4>] (ion_pages_sync_for_device) from [<c0c054ac>] (ion_system_heap_allocate+0x2a0/0x2e0)
    [<c0c054ac>] (ion_system_heap_allocate) from [<c0c02c78>] (ion_alloc+0x12c/0x494)
    [<c0c02c78>] (ion_alloc) from [<c0c03eac>] (ion_ioctl+0x510/0x63c)
    [<c0c03eac>] (ion_ioctl) from [<c027c4b0>] (do_vfs_ioctl+0xa8/0x9b4)
    [<c027c4b0>] (do_vfs_ioctl) from [<c027ce28>] (SyS_ioctl+0x6c/0x7c)
    [<c027ce28>] (SyS_ioctl) from [<c0108a40>] (ret_fast_syscall+0x0/0x48)
    Code: e3a02004 e1a02312 e2423001 e1c00003 (ee070f3a)
    ---[ end trace 89278304932c0e87 ]---
    Kernel panic - not syncing: Fatal exception
    
    Signed-off-by: Cheng Chao <cs.os.kernel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -75,7 +75,7 @@ static struct page *alloc_buffer_page(struct ion_system_heap *heap,
 
 	page = ion_page_pool_alloc(pool);
 
-	if (cached)
+	if (page && cached)
 		ion_pages_sync_for_device(NULL, page, PAGE_SIZE << order,
 					  DMA_BIDIRECTIONAL);
 	return page;
