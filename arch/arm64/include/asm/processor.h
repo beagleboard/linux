7f319f30e417373dd865a54a8748a87e2e0d6ec2
James Morse <james.morse@arm.com>
2022-04-06 17:45:05 +0100

    arm64: Remove useless UAO IPI and describe how this gets enabled
    commit c8b06e3fddddaae1a87ed479edcb8b3d85caecc7 upstream.
    
    Since its introduction, the UAO enable call was broken, and useless.
    commit 2a6dcb2b5f3e ("arm64: cpufeature: Schedule enable() calls instead
    of calling them via IPI"), fixed the framework so that these calls
    are scheduled, so that they can modify PSTATE.
    
    Now it is just useless. Remove it. UAO is enabled by the code patching
    which causes get_user() and friends to use the 'ldtr' family of
    instructions. This relies on the PSTATE.UAO bit being set to match
    addr_limit, which we do in uao_thread_switch() called via __switch_to().
    
    All that is needed to enable UAO is patch the code, and call schedule().
    __apply_alternatives_multi_stop() calls stop_machine() when it modifies
    the kernel text to enable the alternatives, (including the UAO code in
    uao_thread_switch()). Once stop_machine() has finished __switch_to() is
    called to reschedule the original task, this causes PSTATE.UAO to be set
    appropriately. An explicit enable() call is not needed.
    
    Reported-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -220,7 +220,6 @@ static inline void spin_lock_prefetch(const void *ptr)
 #endif
 
 int cpu_enable_pan(void *__unused);
-int cpu_enable_uao(void *__unused);
 int cpu_enable_cache_maint_trap(void *__unused);
 
 #endif /* __ASSEMBLY__ */
