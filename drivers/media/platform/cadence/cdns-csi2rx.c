81f8b0a048e4fead18b57c35fadf473ae4b1f845
Pratyush Yadav <p.yadav@ti.com>
2022-04-13 00:59:29 +0530

    media: cadence: csi2rx: Add RAW12 formats
    Add RAW12 formats in the formats table to allow capturing those formats.
    
    Signed-off-by: Pratyush Yadav <p.yadav@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- drivers/media/platform/cadence/cdns-csi2rx.c
+++ drivers/media/platform/cadence/cdns-csi2rx.c
@@ -152,6 +152,22 @@ static const struct csi2rx_fmt formats[] = {
 		.code	= MEDIA_BUS_FMT_SRGGB10_1X10,
 		.bpp	= 10,
 	},
+	{
+		.code	= MEDIA_BUS_FMT_SBGGR12_1X12,
+		.bpp	= 12,
+	},
+	{
+		.code	= MEDIA_BUS_FMT_SGBRG12_1X12,
+		.bpp	= 12,
+	},
+	{
+		.code	= MEDIA_BUS_FMT_SGRBG12_1X12,
+		.bpp	= 12,
+	},
+	{
+		.code	= MEDIA_BUS_FMT_SRGGB12_1X12,
+		.bpp	= 12,
+	},
 };
 
 static u8 csi2rx_get_bpp(u32 code)
