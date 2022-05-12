d873c105e06941952084a0eb231f3a5b0d806b65
Robert Nelson <robertcnelson@gmail.com>
2022-05-12 12:51:22 -0500

    CI: cleanup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -8,32 +8,5 @@ export CC=/usr/bin/arm-linux-gnueabihf-
 make ARCH=arm CROSS_COMPILE=${CC} clean
 make ARCH=arm CROSS_COMPILE=${CC} bb.org_defconfig
 
-echo "[make ARCH=arm -j4 CROSS_COMPILE=\"${binary}\" zImage]"
-make ARCH=arm -j4 CROSS_COMPILE="ccache ${CC}" zImage
-if [ ! -f arch/arm/boot/zImage ] ; then
-	echo "failed: [arch/arm/boot/zImage]"
-	exit 1
-fi
-
-echo "[make ARCH=arm -j4 CROSS_COMPILE=\"${binary}\" modules]"
-make ARCH=arm -j4 CROSS_COMPILE="ccache ${CC}" modules
-if [ ! -f drivers/spi/spidev.ko ] ; then
-	echo "failed: [drivers/spi/spidev.ko]"
-	exit 1
-fi
-
-echo "[make ARCH=arm CROSS_COMPILE=\"${binary}\" dtbs]"
-make ARCH=arm CROSS_COMPILE="ccache ${CC}" dtbs
-if [ ! -f arch/arm/boot/dts/am335x-boneblack.dtb ] ; then
-	echo "failed: [arch/arm/boot/dts/am335x-boneblack.dtb]"
-	exit 1
-else
-	if [ -f arch/arm/boot/dts/am335x-pocketbeagle.dts ] ; then
-		if [ ! -f arch/arm/boot/dts/am335x-pocketbeagle.dtb ] ; then
-			echo "failed: [arch/arm/boot/dts/am335x-pocketbeagle.dtb]"
-			exit 1
-		fi
-	fi
-fi
-
-make ARCH=arm CROSS_COMPILE=${CC} clean
+echo "make -j4 ARCH=arm KBUILD_DEBARCH=armhf CROSS_COMPILE=${CC} bindeb-pkg"
+make -j4 ARCH=arm KBUILD_DEBARCH=armhf KDEB_PKGVERSION=1xross CROSS_COMPILE=${CC} bindeb-pkg
