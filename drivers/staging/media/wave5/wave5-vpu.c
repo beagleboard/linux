d78428a3f04fac1f88b2e709861dd55caa1997ab
Prasanth Babu Mantena <p-mantena@ti.com>
2022-06-14 19:49:50 +0000

    Revert "staging: media: wave5: vpu: Include io.h"
    This reverts commit 48df94dc7b64b8f0519a760d8013b80001b3db92.
    
    Reverting back to apply more recent patchset submitted to upstream.
    
    Signed-off-by: Prasanth Babu Mantena <p-mantena@ti.com>

--- drivers/staging/media/wave5/wave5-vpu.c
+++ drivers/staging/media/wave5/wave5-vpu.c
@@ -8,7 +8,6 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/clk.h>
-#include <linux/io.h>
 #include <linux/firmware.h>
 #include <linux/interrupt.h>
 #include "wave5-vpu.h"
