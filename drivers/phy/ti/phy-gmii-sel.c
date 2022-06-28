71f19d328da530aafb27b3eac63ed57bd6d5667c
Roger Quadros <rogerq@kernel.org>
2022-06-28 10:27:12 +0300

    phy: ti: phy-gmii-sel: Add support for XAUI and USXGMII
    XAUI and USXGMII PHY modes are supported via XFI mode setting
    in the control register.
    
    Signed-off-by: Roger Quadros <rogerq@kernel.org>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- drivers/phy/ti/phy-gmii-sel.c
+++ drivers/phy/ti/phy-gmii-sel.c
@@ -25,6 +25,7 @@
 /* J72xx SoC specific definitions for the CONTROL port */
 #define J72XX_GMII_SEL_MODE_SGMII	3
 #define J72XX_GMII_SEL_MODE_QSGMII	4
+#define J72XX_GMII_SEL_MODE_XFI		5
 #define J72XX_GMII_SEL_MODE_QSGMII_SUB	6
 
 #define PHY_GMII_PORT(n)	BIT((n) - 1)
@@ -111,6 +112,18 @@ static int phy_gmii_sel_mode(struct phy *phy, enum phy_mode mode, int submode)
 			goto unsupported;
 		else
 			gmii_sel_mode = J72XX_GMII_SEL_MODE_SGMII;
+		break;
+
+	case PHY_INTERFACE_MODE_XAUI:
+		if (!(soc_data->extra_modes & BIT(PHY_INTERFACE_MODE_XAUI)))
+			goto unsupported;
+		gmii_sel_mode = J72XX_GMII_SEL_MODE_XFI;
+		break;
+
+	case PHY_INTERFACE_MODE_USXGMII:
+		if (!(soc_data->extra_modes & BIT(PHY_INTERFACE_MODE_USXGMII)))
+			goto unsupported;
+		gmii_sel_mode = J72XX_GMII_SEL_MODE_XFI;
 		break;
 
 	default:
@@ -220,7 +233,10 @@ static const
 struct phy_gmii_sel_soc_data phy_gmii_sel_cpsw5g_soc_j7200 = {
 	.use_of_data = true,
 	.regfields = phy_gmii_sel_fields_am654,
-	.extra_modes = BIT(PHY_INTERFACE_MODE_QSGMII) | BIT(PHY_INTERFACE_MODE_SGMII),
+	.extra_modes = BIT(PHY_INTERFACE_MODE_QSGMII) |
+		       BIT(PHY_INTERFACE_MODE_SGMII) |
+		       BIT(PHY_INTERFACE_MODE_XAUI) |
+		       BIT(PHY_INTERFACE_MODE_USXGMII),
 };
 
 static const
