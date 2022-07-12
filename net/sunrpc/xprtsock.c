0971bb6a94c42ae9bd9aff4b2ca529e66ae608fd
Robert Nelson <robertcnelson@gmail.com>
2022-07-12 14:23:40 -0500

    Revert "SUNRPC: Ensure we flush any closed sockets before xs_xprt_free()"
    This reverts commit e68b60ae29de10c7bd7636e227164a8dbe305a82.

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -871,7 +871,7 @@ static int xs_local_send_request(struct rpc_rqst *req)
 
 	/* Close the stream if the previous transmission was incomplete */
 	if (xs_send_request_was_aborted(transport, req)) {
-		xprt_force_disconnect(xprt);
+		xs_close(xprt);
 		return -ENOTCONN;
 	}
 
@@ -909,7 +909,7 @@ static int xs_local_send_request(struct rpc_rqst *req)
 			-status);
 		fallthrough;
 	case -EPIPE:
-		xprt_force_disconnect(xprt);
+		xs_close(xprt);
 		status = -ENOTCONN;
 	}
 
@@ -1191,16 +1191,6 @@ static void xs_reset_transport(struct sock_xprt *transport)
 
 	if (sk == NULL)
 		return;
-	/*
-	 * Make sure we're calling this in a context from which it is safe
-	 * to call __fput_sync(). In practice that means rpciod and the
-	 * system workqueue.
-	 */
-	if (!(current->flags & PF_WQ_WORKER)) {
-		WARN_ON_ONCE(1);
-		set_bit(XPRT_CLOSE_WAIT, &xprt->state);
-		return;
-	}
 
 	if (atomic_read(&transport->xprt.swapper))
 		sk_clear_memalloc(sk);
@@ -1224,7 +1214,7 @@ static void xs_reset_transport(struct sock_xprt *transport)
 	mutex_unlock(&transport->recv_mutex);
 
 	trace_rpc_socket_close(xprt, sock);
-	__fput_sync(filp);
+	fput(filp);
 
 	xprt_disconnect_done(xprt);
 }
