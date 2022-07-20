062e61ccef0202b6e7bf06908220e22c1ea46a41
Robert Nelson <robertcnelson@gmail.com>
2022-07-20 14:20:02 -0500

    backports: spidev: from: linux.git
    Reference: v5.13.19
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -682,6 +682,8 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "lwn,bk4" },
 	{ .compatible = "dh,dhcom-board" },
 	{ .compatible = "menlo,m53cpld" },
+	{ .compatible = "cisco,spi-petra" },
+	{ .compatible = "micron,spi-authenta" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
