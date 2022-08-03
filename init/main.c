f0c556f4b05e48f3b001a429125f0ccceb9a7f29
Hans-Christian Noren Egtvedt <hegtvedt@cisco.com>
2022-08-03 16:01:51 +0200

    random: only call boot_init_stack_canary() once
    In commit 166a592cad36 ("random: move rand_initialize() earlier") the
    boot_init_stack_canary() call was added after the new random_init()
    call.
    
    However, the upstream commit d55535232c3d ("random: move
    rand_initialize() earlier") also included removing the earlier call to
    boot_init_stack_canary(), making sure this call is done after
    random_init().
    
    Hence fix what I assume is a wrong merge conflict resolution on the
    linux-4.9.y stable branch.
    
    Signed-off-by: Hans-Christian Noren Egtvedt <hegtvedt@cisco.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- init/main.c
+++ init/main.c
@@ -500,13 +500,6 @@ asmlinkage __visible void __init start_kernel(void)
 	page_address_init();
 	pr_notice("%s", linux_banner);
 	setup_arch(&command_line);
-	/*
-	 * Set up the the initial canary and entropy after arch
-	 * and after adding latent and command line entropy.
-	 */
-	add_latent_entropy();
-	add_device_randomness(command_line, strlen(command_line));
-	boot_init_stack_canary();
 	mm_init_cpumask(&init_mm);
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
