783b18ea2377869315a5c784c64ab24313530fd6
Apurva Nandan <a-nandan@ti.com>
2022-08-29 11:22:43 +0530

    spi: spi-mem: Add DTR templates for cmd, address, dummy and data phase
    Setting dtr field of spi_mem_op is useful when creating templates
    for DTR ops in spinand.h. Also, 2 bytes cmd phases are required when
    operating in Octal DTR SPI mode.
    
    Create new templates for dtr mode cmd, address, dummy and data phase
    in spi_mem_op, which set the dtr field to 1 and also allow passing
    the nbytes for the cmd phase.
    
    Signed-off-by: Apurva Nandan <a-nandan@ti.com>

--- include/linux/spi/spi-mem.h
+++ include/linux/spi/spi-mem.h
@@ -20,11 +20,27 @@
 		.nbytes = 1,					\
 	}
 
+#define SPI_MEM_OP_CMD_DTR(__nbytes, __opcode, __buswidth)	\
+	{							\
+		.nbytes = __nbytes,				\
+		.opcode = __opcode,				\
+		.buswidth = __buswidth,				\
+		.dtr = 1,					\
+	}
+
 #define SPI_MEM_OP_ADDR(__nbytes, __val, __buswidth)		\
 	{							\
 		.nbytes = __nbytes,				\
 		.val = __val,					\
 		.buswidth = __buswidth,				\
+	}
+
+#define SPI_MEM_OP_ADDR_DTR(__nbytes, __val, __buswidth)	\
+	{							\
+		.nbytes = __nbytes,				\
+		.val = __val,					\
+		.buswidth = __buswidth,				\
+		.dtr = 1,					\
 	}
 
 #define SPI_MEM_OP_NO_ADDR	{ }
@@ -33,6 +49,13 @@
 	{							\
 		.nbytes = __nbytes,				\
 		.buswidth = __buswidth,				\
+	}
+
+#define SPI_MEM_OP_DUMMY_DTR(__nbytes, __buswidth)		\
+	{							\
+		.nbytes = __nbytes,				\
+		.buswidth = __buswidth,				\
+		.dtr = 1,					\
 	}
 
 #define SPI_MEM_OP_NO_DUMMY	{ }
@@ -45,12 +68,30 @@
 		.buswidth = __buswidth,				\
 	}
 
+#define SPI_MEM_OP_DATA_IN_DTR(__nbytes, __buf, __buswidth)	\
+	{							\
+		.dir = SPI_MEM_DATA_IN,				\
+		.nbytes = __nbytes,				\
+		.buf.in = __buf,				\
+		.buswidth = __buswidth,				\
+		.dtr = 1,					\
+	}
+
 #define SPI_MEM_OP_DATA_OUT(__nbytes, __buf, __buswidth)	\
 	{							\
 		.dir = SPI_MEM_DATA_OUT,			\
 		.nbytes = __nbytes,				\
 		.buf.out = __buf,				\
 		.buswidth = __buswidth,				\
+	}
+
+#define SPI_MEM_OP_DATA_OUT_DTR(__nbytes, __buf, __buswidth)	\
+	{							\
+		.dir = SPI_MEM_DATA_OUT,			\
+		.nbytes = __nbytes,				\
+		.buf.out = __buf,				\
+		.buswidth = __buswidth,				\
+		.dtr = 1,					\
 	}
 
 #define SPI_MEM_OP_NO_DATA	{ }
