9437360f924b1a0802f624a5b7311cb00229017e
Matt Ranostay <mranostay@ti.com>
2022-10-25 20:17:10 -0700

    PCI: j721e: correct PCIe lane selection mask
    PCIe lane mask should be based on the maximum number of lanes and not the
    currently selected lane count, since we have no idea what the bootloader may
    have initialized to this register.
    
    Reported-by: Vignesh Raghavendra <vigneshr@ti.com>
    Fixes: 3c97d831018a ("PCI: j721e: correct lane count mask based on lane numbers")
    Signed-off-by: Matt Ranostay <mranostay@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- drivers/pci/controller/cadence/pci-j721e.c
+++ drivers/pci/controller/cadence/pci-j721e.c
@@ -71,6 +71,7 @@ struct j721e_pcie {
 	struct device		*dev;
 	struct clk		*refclk;
 	u32			mode;
+	u32			max_lanes;
 	u32			num_lanes;
 	struct cdns_pcie	*cdns_pcie;
 	void __iomem		*user_cfg_base;
@@ -349,7 +350,7 @@ static int j721e_pcie_set_lane_count(struct j721e_pcie *pcie,
 	u32 val = 0;
 	int ret;
 
-	if (lanes == 4)
+	if (pcie->max_lanes == 4)
 		mask = GENMASK(9, 8);
 
 	val = LANE_COUNT(lanes - 1);
@@ -554,6 +555,7 @@ static int j721e_pcie_probe(struct platform_device *pdev)
 			      num_lanes, data->max_lanes);
 		num_lanes = 1;
 	}
+	pcie->max_lanes = data->max_lanes;
 	pcie->num_lanes = num_lanes;
 
 	if (dma_set_mask_and_coherent(dev, DMA_BIT_MASK(48)))
