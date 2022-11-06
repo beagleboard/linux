0e705eef69eaf2a65a8f85a173e58e41f852893e
Robert Nelson <robertcnelson@gmail.com>
2022-11-06 11:09:45 -0600

    enable: Jenkins/gitlab-ci
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ jenkins_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#git clone -b 5.10 https://github.com/beagleboard/linux --depth=10
+#cd ./linux
+
+CORES=$(getconf _NPROCESSORS_ONLN)
+
+export CC=/usr/bin/arm-linux-gnueabihf-
+
+make ARCH=arm CROSS_COMPILE=${CC} clean
+make ARCH=arm CROSS_COMPILE=${CC} bb.org_defconfig
+
+echo "make -j${CORES} ARCH=arm KBUILD_DEBARCH=armhf CROSS_COMPILE=${CC} bindeb-pkg"
+make -j${CORES} ARCH=arm KBUILD_DEBARCH=armhf KDEB_PKGVERSION=1xross CROSS_COMPILE=${CC} bindeb-pkg
+mv ../*.deb ./
