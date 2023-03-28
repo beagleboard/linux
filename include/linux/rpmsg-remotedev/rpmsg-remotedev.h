c2cbcdaf6b734ebb981d18b45683de96183111ef
Subhajit Paul <subhajit_paul@ti.com>
2023-03-28 15:24:46 +0530

    rpmsg-kdrv: add support for DISPLAY class
    This patch adds support for DISPLAY class of virtual devices which can be
    used by DRM framework to access display-specific services exposed by remote
    cores.
    
    The following APIs are exposed by the virtual backend:
    * ready: Query remote side initialised display
      H/W and is ready to accept frames.
    
      Uses REQUEST / RESPONSE protocol
    
    * get_res_info: Get information on available display
      channels / resources (virtual planes , virtual CRTCs etc)
      exposed by the remote server
    
      Uses REQUEST / RESPONSE protocol
    
    * commit: Send a commit request (atomic bundle of all
      plane updates for a virtual CRTC) to be applied on the
      H/W.
    
      Uses REQUEST / RESPONSE protocol
    
      RESPONSE indicates whether the request could be
      processed or not.
    
    The actual indications of H/W updates and buffer reusability are sent as
    events from server to client which can be handled by registering
    .commit_done() and .buffer_done() callbacks
    
    Signed-off-by: Subhajit Paul <subhajit_paul@ti.com>
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Acked-by: Ravi Gunasekaran <r-gunasekaran@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- include/linux/rpmsg-remotedev/rpmsg-remotedev.h
+++ include/linux/rpmsg-remotedev/rpmsg-remotedev.h
@@ -12,9 +12,81 @@ struct rpmsg_remotedev;
 /* Defines for demo device */
 #define REMOTEDEV_DEMO_MSG_LEN		(128)
 
+/* Defines for display device */
+#define RPMSG_REMOTEDEV_DISPLAY_MAX_PLANES		(3)
+#define RPMSG_REMOTEDEV_DISPLAY_MAX_DISPS		(8)
+#define RPMSG_REMOTEDEV_DISPLAY_MAX_PIPES		(8)
+#define RPMSG_REMOTEDEV_DISPLAY_MAX_FORMATS		(32)
+#define RPMSG_REMOTEDEV_DISPLAY_MAX_ZORDERS		(8)
+
+/* Struct definitions for display device */
+struct rpmsg_remotedev_display_buffer {
+	u32 width;
+	u32 height;
+	u32 format;
+	u32 num_planes;
+	dma_addr_t planes[RPMSG_REMOTEDEV_DISPLAY_MAX_PLANES];
+	u32 pitches[RPMSG_REMOTEDEV_DISPLAY_MAX_PLANES];
+	struct rpmsg_remotedev *rdev;
+	void *priv;
+};
+
+struct rpmsg_remotedev_display_pipe_update {
+	u32 pipe_id;
+	bool enabled;
+	u32 dst_w;
+	u32 dst_h;
+	u32 dst_x;
+	u32 dst_y;
+	struct rpmsg_remotedev_display_buffer *buffer;
+};
+
+struct rpmsg_remotedev_display_commit {
+	u32 disp_id;
+	u32 num_pipe_updates;
+	struct rpmsg_remotedev_display_pipe_update pipes[RPMSG_REMOTEDEV_DISPLAY_MAX_PIPES];
+	struct rpmsg_remotedev *rdev;
+	void *priv;
+};
+
+struct rpmsg_remotedev_display_pipe {
+	u32 pipe_id;
+	bool can_scale;
+	bool can_mod_win;
+	u32 fixed_win_x;
+	u32 fixed_win_y;
+	u32 fixed_win_w;
+	u32 fixed_win_h;
+	u32 initial_zorder;
+	u32 num_formats;
+	u32 formats[RPMSG_REMOTEDEV_DISPLAY_MAX_FORMATS];
+	u32 num_allowed_zorders;
+	u32 allowed_zorders[RPMSG_REMOTEDEV_DISPLAY_MAX_ZORDERS];
+};
+
+struct rpmsg_remotedev_display_disp {
+	u32 disp_id;
+	u32 width;
+	u32 height;
+	u32 refresh;
+	u32 num_pipes;
+	struct rpmsg_remotedev_display_pipe pipes[RPMSG_REMOTEDEV_DISPLAY_MAX_PIPES];
+};
+
+struct rpmsg_remotedev_display_resinfo {
+	u32 num_disps;
+	struct rpmsg_remotedev_display_disp disps[RPMSG_REMOTEDEV_DISPLAY_MAX_DISPS];
+};
+
 /* callbacks for demo device */
 struct rpmsg_remotedev_demo_cb {
 	void (*s2c_message)(void *data, ssize_t len, void *cb_data);
+};
+
+/* callbacks for display device */
+struct rpmsg_remotedev_display_cb {
+	void (*commit_done)(struct rpmsg_remotedev_display_commit *commit, void *cb_data);
+	void (*buffer_done)(struct rpmsg_remotedev_display_buffer *buffer, void *cb_data);
 };
 
 /* requests for demo device */
@@ -24,8 +96,17 @@ struct rpmsg_remotedev_demo_ops {
 	int (*c2s_message)(struct rpmsg_remotedev *rdev, void *c2s_msg_data, ssize_t len);
 };
 
+/* requests for display device */
+struct rpmsg_remotedev_display_ops {
+	bool (*ready)(struct rpmsg_remotedev *rdev);
+	int (*get_res_info)(struct rpmsg_remotedev *rdev,
+			    struct rpmsg_remotedev_display_resinfo *res);
+	int (*commit)(struct rpmsg_remotedev *rdev, struct rpmsg_remotedev_display_commit *commit);
+};
+
 enum rpmsg_remotedev_type {
 	RPMSG_REMOTEDEV_DEMO_DEVICE,
+	RPMSG_REMOTEDEV_DISPLAY_DEVICE,
 };
 
 struct rpmsg_remotedev {
@@ -35,6 +116,10 @@ struct rpmsg_remotedev {
 			struct rpmsg_remotedev_demo_ops *ops;
 			struct rpmsg_remotedev_demo_cb *cb_ops;
 		} demo;
+		struct {
+			struct rpmsg_remotedev_display_ops *ops;
+			struct rpmsg_remotedev_display_cb *cb_ops;
+		} display;
 	} device;
 	void *cb_data;
 };
