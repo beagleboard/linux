1054df2805fd51d2e675274233c95f986023be2c
Subhajit Paul <subhajit_paul@ti.com>
2023-03-28 15:24:44 +0530

    rpmsg-kdrv: Add support for device virtualization
    In J721E, multiple compute entities can run in parallel,
    and can require to use any given H/W simultaneously.
    
    Or, a device may be scheduled between multiple cores, and
    a dedicated core can manage the switching based on
    requests / policies
    
    rpmsg-kdrv is a device virtualization framework that uses
    rpmsg core functionalities. In a system using this feature,
    a remoteproc is expected to advertise virtual device
    endpoints that the host can connect to and request for
    services.
    
    Signed-off-by: Subhajit Paul <subhajit_paul@ti.com>
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Tested-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Acked-by: Ravi Gunasekaran <r-gunasekaran@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

---
+++ drivers/rpmsg-kdrv/rpmsg_kdrv_internal.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ * Author: Subhajit Paul <subhajit_paul@ti.com>
+ */
+
+#ifndef __RPMSG_KDRV_INTERNAL_H__
+#define __RPMSG_KDRV_INTERNAL_H__
+
+#define RRMSG_KDRV_CALLBACK_DONE		(0)
+#define RRMSG_KDRV_CALLBACK_MORE		(1)
+
+struct rpmsg_kdrv_device {
+	struct device dev;
+	struct rpmsg_device *rpdev;
+	int device_type;
+	int device_id;
+	void *device_data;
+	int device_data_len;
+	char *device_name;
+	void *device_private;
+	void *driver_private;
+};
+
+struct rpmsg_kdrv_driver {
+	struct device_driver drv;
+	int device_type;
+	int (*probe)(struct rpmsg_kdrv_device *dev);
+	void (*remove)(struct rpmsg_kdrv_device *dev);
+	int (*callback)(struct rpmsg_kdrv_device *dev, void *msg, int len);
+};
+
+typedef int (*request_cb_t)(void *data, void *req, int req_sz, void *resp, int resp_sz);
+
+int rpmsg_kdrv_register_driver(struct rpmsg_kdrv_driver *drv);
+
+int rpmsg_kdrv_send_request_with_callback(struct rpmsg_device *rpdev, u32 device_id, void *message,
+					  u32 message_size, void *cb_data, request_cb_t callback);
+int rpmsg_kdrv_send_request_with_response(struct rpmsg_device *rpdev, u32 device_id, void *message,
+					  u32 message_size, void *response, u32 response_size);
+int rpmsg_kdrv_send_message(struct rpmsg_device *rpdev, u32 device_id, void *message,
+			    u32 message_size);
+
+#endif
