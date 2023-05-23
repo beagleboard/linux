246f59dad307fdb2347daec51387c228d88cb4a9
Robert Nelson <robertcnelson@gmail.com>
2023-05-23 15:28:45 -0500

    merge: aufs-mmap
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -665,7 +665,7 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 		if (file) {
 			struct address_space *mapping = file->f_mapping;
 
-			get_file(file);
+			vma_get_file(tmp);
 			i_mmap_lock_write(mapping);
 			if (tmp->vm_flags & VM_SHARED)
 				mapping_allow_writable(mapping);
