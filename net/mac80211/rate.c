fe46c067afbd7abdbe3180e385fa14c211c291cb
Robert Nelson <robertcnelson@gmail.com>
2023-05-23 13:42:45 -0500

    backports: brcm80211: from: linux.git
    Reference: v5.10.9
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- net/mac80211/rate.c
+++ net/mac80211/rate.c
@@ -960,8 +960,7 @@ int rate_control_set_rates(struct ieee80211_hw *hw,
 	if (old)
 		kfree_rcu(old, rcu_head);
 
-	if (sta->uploaded)
-		drv_sta_rate_tbl_update(hw_to_local(hw), sta->sdata, pubsta);
+	drv_sta_rate_tbl_update(hw_to_local(hw), sta->sdata, pubsta);
 
 	ieee80211_sta_set_expected_throughput(pubsta, sta_get_expected_throughput(sta));
 
