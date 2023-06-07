4322af8c3aec668a617a122b092713bfe30b39ba
Ravi Gunasekaran <r-gunasekaran@ti.com>
2023-06-07 13:59:45 +0530

    net: hsr: Disable promiscuous mode in offload mode
    When port-to-port forwarding for interfaces in HSR node is enabled,
    disable promiscuous mode since L2 frame forward happens at the
    offloaded hardware.
    
    Signed-off-by: Ravi Gunasekaran <r-gunasekaran@ti.com>

--- net/hsr/hsr_main.h
+++ net/hsr/hsr_main.h
@@ -189,6 +189,7 @@ struct hsr_priv {
 	struct list_head	self_node_db;	/* MACs of slaves */
 	struct timer_list	announce_timer;	/* Supervision frame dispatch */
 	struct timer_list	prune_timer;
+	unsigned int		fwd_offloaded : 1; /* Forwarding offloaded to HW */
 	int announce_count;
 	u16 sequence_nr;
 	u16 sup_sequence_nr;	/* For HSRv1 separate seq_nr for supervision */
