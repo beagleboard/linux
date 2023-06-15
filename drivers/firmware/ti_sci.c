757a164979d796753b0a5b039a74209f2c3fdd1a
Dhruva Gole <d-gole@ti.com>
2023-06-15 18:16:39 +0530

    firmware: ti_sci: remove FS Stub loading implementation
    Now that the TIFS Stub is loaded from the bootloader stage, remove the
    code from this driver that tries to load the fs stub from the linux
    filesystem.
    Also remove the pm_notifier entirely since there's no custom firmware
    loading to be done anymore
    
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

--- drivers/firmware/ti_sci.c
+++ drivers/firmware/ti_sci.c
@@ -93,13 +93,10 @@ struct ti_sci_desc {
  * @dev:	Device pointer
  * @desc:	SoC description for this instance
  * @nb:	Reboot Notifier block
- * @pm_nb:	PM notifier block
  * @d:		Debugfs file entry
  * @debug_region: Memory region where the debug message are available
  * @debug_region_size: Debug region size
  * @debug_buffer: Buffer allocated to copy debug messages.
- * @lpm_region: Memory region where the FS Stub LPM Firmware will be stored
- * @lpm_region_size: LPM region size
  * @handle:	Instance of TI SCI handle to send to clients.
  * @cl:		Mailbox Client
  * @chan_tx:	Transmit mailbox channel
@@ -112,20 +109,15 @@ struct ti_sci_desc {
  * @fw_caps:	FW/SoC low power capabilities
  * @users:	Number of users of this instance
  * @is_suspending: Flag set to indicate in suspend path.
- * @lpm_firmware_loaded: Flag to indicate if LPM firmware has been loaded
- * @lpm_firmware_name: Name of firmware binary to load from fw search path
  */
 struct ti_sci_info {
 	struct device *dev;
 	struct notifier_block nb;
-	struct notifier_block pm_nb;
 	const struct ti_sci_desc *desc;
 	struct dentry *d;
 	void __iomem *debug_region;
 	char *debug_buffer;
 	size_t debug_region_size;
-	void __iomem *lpm_region;
-	size_t lpm_region_size;
 	struct ti_sci_handle handle;
 	struct mbox_client cl;
 	struct mbox_chan *chan_tx;
@@ -139,14 +131,11 @@ struct ti_sci_info {
 	/* protected by ti_sci_list_mutex */
 	int users;
 	bool is_suspending;
-	bool lpm_firmware_loaded;
-	const char *lpm_firmware_name;
 };
 
 #define cl_to_ti_sci_info(c)	container_of(c, struct ti_sci_info, cl)
 #define handle_to_ti_sci_info(h) container_of(h, struct ti_sci_info, handle)
 #define reboot_to_ti_sci_info(n) container_of(n, struct ti_sci_info, nb)
-#define pm_nb_to_ti_sci_info(n) container_of(n, struct ti_sci_info, pm_nb)
 
 #ifdef CONFIG_DEBUG_FS
 
@@ -3538,32 +3527,6 @@ static int tisci_reboot_handler(struct notifier_block *nb, unsigned long mode,
 	return NOTIFY_BAD;
 }
 
-static int ti_sci_load_lpm_firmware(struct device *dev, struct ti_sci_info *info)
-{
-	const struct firmware *firmware;
-	int ret = 0;
-
-	/* If no firmware name is set, do not attempt to load. */
-	if (!info->lpm_firmware_name)
-		return -EINVAL;
-
-	ret = request_firmware_direct(&firmware, info->lpm_firmware_name, dev);
-	if (ret) {
-		dev_warn(dev, "Cannot load %s\n", info->lpm_firmware_name);
-		return ret;
-	}
-
-	if (firmware->size > info->lpm_region_size) {
-		release_firmware(firmware);
-		return -ENOMEM;
-	}
-
-	memcpy_toio(info->lpm_region, firmware->data, firmware->size);
-
-	release_firmware(firmware);
-
-	return ret;
-}
 static void ti_sci_set_is_suspending(struct ti_sci_info *info, bool is_suspending)
 {
 	info->is_suspending = is_suspending;
@@ -3588,9 +3551,6 @@ static int ti_sci_prepare_system_suspend(struct ti_sci_info *info)
 		/* S2MEM is not supported by the firmware. */
 		if (!(info->fw_caps & MSG_FLAG_CAPS_LPM_DEEP_SLEEP))
 			return 0;
-		/* S2MEM can't continue if the LPM firmware is not loaded. */
-		if (!info->lpm_firmware_loaded)
-			return -EINVAL;
 
 		mode = mcu_state ? TISCI_MSG_VALUE_SLEEP_MODE_MCU_ONLY
 			: TISCI_MSG_VALUE_SLEEP_MODE_DEEP_SLEEP;
@@ -3655,33 +3615,10 @@ static const struct dev_pm_ops ti_sci_pm_ops = {
 	.resume_noirq = ti_sci_resume,
 };
 
-static int tisci_pm_handler(struct notifier_block *nb, unsigned long pm_event,
-			    void *unused)
-{
-	struct ti_sci_info *info = pm_nb_to_ti_sci_info(nb);
-	int ret;
-
-	/* Load the LPM firmware on PM_SUSPEND_PREPARE if not loaded yet */
-	if (pm_event != PM_SUSPEND_PREPARE || info->lpm_firmware_loaded)
-		return NOTIFY_DONE;
-
-	ret = ti_sci_load_lpm_firmware(info->dev, info);
-	if (ret) {
-		dev_err(info->dev, "Failed to load LPM firmware (%d)\n", ret);
-		return NOTIFY_BAD;
-	}
-
-	info->lpm_firmware_loaded = true;
-
-	return NOTIFY_OK;
-}
-
 static int ti_sci_init_suspend(struct platform_device *pdev,
 			       struct ti_sci_info *info)
 {
 	struct device *dev = &pdev->dev;
-	struct resource *res;
-	int ret;
 
 	dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64));
 	info->ctx_mem_buf = dma_alloc_attrs(info->dev, LPM_CTX_MEM_SIZE,
@@ -3694,44 +3631,7 @@ static int ti_sci_init_suspend(struct platform_device *pdev,
 		return -ENOMEM;
 	}
 
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "lpm");
-	if (!res) {
-		dev_warn(dev,
-			 "lpm region is required for suspend but not provided.\n");
-		ret = -EINVAL;
-		goto err;
-	}
-
-	info->lpm_region = devm_ioremap_resource(dev, res);
-	if (IS_ERR(info->lpm_region)) {
-		ret = PTR_ERR(info->lpm_region);
-		goto err;
-	}
-	info->lpm_region_size = resource_size(res);
-
-	if (of_property_read_string(dev->of_node, "firmware-name",
-				    &info->lpm_firmware_name)) {
-		dev_warn(dev,
-			 "firmware-name is required for suspend but not provided.\n");
-		ret = -EINVAL;
-		goto err;
-	}
-
-	info->pm_nb.notifier_call = tisci_pm_handler;
-	info->pm_nb.priority = 128;
-
-	ret = register_pm_notifier(&info->pm_nb);
-	if (ret) {
-		dev_err(dev, "pm_notifier registration fail (%d)\n", ret);
-		goto err;
-	}
-
 	return 0;
-err:
-	dma_free_coherent(info->dev, LPM_CTX_MEM_SIZE,
-			  info->ctx_mem_buf,
-			  info->ctx_mem_addr);
-	return ret;
 }
 
 /* Description for K2G */
@@ -3923,9 +3823,6 @@ static int ti_sci_remove(struct platform_device *pdev)
 	of_platform_depopulate(dev);
 
 	info = platform_get_drvdata(pdev);
-
-	if (info->pm_nb.notifier_call)
-		unregister_pm_notifier(&info->pm_nb);
 
 	if (info->nb.notifier_call)
 		unregister_restart_handler(&info->nb);
