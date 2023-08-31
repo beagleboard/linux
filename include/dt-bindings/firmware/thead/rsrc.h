887b0f1480f81c8d38c78331ca9b12ef8f79c2f0
Robert Nelson <robertcnelson@gmail.com>
2023-08-31 15:31:11 -0500

    Add BeagleBoard.org Device Tree Changes
    https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/tree/v5.10.x-ti-unified
    
    https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/78e0d983987866149da4b77d926ece5139feb314
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ include/dt-bindings/firmware/thead/rsrc.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2022 Alibaba Group Holding Limited.
+ */
+
+#ifndef __DT_BINDINGS_RSCRC_LIGHT_H
+#define __DT_BINDINGS_RSCRC_LIGHT_H
+
+#define LIGHT_AON_AUDIO_PD	0
+#define LIGHT_AON_VDEC_PD	1
+#define LIGHT_AON_NPU_PD        2
+#define LIGHT_AON_VENC_PD       3
+#define LIGHT_AON_GPU_PD        4
+#define LIGHT_AON_DSP0_PD       5
+#define LIGHT_AON_DSP1_PD       6
+
+#endif
