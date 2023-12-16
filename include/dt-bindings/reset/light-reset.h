6bfa7731b86ea69740d3844f7afe872864dbed69
Robert Nelson <robertcnelson@gmail.com>
2023-12-16 16:53:17 -0600

    Add BeagleBoard.org Device Tree Changes
    https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/tree/v5.10.x-ti-unified
    
    https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/445ba7c06291522020a102e90c4a6864a461a38a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ include/dt-bindings/reset/light-reset.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2021 Alibaba, Inc.
+ *
+ * Author: zenglinghui  <zenglinghui.zlh@linux.alibaba.com>
+ */
+
+#ifndef DT_BINDING_RESET_LIGHT_H
+#define DT_BINDING_RESET_LIGHT_H
+
+#define LIGHT_RESET_WDT0    0
+#define LIGHT_RESET_WDT1    1
+#endif
