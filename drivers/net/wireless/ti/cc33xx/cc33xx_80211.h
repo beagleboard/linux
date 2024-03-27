b6a28655e03f9a3259ee8e7c8dd284e125477347
Robert Nelson <robertcnelson@gmail.com>
2024-03-27 15:08:21 -0500

    am62x-cc33xx-linux-package-1.0.0.1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ drivers/net/wireless/ti/cc33xx/cc33xx_80211.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __CC33XX_80211_H__
+#define __CC33XX_80211_H__
+
+#include <linux/if_ether.h>	/* ETH_ALEN */
+#include <linux/if_arp.h>
+
+
+/* This really should be 8, but not for our firmware */
+#define MAX_SUPPORTED_RATES 32
+#define MAX_COUNTRY_TRIPLETS 32
+
+/* Headers */
+struct ieee80211_header {
+	__le16 frame_ctl;
+	__le16 duration_id;
+	u8 da[ETH_ALEN];
+	u8 sa[ETH_ALEN];
+	u8 bssid[ETH_ALEN];
+	__le16 seq_ctl;
+	u8 payload[0];
+} __packed;
+
+
+/* Templates */
+
+struct cc33xx_null_data_template {
+	struct ieee80211_header header;
+} __packed;
+
+struct cc33xx_arp_rsp_template {
+	/* not including ieee80211 header */
+
+	u8 llc_hdr[sizeof(rfc1042_header)];
+	__be16 llc_type;
+
+	struct arphdr arp_hdr;
+	u8 sender_hw[ETH_ALEN];
+	__be32 sender_ip;
+	u8 target_hw[ETH_ALEN];
+	__be32 target_ip;
+} __packed;
+
+struct cc33xx_disconn_template {
+	struct ieee80211_header header;
+	__le16 disconn_reason;
+} __packed;
+
+#endif
