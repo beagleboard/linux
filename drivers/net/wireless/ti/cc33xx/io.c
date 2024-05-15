82f12663243ffc636f241fe9543e45621c78c4b4
Robert Nelson <robertcnelson@gmail.com>
2024-05-15 15:41:39 -0500

    cc33xx-linux-package-1.0.0.3
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/net/wireless/ti/cc33xx/io.c
+++ drivers/net/wireless/ti/cc33xx/io.c
@@ -7,16 +7,11 @@
  * Contact: Luciano Coelho <luciano.coelho@nokia.com>
  */
 
-#include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/spi/spi.h>
-#include <linux/interrupt.h>
-
 #include "wlcore.h"
 #include "debug.h"
-#include "cc33xx_80211.h"
 #include "io.h"
 #include "tx.h"
+
 
 bool cc33xx_set_block_size(struct cc33xx *wl)
 {
@@ -26,8 +21,9 @@ bool cc33xx_set_block_size(struct cc33xx *wl)
 			"Set BLKsize to %d", CC33XX_BUS_BLOCK_SIZE);
 		return true;
 	}
-	else
+	else {
 		cc33xx_debug(DEBUG_CC33xx, "Could not set BLKsize");
+	}
 	return false;
 }
 
@@ -56,4 +52,84 @@ void cc33xx_io_init(struct cc33xx *wl)
 {
 	if (wl->if_ops->init)
 		wl->if_ops->init(wl->dev);
+}
+
+/* Raw target IO, address is not translated */
+static int __must_check wlcore_raw_write(struct cc33xx *wl, int addr,
+					 void *buf, size_t len, bool fixed)
+{
+	int ret;
+
+	if (test_bit(CC33XX_FLAG_IO_FAILED, &wl->flags) ||
+	    WARN_ON((test_bit(CC33XX_FLAG_IN_ELP, &wl->flags) &&
+		     addr != HW_ACCESS_ELP_CTRL_REG)))
+		return -EIO;
+
+	ret = wl->if_ops->write(wl->dev, addr, buf, len, fixed);
+	if (ret && wl->state != WLCORE_STATE_OFF)
+		set_bit(CC33XX_FLAG_IO_FAILED, &wl->flags);
+
+	return ret;
+}
+
+int __must_check wlcore_raw_read(struct cc33xx *wl, int addr,
+				 void *buf, size_t len, bool fixed)
+{
+	int ret;
+
+	if (test_bit(CC33XX_FLAG_IO_FAILED, &wl->flags) ||
+	    WARN_ON((test_bit(CC33XX_FLAG_IN_ELP, &wl->flags) &&
+		     addr != HW_ACCESS_ELP_CTRL_REG)))
+		return -EIO;
+
+	ret = wl->if_ops->read(wl->dev, addr, buf, len, fixed);
+	if (ret && wl->state != WLCORE_STATE_OFF)
+		set_bit(CC33XX_FLAG_IO_FAILED, &wl->flags);
+
+	return ret;
+}
+
+int __must_check wlcore_write(struct cc33xx *wl, int addr,
+			      void *buf, size_t len, bool fixed)
+{
+	return wlcore_raw_write(wl, addr, buf, len, fixed);
+}
+
+void claim_core_status_lock(struct cc33xx *wl)
+{
+	/* When accessing core-status data (read or write) the transport lock
+	 * should be held. */
+	wl->if_ops->interface_claim(wl->dev);
+}
+
+void release_core_status_lock(struct cc33xx *wl)
+{
+	/* After accessing core-status data (read or write) the transport lock
+	 * should be released. */
+	wl->if_ops->interface_release(wl->dev);
+}
+
+void cc33xx_power_off(struct cc33xx *wl)
+{
+	int ret = 0;
+
+	if (!test_bit(CC33XX_FLAG_GPIO_POWER, &wl->flags))
+		return;
+
+	if (wl->if_ops->power)
+		ret = wl->if_ops->power(wl->dev, false);
+	if (!ret)
+		clear_bit(CC33XX_FLAG_GPIO_POWER, &wl->flags);
+}
+
+int cc33xx_power_on(struct cc33xx *wl)
+{
+	int ret = 0;
+
+	if (wl->if_ops->power)
+		ret = wl->if_ops->power(wl->dev, true);
+	if (ret == 0)
+		set_bit(CC33XX_FLAG_GPIO_POWER, &wl->flags);
+
+	return ret;
 }
