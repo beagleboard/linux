2f679051a642adf527d45a9f6d1291a8047d9f44
Siddharth Vadapalli <s-vadapalli@ti.com>
2024-05-24 11:12:00 +0530

    net: phy: mscc: Add auto-negotiation feature to VSC8514
    Add function vsc85xx_config_inband_aneg() in mscc_main.c to enable
    auto-negotiation. The function enables auto-negotiation by configuring
    the MAC SerDes PCS Control register of VSC8514.
    
    Invoke the vsc85xx_config_inband_aneg() function from the
    vsc8514_config_init() function present in mscc_main.c to start the
    auto-negotiation process. This is required to get Ethernet working with
    the Quad port Ethernet Add-On card connected to J7 common processor board.
    
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>
    Signed-off-by: Chintan Vankar <c-vankar@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- drivers/net/phy/mscc/mscc_main.c
+++ drivers/net/phy/mscc/mscc_main.c
@@ -1700,6 +1700,21 @@ static int vsc8574_config_host_serdes(struct phy_device *phydev)
 			   PROC_CMD_RST_CONF_PORT | PROC_CMD_FIBER_1000BASE_X);
 }
 
+static int vsc85xx_config_inband_aneg(struct phy_device *phydev, bool enabled)
+{
+	u16 reg_val = 0;
+	int rc;
+
+	if (enabled)
+		reg_val = MSCC_PHY_SERDES_ANEG;
+
+	rc = phy_modify_paged(phydev, MSCC_PHY_PAGE_EXTENDED_3,
+			      MSCC_PHY_SERDES_PCS_CTRL, MSCC_PHY_SERDES_ANEG,
+			      reg_val);
+
+	return rc;
+}
+
 static int vsc8584_config_init(struct phy_device *phydev)
 {
 	struct vsc8531_private *vsc8531 = phydev->priv;
@@ -2107,6 +2122,10 @@ static int vsc8514_config_init(struct phy_device *phydev)
 		return ret;
 
 	ret = genphy_soft_reset(phydev);
+	if (ret)
+		return ret;
+
+	ret = vsc85xx_config_inband_aneg(phydev, true);
 
 	if (ret)
 		return ret;
