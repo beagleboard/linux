8b2692d519dc12a1e88fab26e14861c4e939c7da
Jayesh Choudhary <j-choudhary@ti.com>
2024-05-28 13:47:38 +0530

    drm/bridge: sii902x: Add pixel clock check in atomic_check
    Check the pixel clock for the mode in atomic_check and ensure that
    it is within the range supported by the bridge.
    
    Signed-off-by: Jayesh Choudhary <j-choudhary@ti.com>

--- drivers/gpu/drm/bridge/sii902x.c
+++ drivers/gpu/drm/bridge/sii902x.c
@@ -523,6 +523,12 @@ static int sii902x_bridge_atomic_check(struct drm_bridge *bridge,
 				       struct drm_crtc_state *crtc_state,
 				       struct drm_connector_state *conn_state)
 {
+	if (crtc_state->mode.clock < SII902X_MIN_PIXEL_CLOCK_KHZ)
+		return MODE_CLOCK_LOW;
+
+	if (crtc_state->mode.clock > SII902X_MAX_PIXEL_CLOCK_KHZ)
+		return MODE_CLOCK_HIGH;
+
 	/*
 	 * There might be flags negotiation supported in future but
 	 * set the bus flags in atomic_check statically for now.
