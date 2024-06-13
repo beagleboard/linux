87be797045f4abc1a4b2c2bed47a4873e947d6ec
Devarsh Thakkar <devarsht@ti.com>
2024-06-13 11:50:10 +0530

    math.h: Add macros for rounding to closest value
    Add below rounding related macros:
    
    round_closest_up(x, y) : Rounds x to closest multiple of y where y is a
    power of 2, with a preference to round up in case two nearest values are
    possible.
    
    round_closest_down(x, y) : Rounds x to closest multiple of y where y is a
    power of 2, with a preference to round down in case two nearest values are
    possible.
    
    roundclosest(x, y) : Rounds x to closest multiple of y, this macro should
    generally be used only when y is not multiple of 2 as otherwise
    round_closest* macros should be used which are much faster.
    
    Examples:
     * round_closest_up(17, 4) = 16
     * round_closest_up(15, 4) = 16
     * round_closest_up(14, 4) = 16
     * round_closest_down(17, 4) = 16
     * round_closest_down(15, 4) = 16
     * round_closest_down(14, 4) = 12
     * roundclosest(21, 5) = 20
     * roundclosest(19, 5) = 20
     * roundclosest(17, 5) = 15
    
    Signed-off-by: Devarsh Thakkar <devarsht@ti.com>
    Acked-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/linux/math.h
+++ include/linux/math.h
@@ -34,6 +34,52 @@
  */
 #define round_down(x, y) ((x) & ~__round_mask(x, y))
 
+/**
+ * round_closest_up - round closest to be multiple of specified value (which is
+ *                    power of 2) with preference to rounding up
+ * @x: the value to round
+ * @y: multiple to round closest to (must be a power of 2)
+ *
+ * Rounds @x to closest multiple of @y (which must be a power of 2).
+ * The value can be either rounded up or rounded down depending upon rounded
+ * value's closeness to the specified value. If there are two closest possible
+ * values, i.e. the difference between the specified value and it's rounded up
+ * and rounded down values is same then preference is given to rounded up
+ * value.
+ *
+ * To perform arbitrary rounding to closest value (not multiple of 2), use
+ * roundclosest().
+ *
+ * Examples:
+ * * round_closest_up(17, 4) = 16
+ * * round_closest_up(15, 4) = 16
+ * * round_closest_up(14, 4) = 16
+ */
+#define round_closest_up(x, y) round_down((x) + (y) / 2, (y))
+
+/**
+ * round_closest_down - round closest to be multiple of specified value (which
+ *			is power of 2) with preference to rounding down
+ * @x: the value to round
+ * @y: multiple to round closest to (must be a power of 2)
+ *
+ * Rounds @x to closest multiple of @y (which must be a power of 2).
+ * The value can be either rounded up or rounded down depending upon rounded
+ * value's closeness to the specified value. If there are two closest possible
+ * values, i.e. the difference between the specified value and it's rounded up
+ * and rounded down values is same then preference is given to rounded up
+ * value.
+ *
+ * To perform arbitrary rounding to closest value (not multiple of 2), use
+ * roundclosest().
+ *
+ * Examples:
+ * * round_closest_down(17, 4) = 16
+ * * round_closest_down(15, 4) = 16
+ * * round_closest_down(14, 4) = 12
+ */
+#define round_closest_down(x, y) round_up((x) - (y) / 2, (y))
+
 #define DIV_ROUND_UP __KERNEL_DIV_ROUND_UP
 
 #define DIV_ROUND_DOWN_ULL(ll, d) \
@@ -76,6 +122,23 @@
 	__x - (__x % (y));				\
 }							\
 )
+
+/**
+ * roundclosest - round to nearest multiple
+ * @x: the value to round
+ * @y: multiple to round nearest to
+ *
+ * Rounds @x to nearest multiple of @y.
+ * The rounded value can be greater than or less than @x depending
+ * upon it's nearness to @x. If @y will always be a power of 2, consider
+ * using the faster round_closest_up() or round_closest_down().
+ *
+ * Examples:
+ * * roundclosest(21, 5) = 20
+ * * roundclosest(19, 5) = 20
+ * * roundclosest(17, 5) = 15
+ */
+#define roundclosest(x, y) rounddown((x) + (y) / 2, (y))
 
 /*
  * Divide positive or negative dividend by positive or negative divisor
