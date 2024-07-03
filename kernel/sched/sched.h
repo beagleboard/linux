9644bdc2e45884a34ad17fec810a0dbc55d7b912
Robert Nelson <robertcnelson@gmail.com>
2024-07-02 19:16:02 -0500

    merge: CONFIG_PREEMPT_RT Patch Set
    patch-6.6.32-rt32.patch.xz
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2437,6 +2437,7 @@ extern void init_sched_fair_class(void);
 extern void reweight_task(struct task_struct *p, int prio);
 
 extern void resched_curr(struct rq *rq);
+extern void resched_curr_lazy(struct rq *rq);
 extern void resched_cpu(int cpu);
 
 extern struct rt_bandwidth def_rt_bandwidth;
