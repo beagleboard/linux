7a78c89eb58bad042c55b7b0234300ab9f561919
Santhosh Kumar K <s-k6@ti.com>
2024-07-17 11:56:01 +0530

    spi: cadence-quadspi: Enable SPI_TX_QUAD
    Enable SPI_TX_QUAD in spi_controller->mode_bits to add support for
    the 1S-4S-4S operations.
    
    Signed-off-by: Santhosh Kumar K <s-k6@ti.com>
    Tested-by: Prasanth Babu Mantena <p-mantena@ti.com>

--- drivers/spi/spi-cadence-quadspi.c
+++ drivers/spi/spi-cadence-quadspi.c
@@ -2801,7 +2801,7 @@ static int cqspi_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "devm_spi_alloc_host failed\n");
 		return -ENOMEM;
 	}
-	host->mode_bits = SPI_RX_QUAD | SPI_RX_DUAL;
+	host->mode_bits = SPI_RX_QUAD | SPI_TX_QUAD | SPI_RX_DUAL;
 	host->mem_ops = &cqspi_mem_ops;
 	host->mem_caps = &cqspi_mem_caps;
 	host->dev.of_node = pdev->dev.of_node;
