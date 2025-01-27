9760bb4a404b66556200450f0e7db4f1780ce086
Robert Nelson <robertcnelson@gmail.com>
2025-01-27 12:57:34 -0600

    backports: scripts: from: linux.git
    Reference: v6.1.127
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -829,7 +829,7 @@ static void check_section(const char *modname, struct elf_info *elf,
 		".ltext", ".ltext.*"
 #define OTHER_TEXT_SECTIONS ".ref.text", ".head.text", ".spinlock.text", \
 		".fixup", ".entry.text", ".exception.text", \
-		".coldtext", ".softirqentry.text"
+		".coldtext", ".softirqentry.text", ".irqentry.text"
 
 #define INIT_SECTIONS      ".init.*"
 #define MEM_INIT_SECTIONS  ".meminit.*"
@@ -889,7 +889,7 @@ enum mismatch {
 struct sectioncheck {
 	const char *fromsec[20];
 	const char *bad_tosec[20];
-	const char *good_tosec[20];
+	const char *good_tosec[21];
 	enum mismatch mismatch;
 	void (*handler)(const char *modname, struct elf_info *elf,
 			const struct sectioncheck* const mismatch,
