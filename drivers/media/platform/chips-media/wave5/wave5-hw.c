c114fb382096221601c6698df341f8468ed4b0f3
Jackson.lee <jackson.lee@chipsnmedia.com>
2025-02-03 10:56:37 -0600

    PENDING: media: chips-media: wave5: Support 48bit memory addressing.
    The host should ensure the upper address of the memory allocated by the
    wave5 driver always has the same value. Upper 16 bits are static after
    device probe.
    
    Change dma_mask value so that wave5 driver can allow 48bit memory address.
    Change the u32 type address variable to dma_addr_t to allow 48bit address.
    Get high_address value from common_memory dma address.
    
    Signed-off-by: Jackson Lee <jackson.lee@chipsnmedia.com>
    Signed-off-by: Nas Chung <nas.chung@chipsnmedia.com>
    Signed-off-by: Brandon Brnich <b-brnich@ti.com>

--- drivers/media/platform/chips-media/wave5/wave5-hw.c
+++ drivers/media/platform/chips-media/wave5/wave5-hw.c
@@ -185,6 +185,19 @@ bool wave5_vpu_is_init(struct vpu_device *vpu_dev)
 	return vpu_read_reg(vpu_dev, W5_VCPU_CUR_PC) != 0;
 }
 
+static dma_addr_t wave5_read_reg_for_mem_addr(struct vpu_instance *inst,
+					      unsigned int reg_addr)
+{
+	dma_addr_t addr;
+	dma_addr_t high_addr = inst->dev->ext_addr;
+	u32 val;
+
+	val = vpu_read_reg(inst->dev, reg_addr);
+	addr = ((high_addr << 32) | val);
+
+	return addr;
+}
+
 unsigned int wave5_vpu_get_product_id(struct vpu_device *vpu_dev)
 {
 	u32 val = vpu_read_reg(vpu_dev, W5_PRODUCT_NUMBER);
@@ -466,7 +479,8 @@ int wave5_vpu_init(struct device *dev, u8 *fw, size_t size)
 	vpu_write_reg(vpu_dev, W5_HW_OPTION, 0);
 
 	if (vpu_dev->product_code != WAVE515_CODE) {
-		wave5_fio_writel(vpu_dev, W5_BACKBONE_PROC_EXT_ADDR, 0);
+		reg_val = (vpu_dev->ext_addr & 0xFFFF);
+		wave5_fio_writel(vpu_dev, W5_BACKBONE_PROC_EXT_ADDR, reg_val);
 		wave5_fio_writel(vpu_dev, W5_BACKBONE_AXI_PARAM, 0);
 		vpu_write_reg(vpu_dev, W5_SEC_AXI_PARAM, 0);
 	}
@@ -572,7 +586,7 @@ int wave5_vpu_build_up_dec_param(struct vpu_instance *inst,
 
 	if (inst->dev->product_code != WAVE515_CODE) {
 		/* This register must be reset explicitly */
-		vpu_write_reg(inst->dev, W5_CMD_EXT_ADDR, 0);
+		vpu_write_reg(inst->dev, W5_CMD_EXT_ADDR, inst->dev->ext_addr);
 		vpu_write_reg(inst->dev, W5_CMD_NUM_CQ_DEPTH_M1,
 			      WAVE521_COMMAND_QUEUE_DEPTH - 1);
 	}
@@ -1160,7 +1174,8 @@ int wave5_vpu_re_init(struct device *dev, u8 *fw, size_t size)
 		vpu_write_reg(vpu_dev, W5_HW_OPTION, 0);
 
 		if (vpu_dev->product_code != WAVE515_CODE) {
-			wave5_fio_writel(vpu_dev, W5_BACKBONE_PROC_EXT_ADDR, 0);
+			reg_val = (vpu_dev->ext_addr & 0xFFFF);
+			wave5_fio_writel(vpu_dev, W5_BACKBONE_PROC_EXT_ADDR, reg_val);
 			wave5_fio_writel(vpu_dev, W5_BACKBONE_AXI_PARAM, 0);
 			vpu_write_reg(vpu_dev, W5_SEC_AXI_PARAM, 0);
 		}
@@ -1285,7 +1300,8 @@ static int wave5_vpu_sleep_wake(struct device *dev, bool i_sleep_wake, const uin
 		vpu_write_reg(vpu_dev, W5_HW_OPTION, 0);
 
 		if (vpu_dev->product_code != WAVE515_CODE) {
-			wave5_fio_writel(vpu_dev, W5_BACKBONE_PROC_EXT_ADDR, 0);
+			reg_val = (vpu_dev->ext_addr & 0xFFFF);
+			wave5_fio_writel(vpu_dev, W5_BACKBONE_PROC_EXT_ADDR, reg_val);
 			wave5_fio_writel(vpu_dev, W5_BACKBONE_AXI_PARAM, 0);
 			vpu_write_reg(vpu_dev, W5_SEC_AXI_PARAM, 0);
 		}
@@ -1520,7 +1536,7 @@ dma_addr_t wave5_dec_get_rd_ptr(struct vpu_instance *inst)
 	if (ret)
 		return inst->codec_info->dec_info.stream_rd_ptr;
 
-	return vpu_read_reg(inst->dev, W5_RET_QUERY_DEC_BS_RD_PTR);
+	return wave5_read_reg_for_mem_addr(inst, W5_RET_QUERY_DEC_BS_RD_PTR);
 }
 
 int wave5_dec_set_rd_ptr(struct vpu_instance *inst, dma_addr_t addr)
@@ -1571,7 +1587,7 @@ int wave5_vpu_build_up_enc_param(struct device *dev, struct vpu_instance *inst,
 
 	reg_val = (open_param->line_buf_int_en << 6) | BITSTREAM_ENDIANNESS_BIG_ENDIAN;
 	vpu_write_reg(inst->dev, W5_CMD_BS_PARAM, reg_val);
-	vpu_write_reg(inst->dev, W5_CMD_EXT_ADDR, 0);
+	vpu_write_reg(inst->dev, W5_CMD_EXT_ADDR, inst->dev->ext_addr);
 	vpu_write_reg(inst->dev, W5_CMD_NUM_CQ_DEPTH_M1, WAVE521_COMMAND_QUEUE_DEPTH - 1);
 
 	/* This register must be reset explicitly */
@@ -2354,10 +2370,10 @@ int wave5_vpu_enc_get_result(struct vpu_instance *inst, struct enc_output_info *
 	result->recon_frame_index = vpu_read_reg(inst->dev, W5_RET_ENC_PIC_IDX);
 	result->enc_pic_byte = vpu_read_reg(inst->dev, W5_RET_ENC_PIC_BYTE);
 	result->enc_src_idx = vpu_read_reg(inst->dev, W5_RET_ENC_USED_SRC_IDX);
-	p_enc_info->stream_wr_ptr = vpu_read_reg(inst->dev, W5_RET_ENC_WR_PTR);
-	p_enc_info->stream_rd_ptr = vpu_read_reg(inst->dev, W5_RET_ENC_RD_PTR);
+	p_enc_info->stream_wr_ptr = wave5_read_reg_for_mem_addr(inst, W5_RET_ENC_WR_PTR);
+	p_enc_info->stream_rd_ptr = wave5_read_reg_for_mem_addr(inst, W5_RET_ENC_RD_PTR);
 
-	result->bitstream_buffer = vpu_read_reg(inst->dev, W5_RET_ENC_RD_PTR);
+	result->bitstream_buffer = wave5_read_reg_for_mem_addr(inst, W5_RET_ENC_RD_PTR);
 	result->rd_ptr = p_enc_info->stream_rd_ptr;
 	result->wr_ptr = p_enc_info->stream_wr_ptr;
 
