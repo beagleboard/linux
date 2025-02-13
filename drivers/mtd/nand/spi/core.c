4a1cc606332010d8b648237235199403936aa4e3
Santhosh Kumar K <s-k6@ti.com>
2025-02-13 15:15:23 +0530

    PENDING: mtd: spinand: Call do_calibration when SPI NAND probes
    Add the PHY calibration call during SPI NAND probe, so that, the PHY
    is tuned with the optimal tuning point which enables controller to
    run at higher speeds.
    
    Load the known pattern to page buffer, and pass a page read op with
    the do_calibration call.
    
    Signed-off-by: Santhosh Kumar K <s-k6@ti.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -9,6 +9,8 @@
 
 #define pr_fmt(fmt)	"spi-nand: " fmt
 
+#define PHY_PATTERN_SIZE	0x80
+
 #include <linux/device.h>
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
@@ -1513,7 +1515,13 @@ static int spinand_probe(struct spi_mem *mem)
 {
 	struct spinand_device *spinand;
 	struct mtd_info *mtd;
-	int ret;
+	struct mtd_part *part;
+	struct nand_device *nand;
+	struct nand_pos page_pos;
+	struct nand_page_io_req page_req;
+	struct spi_mem_op read_page_op;
+	int ret, pageoffs;
+	u8 status;
 
 	spinand = devm_kzalloc(&mem->spi->dev, sizeof(*spinand),
 			       GFP_KERNEL);
@@ -1531,9 +1539,38 @@ static int spinand_probe(struct spi_mem *mem)
 	if (ret)
 		return ret;
 
+	nand = spinand_to_nand(spinand);
+
 	ret = mtd_device_register(mtd, NULL, 0);
 	if (ret)
 		goto err_spinand_cleanup;
+
+	list_for_each_entry(part, &mtd->partitions, node) {
+		struct mtd_info *part_info =
+			container_of(part, struct mtd_info, part);
+		if (part_info->name &&
+		    !strcmp(part_info->name, "ospi_nand.phypattern")) {
+			pageoffs = nanddev_offs_to_pos(nand, part->offset, &page_pos);
+			page_req.pos = page_pos;
+
+			read_page_op = *spinand->op_templates.read_cache;
+			read_page_op.addr.val = pageoffs;
+			read_page_op.data.nbytes = PHY_PATTERN_SIZE;
+
+			ret = spinand_load_page_op(spinand, &page_req);
+			if (ret)
+				goto err_spinand_cleanup;
+
+			ret = spinand_wait(spinand,
+					   SPINAND_READ_INITIAL_DELAY_US,
+					   SPINAND_READ_POLL_DELAY_US, &status);
+			if (ret < 0)
+				goto err_spinand_cleanup;
+
+			spinand_ondie_ecc_save_status(nand, status);
+			spi_mem_do_calibration(spinand->spimem, &read_page_op);
+		}
+	}
 
 	return 0;
 
