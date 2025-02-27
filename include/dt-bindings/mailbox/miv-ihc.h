0b32e04964f3d0327479fee853f4c27dab741677
Robert Nelson <robertcnelson@gmail.com>
2025-02-27 13:04:38 -0600

    Add BeagleBoard.org Device Tree Changes
    https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/tree/v6.12.x-Beagle
    
    https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/144e1f03c2b17076cd3bbf19f7ce5ac6ae81d200
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ include/dt-bindings/mailbox/miv-ihc.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause */
+/*
+ * Copyright (c) 2021 Microchip Technology Inc. All rights reserved.
+ */
+
+#ifndef __DT_BINDINGS_MIV_IHC_H
+#define __DT_BINDINGS_MIV_IHC_H
+
+#define IHC_CONTEXT_A	5
+#define IHC_CONTEXT_B	6
+
+#define IHC_HART1_INT	180
+#define IHC_HART2_INT	179
+#define IHC_HART3_INT	178
+#define IHC_HART4_INT	177
+
+#endif
