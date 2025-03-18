5c3054c04e040b633faea4fefc5d74d0e7953a57
Abhishek Sharma <abhishek.sharma@ti.com>
2025-03-18 16:41:17 +0530

    PENDING: HACK: media: v4l2-core: v4l2-ctrls-defs: Add custom IR exposure/gain
    The OX05B1S camera sensor supports both RGB and IR stream exposure/gain
    controls.
    
    The sensor has one source pad which gives out 2 streams: IR
    dominant (stream 0) and RGB dominant stream (stream 1). When programming
    the exposure/gain settings for the camera from a user space application
    like V4L2 user space API, we are unable to specify the stream for
    which the exposure/gain change is required.
    
    Hence, we have defined 3 custom control IDs for IR channel exposure,
    anologue and digital gain control. These custom controls added for IR
    channel are not an upstream acceptable solution and a better solution is
    required in the V4L2 framework for exposure/gain control in multistream
    sensors to remove this HACK.
    
    Signed-off-by: Abhishek Sharma <abhishek.sharma@ti.com>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -110,8 +110,10 @@ enum v4l2_colorfx {
 #define V4L2_CID_COLORFX_CBCR			(V4L2_CID_BASE+42)
 #define V4L2_CID_COLORFX_RGB			(V4L2_CID_BASE+43)
 
+#define V4L2_CID_IR_EXPOSURE				(V4L2_CID_BASE+44)
+
 /* last CID + 1 */
-#define V4L2_CID_LASTP1                         (V4L2_CID_BASE+44)
+#define V4L2_CID_LASTP1                         (V4L2_CID_BASE+45)
 
 /* USER-class private control IDs */
 
@@ -1215,6 +1217,7 @@ enum v4l2_jpeg_chroma_subsampling {
 #define V4L2_CID_TEST_PATTERN_GREENB		(V4L2_CID_IMAGE_SOURCE_CLASS_BASE + 7)
 #define V4L2_CID_UNIT_CELL_SIZE			(V4L2_CID_IMAGE_SOURCE_CLASS_BASE + 8)
 #define V4L2_CID_NOTIFY_GAINS			(V4L2_CID_IMAGE_SOURCE_CLASS_BASE + 9)
+#define V4L2_CID_IR_ANALOGUE_GAIN			(V4L2_CID_IMAGE_SOURCE_CLASS_BASE + 10)
 
 
 /* Image processing controls */
@@ -1227,6 +1230,7 @@ enum v4l2_jpeg_chroma_subsampling {
 #define V4L2_CID_TEST_PATTERN			(V4L2_CID_IMAGE_PROC_CLASS_BASE + 3)
 #define V4L2_CID_DEINTERLACING_MODE		(V4L2_CID_IMAGE_PROC_CLASS_BASE + 4)
 #define V4L2_CID_DIGITAL_GAIN			(V4L2_CID_IMAGE_PROC_CLASS_BASE + 5)
+#define V4L2_CID_IR_DIGITAL_GAIN		(V4L2_CID_IMAGE_PROC_CLASS_BASE + 6)
 
 /*  DV-class control IDs defined by V4L2 */
 #define V4L2_CID_DV_CLASS_BASE			(V4L2_CTRL_CLASS_DV | 0x900)
