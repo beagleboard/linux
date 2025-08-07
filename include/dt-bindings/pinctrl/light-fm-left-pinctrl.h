8384186b58bae25274266810232693e9e2b20085
Robert Nelson <robertcnelson@gmail.com>
2025-08-07 13:33:12 -0500

    Add BeagleBoard.org Device Tree Changes
    https://github.com/beagleboard/BeagleBoard-DeviceTrees/tree/v5.10.x-ti-unified
    
    https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/100884db5ca038af3a9c56c6142aa2067662218a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ include/dt-bindings/pinctrl/light-fm-left-pinctrl.h
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef _DT_BINDINGS_LIGHT_FM_LEFT_PINCTRL_H_
+#define _DT_BINDINGS_LIGHT_FM_LEFT_PINCTRL_H_
+
+#define FM_QSPI1_SCLK		0
+#define FM_QSPI1_CSN0		1
+#define FM_QSPI1_D0_MOSI	2
+#define FM_QSPI1_D1_MISO	3
+#define FM_QSPI1_D2_WP		4
+#define FM_QSPI1_D3_HOLD	5
+#define FM_I2C0_SCL		6
+#define FM_I2C0_SDA		7
+#define FM_I2C1_SCL		8
+#define FM_I2C1_SDA		9
+#define FM_UART1_TXD		10
+#define FM_UART1_RXD		11
+#define FM_UART4_TXD		12
+#define FM_UART4_RXD		13
+#define FM_UART4_CTSN		14
+#define FM_UART4_RTSN		15
+#define FM_UART3_TXD		16
+#define FM_UART3_RXD		17
+#define FM_GPIO0_18		18
+#define FM_GPIO0_19		19
+#define FM_GPIO0_20		20
+#define FM_GPIO0_21		21
+#define FM_GPIO0_22		22
+#define FM_GPIO0_23		23
+#define FM_GPIO0_24		24
+#define FM_GPIO0_25		25
+#define FM_GPIO0_26		26
+#define FM_GPIO0_27		27
+#define FM_GPIO0_28		28
+#define FM_GPIO0_29		29
+#define FM_GPIO0_30		30
+#define FM_GPIO0_31		31
+#define FM_GPIO1_0		32
+#define FM_GPIO1_1		33
+#define FM_GPIO1_2		34
+#define FM_GPIO1_3		35
+#define FM_GPIO1_4		36
+#define FM_GPIO1_5		37
+#define FM_GPIO1_6		38
+#define FM_GPIO1_7		39
+#define FM_GPIO1_8		40
+#define FM_GPIO1_9		41
+#define FM_GPIO1_10		42
+#define FM_GPIO1_11		43
+#define FM_GPIO1_12		44
+#define FM_GPIO1_13		45
+#define FM_GPIO1_14		46
+#define FM_GPIO1_15		47
+#define FM_GPIO1_16		48
+#define FM_CLK_OUT_0		49
+#define FM_CLK_OUT_1		50
+#define FM_CLK_OUT_2		51
+#define FM_CLK_OUT_3		52
+#define FM_GPIO1_21		53
+#define FM_GPIO1_22		54
+#define FM_GPIO1_23		55
+#define FM_GPIO1_24		56
+#define FM_GPIO1_25		57
+#define FM_GPIO1_26		58
+#define FM_GPIO1_27		59
+#define FM_GPIO1_28		60
+#define FM_GPIO1_29		61
+#define FM_GPIO1_30		62
+
+
+#endif /* _DT_BINDINGS_LIGHT_FM_LEFT_PINCTRL_H_ */
---
+++ include/dt-bindings/soc/thead,light-iopmp.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2021 Alibaba Group Holding Limited.
+ */
+
+
+#ifndef __DT_THEAD_LIGHT_IOPMP_H__
+#define __DT_THEAD_LIGHT_IOPMP_H__
+
+#define IOPMP_EMMC	0
+#define IOPMP_SDIO0	1
+#define IOPMP_SDIO1	2
+#define IOPMP_USB0	3
+#define IOPMP_AO	4
+#define IOPMP_AUD	5
+#define IOPMP_CHIP_DBG	6
+#define IOPMP_EIP120I	7
+#define IOPMP_EIP120II	8
+#define IOPMP_EIP120III	9
+#define IOPMP_ISP0	10
+#define IOPMP_ISP1	11
+#define IOPMP_DW200	12
+#define IOPMP_VIPRE	13
+#define IOPMP_VENC	14
+#define IOPMP_VDEC	15
+#define IOPMP_G2D	16
+#define IOPMP_FCE	17
+#define IOPMP_NPU	18
+#define IOPMP0_DPU	19
+#define IOPMP1_DPU	20
+#define IOPMP_GPU	21
+#define IOPMP_GMAC1	22
+#define IOPMP_GMAC2	23
+#define IOPMP_DMAC	24
+#define IOPMP_TEE_DMAC	25
+#define IOPMP_DSP0	26
+#define IOPMP_DSP1	27
+
+#endif /* __DT_THEAD_LIGHT_IOPMP_H__ */
