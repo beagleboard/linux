6d0d6e56e78d75a89c6f4c64d4f5c8d55b67bf8f
Robert Nelson <robertcnelson@gmail.com>
2025-08-29 11:24:42 -0500

    backports: tps65219: from: linux.git
    Reference: v6.16.4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- drivers/regulator/tps65219-regulator.c
+++ drivers/regulator/tps65219-regulator.c
@@ -422,7 +422,7 @@ static int tps65219_regulator_probe(struct platform_device *pdev)
 		rdev = devm_regulator_register(&pdev->dev, &pmic->common_rdesc[i],
 					       &config);
 		if (IS_ERR(rdev))
-			 return dev_err_probe(tps->dev, PTR_ERR(rdev),
+			return dev_err_probe(tps->dev, PTR_ERR(rdev),
 					      "Failed to register %s regulator\n",
 					      pmic->common_rdesc[i].name);
 	}
@@ -454,9 +454,9 @@ static int tps65219_regulator_probe(struct platform_device *pdev)
 						  irq_type->irq_name,
 						  irq_data);
 		if (error)
-			return dev_err_probe(tps->dev, PTR_ERR(rdev),
-					     "Failed to request %s IRQ %d: %d\n",
-					     irq_type->irq_name, irq, error);
+			return dev_err_probe(tps->dev, error,
+					     "Failed to request %s IRQ %d\n",
+					     irq_type->irq_name, irq);
 	}
 
 	for (i = 0; i < pmic->dev_irq_size; ++i) {
@@ -477,9 +477,9 @@ static int tps65219_regulator_probe(struct platform_device *pdev)
 						  irq_type->irq_name,
 						  irq_data);
 		if (error)
-			return dev_err_probe(tps->dev, PTR_ERR(rdev),
-					     "Failed to request %s IRQ %d: %d\n",
-					     irq_type->irq_name, irq, error);
+			return dev_err_probe(tps->dev, error,
+					     "Failed to request %s IRQ %d\n",
+					     irq_type->irq_name, irq);
 	}
 
 	return 0;
